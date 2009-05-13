a3f85bb4ffd1d78f0d63e7a58c402eea497d6e80
Mathieu Kermagoret <mkermagoret@merethis.com>
2009-05-13 13:02:25 +0000

    Added some comments in the Thread class.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk/centreon-broker@35 7c7d615a-03b3-410d-a316-1135b1bd9499

--- inc/thread.h
+++ inc/thread.h
@@ -7,7 +7,7 @@
 ** See LICENSE file for details.
 ** 
 ** Started on  05/04/09 Matthieu Kermagoret
-** Last update 05/05/09 Matthieu Kermagoret
+** Last update 05/13/09 Matthieu Kermagoret
 */
 
 #ifndef THREAD_H_
@@ -18,20 +18,55 @@
 
 namespace       CentreonBroker
 {
+  /**
+   *  Standard exception thrown by the Thread class.
+   */
+  class              ThreadException : public Exception
+  {
+   public:
+    enum             Where
+    {
+      UNKNOWN,
+      CANCEL,
+      JOIN,
+      RUN
+    };
+
+   private:
+    Where            where_;
+
+   public:
+                     ThreadException();
+                     ThreadException(const ThreadException& te);
+                     ThreadException(const char* str, Where w = UNKNOWN);
+                     ThreadException(const std::string& str,
+                                     Where w = UNKNOWN);
+                     ~ThreadException() throw();
+    ThreadException& operator=(const ThreadException& te);
+    Where            GetWhere() const throw ();
+    void             SetWhere(Where w) throw ();
+  };
+
+  /**
+   *  The Thread class is the root of all threads that can be created in
+   *  CentreonBroker. It is really to use. A class that want to be thread just
+   *  have to subclass Thread and redefine Core(). Then just call Run(), Join()
+   *  or Cancel() to manager the Thread.
+   */
   class         Thread
   {
-  private:
+   private:
     pthread_t   *thread;
                 Thread(const Thread& thread);
     Thread&     operator=(const Thread& thread);
 
-  public:
+   public:
                 Thread();
     virtual     ~Thread();
-    void        Cancel() throw (CentreonBroker::Exception);
+    void        Cancel() throw (ThreadException);
     virtual int Core() = 0;
-    void        Join() throw (CentreonBroker::Exception);
-    void        Run() throw (CentreonBroker::Exception);
+    void        Join() throw (ThreadException);
+    void        Run() throw (ThreadException);
   };
 }
 
