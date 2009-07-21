7ef70f8c3882de7fe2039c636a1f6a9307ed17e5
Mathieu Kermagoret <mkermagoret@merethis.com>
2009-07-21 14:49:22 +0000

    Working on file output.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk/centreon-broker@100 7c7d615a-03b3-410d-a316-1135b1bd9499

--- inc/file_output.h
+++ inc/file_output.h
@@ -21,38 +21,36 @@
 #ifndef FILE_OUTPUT_H_
 # define FILE_OUTPUT_H_
 
+# include <boost/thread.hpp>
 # include <fstream>
-# include <string>
-# include "condition_variable.h"
-# include "exception.h"
-# include "mutex.h"
-# include "thread.h"
+# include "event_subscriber.h"
+# include "waitable_list.hpp"
 
-namespace                             CentreonBroker
+namespace                       CentreonBroker
 {
-  class                               FileOutput
-    : private CentreonBroker::Thread
+  // Forward declarations
+  namespace                     Events
+  {
+    class                       Event;
+  }
+
+  class                         FileOutput : private EventSubscriber
   {
    private:
-    std::string                       buffer;
-    CentreonBroker::ConditionVariable condvar;
-    volatile bool                     exit_thread;
-    CentreonBroker::Mutex             mutex;
-    std::ofstream                     ofs;
-                                      FileOutput(const FileOutput& fileo);
-    FileOutput&                       operator=(const FileOutput& fileo);
-
-    // Thread method
-    int                               Core();
+    WaitableList<Events::Event> events_;
+    volatile bool               exit_;
+    std::ofstream               ofs;
+    boost::thread*              thread_;
+                                FileOutput(const FileOutput& fo);
+    FileOutput&                 operator=(const FileOutput& fo);
+    void                        OnEvent(Events::Event* e) throw ();
 
    public:
-                                      FileOutput();
-                                      ~FileOutput();
-    void                              Close();
-    void                              Open(const std::string& filename)
-      throw (CentreonBroker::Exception);
-
-    // XXX : handle events in a thread-safe way
+                                FileOutput();
+                                ~FileOutput();
+    void                        operator()();
+    void                        Close();
+    void                        Open(const char* filename);
   };
 }
 
