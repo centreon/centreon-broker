c3f3df1a70833831d183aaeb8057feda27874fdf
Mathieu Kermagoret <mkermagoret@merethis.com>
2009-12-15 09:12:04 +0000

    Added events.h header which includes all events headers.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@195 7c7d615a-03b3-410d-a316-1135b1bd9499

---
+++ centreon-broker/inc/events/events.h
@@ -0,0 +1,34 @@
+/*
+**  Copyright 2009 MERETHIS
+**  This file is part of CentreonBroker.
+**
+**  CentreonBroker is free software: you can redistribute it and/or modify it
+**  under the terms of the GNU General Public License as published by the Free
+**  Software Foundation, either version 2 of the License, or (at your option)
+**  any later version.
+**
+**  CentreonBroker is distributed in the hope that it will be useful, but
+**  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+**  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+**  for more details.
+**
+**  You should have received a copy of the GNU General Public License along
+**  with CentreonBroker.  If not, see <http://www.gnu.org/licenses/>.
+**
+**  For more information : contact@centreon.com
+*/
+
+#ifndef EVENTS_EVENTS_H_
+# define EVENTS_EVENTS_H_
+
+# include "events/acknowledgement.h"
+# include "events/comment.h"
+# include "events/downtime.h"
+# include "events/host.h"
+# include "events/host_group.h"
+# include "events/host_status.h"
+# include "events/program_status.h"
+# include "events/service.h"
+# include "events/service_status.h"
+
+#endif /* EVENTS_EVENTS_H_ */
