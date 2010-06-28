30f4c755f2d86c3ce5aaf743d33bbfce31746c1e
Mathieu Kermagoret <mkermagoret@merethis.com>
2010-06-28 15:31:07 +0000

    Updated file headers.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@447 7c7d615a-03b3-410d-a316-1135b1bd9499

--- centreon-broker/inc/exceptions/basic.hh
+++ centreon-broker/inc/exceptions/basic.hh
@@ -1,18 +1,21 @@
 /*
-** This file is part of Centreon Dhana.
+**  Copyright 2010 MERETHIS
+**  This file is part of CentreonBroker.
 **
-** Centreon Dhana is free software: you can redistribute it and/or modify it
-** under the terms of the GNU Affero General Public License as published by the
-** Free Software Foundation, either version 3 of the License, or (at your
-** option) any later version.
+**  CentreonBroker is free software: you can redistribute it and/or modify it
+**  under the terms of the GNU General Public License as published by the Free
+**  Software Foundation, either version 2 of the License, or (at your option)
+**  any later version.
 **
-** Centreon Dhana is distributed in the hope that it will be useful, but
-** WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-** or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
-** License for more details.
+**  CentreonBroker is distributed in the hope that it will be useful, but
+**  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+**  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+**  for more details.
 **
-** You should have received a copy of the GNU Affero General Public License
-** along with Centreon Dhana. If not, see <http://www.gnu.org/licenses/>.
+**  You should have received a copy of the GNU General Public License along
+**  with CentreonBroker.  If not, see <http://www.gnu.org/licenses/>.
+**
+**  For more information : contact@centreon.com
 */
 
 #ifndef EXCEPTIONS_BASIC_HH_
@@ -25,7 +28,7 @@ namespace                     exceptions
 {
   /**
    *  @class basic basic.hh "exceptions/basic.hh"
-   *  @brief Base class of exceptions thrown in Centreon Dhana.
+   *  @brief Base class of exceptions thrown in Centreon Broker.
    *
    *  basic is a simple exception class that is only associated with a message.
    *  basic accepts multiple types on input but its limitation is that it only
--- centreon-broker/inc/logging/backend.hh
+++ centreon-broker/inc/logging/backend.hh
@@ -1,18 +1,21 @@
 /*
-** This file is part of Centreon Dhana.
+**  Copyright 2010 MERETHIS
+**  This file is part of CentreonBroker.
 **
-** Centreon Dhana is free software: you can redistribute it and/or modify it
-** under the terms of the GNU Affero General Public License as published by the
-** Free Software Foundation, either version 3 of the License, or (at your
-** option) any later version.
+**  CentreonBroker is free software: you can redistribute it and/or modify it
+**  under the terms of the GNU General Public License as published by the Free
+**  Software Foundation, either version 2 of the License, or (at your option)
+**  any later version.
 **
-** Centreon Dhana is distributed in the hope that it will be useful, but
-** WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-** or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
-** License for more details.
+**  CentreonBroker is distributed in the hope that it will be useful, but
+**  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+**  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+**  for more details.
 **
-** You should have received a copy of the GNU Affero General Public License
-** along with Centreon Dhana. If not, see <http://www.gnu.org/licenses/>.
+**  You should have received a copy of the GNU General Public License along
+**  with CentreonBroker.  If not, see <http://www.gnu.org/licenses/>.
+**
+**  For more information : contact@centreon.com
 */
 
 #ifndef LOGGING_BACKEND_HH_
@@ -26,7 +29,7 @@ namespace        logging
    *  @class backend backend.hh "logging/backend.hh"
    *  @brief Interface for log backends.
    *
-   *  In Dhana, multiple backends are available for log writing. This can
+   *  In Broker, multiple backends are available for log writing. This can
    *  either be plain-text files, syslog or standard outputs. This class
    *  defines an interface to communicate with any of these backends.
    *
