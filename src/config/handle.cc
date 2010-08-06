4bfa8b678be30c788efcec6c6b32e215a65ba172
Mathieu Kermagoret <mkermagoret@merethis.com>
2010-08-06 15:29:18 +0000

    Updated license of new configuration handling files.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@469 7c7d615a-03b3-410d-a316-1135b1bd9499

--- src/config/handle.cc
+++ src/config/handle.cc
@@ -1,18 +1,21 @@
 /*
-** This file is part of Centreon Dhana.
+**  Copyright 2010 MERETHIS
+**  This file is part of CentreonBroker.
 **
-** Centreon Dhana is free software: you can redistribute it and/or modify it
-** under the terms of the GNU Affero General Public License as published by the
-** Free Software Foundation, either version 3 of the License, or (at your
-** option) any later version.
+**  CentreonBroker is free software: you can redistribute it and/or modify it
+**  under the terms of the GNU General Public License as published by the Free
+**  Software Foundation, either version 2 of the License, or (at your option)
+**  any later version.
 **
-** Centreon Dhana is distributed in the hope that it will be useful, but
-** WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-** or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
-** License for more details.
+**  CentreonBroker is distributed in the hope that it will be useful, but
+**  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+**  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+**  for more details.
 **
-** You should have received a copy of the GNU Affero General Public License
-** along with Centreon Dhana. If not, see <http://www.gnu.org/licenses/>.
+**  You should have received a copy of the GNU General Public License along
+**  with CentreonBroker.  If not, see <http://www.gnu.org/licenses/>.
+**
+**  For more information : contact@centreon.com
 */
 
 #include <signal.h>
