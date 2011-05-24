e181f3591927b2268b3515a3ee0c9979000e89d9
Mathieu Kermagoret <mkermagoret@merethis.com>
2011-05-24 12:26:03 +0000

    Added module loading class.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@615 7c7d615a-03b3-410d-a316-1135b1bd9499

---
+++ inc/modules/loader.hh
@@ -0,0 +1,56 @@
+/*
+** Copyright 2011 Merethis
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCB_MODULES_LOADER_HH_
+# define CCB_MODULES_LOADER_HH_
+
+# include <QList>
+# include <QSharedPointer>
+# include <QString>
+# include "modules/handle.hh"
+
+namespace         com {
+  namespace       centreon {
+    namespace     broker {
+      namespace   modules {
+        /**
+         *  @class loader loader.hh "modules/loader.hh"
+         *  @brief Load Centreon Broker plugins.
+         *
+         *  This class is used to load Centreon Broker plugins and hold
+         *  reference to them as long as they exist.
+         */
+        class     loader {
+         private:
+          QList<QSharedPointer<handle> >
+                  _handles;
+
+         public:
+                  loader();
+                  loader(loader const& l);
+                  ~loader();
+          loader& operator=(loader const& l);
+          void    load_dir(QString const& dirname);
+          void    load_file(QString const& filename);
+        };
+      }
+    }
+  }
+}
+
+#endif /* !CCB_MODULES_LOADER_HH_ */
---
+++ src/modules/loader.cc
@@ -0,0 +1,90 @@
+/*
+** Copyright 2011 Merethis
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <QDir>
+#include "modules/loader.hh"
+
+using namespace com::centreon::broker::modules;
+
+/**************************************
+*                                     *
+*           Public Methods            *
+*                                     *
+**************************************/
+
+/**
+ *  Default constructor.
+ */
+loader::loader() {}
+
+/**
+ *  Copy constructor.
+ *
+ *  @param[in] l Object to copy.
+ */
+loader::loader(loader const& l) : _handles(l._handles) {}
+
+/**
+ *  Destructor.
+ */
+loader::~loader() {}
+
+/**
+ *  Assignment operator.
+ *
+ *  @param[in] l Object to copy.
+ *
+ *  @return This object.
+ */
+loader& loader::operator=(loader const& l) {
+  _handles = l._handles;
+  return (*this);
+}
+
+/**
+ *  Load a directory containing plugins.
+ *
+ *  @param[in] dirname Directory name.
+ */
+void loader::load_dir(QString const& dirname) {
+  // Set directory browsing parameters.
+  QDir dir(dirname);
+  QStringList l;
+  l.push_back("*.so");
+  dir.setNameFilters(l);
+
+  // Iterate through all modules in directory.
+  l = dir.entryList();
+  for (QStringList::iterator it = l.begin(), end = l.end();
+       it != end;
+       ++it)
+    load_file(*it);
+
+  return ;
+}
+
+/**
+ *  Load a plugin.
+ *
+ *  @param[in] filename File name.
+ */
+void loader::load_file(QString const& filename) {
+  QSharedPointer<handle> handl(new handle);
+  handl->open(filename);
+  return ;
+}
