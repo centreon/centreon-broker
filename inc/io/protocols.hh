93d2233532f98116cc9c914d6cfd680625b96ea7
Mathieu Kermagoret <mkermagoret@merethis.com>
2011-05-25 14:44:52 +0000

    Added io::protocols singleton.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@620 7c7d615a-03b3-410d-a316-1135b1bd9499

---
+++ inc/io/protocols.hh
@@ -0,0 +1,64 @@
+/*
+** Copyright 2011 Merethis
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCB_IO_PROTOCOLS_HH_
+# define CCB_IO_PROTOCOLS_HH_
+
+# include <QMap>
+# include <QString>
+# include "endpoint.hh"
+# include "factory.hh"
+
+namespace                           com {
+  namespace                         centreon {
+    namespace                       broker {
+      namespace                     io {
+        /**
+         *  @class protocols protocols.hh "io/protocols.hh"
+         *  @brief Reference available protocols.
+         *
+         *  This class registers every available protocol that are used
+         *  to build input or output objects.
+         */
+        class                       protocols {
+         private:
+          struct                    protocol {
+            QSharedPointer<factory> endpntfactry;
+            unsigned short          osi_from;
+            unsigned short          osi_to;
+	  };
+	  QMap<QString, protocol>   _protocols;
+                                    protocols();
+                                    protocols(protocols const& p);
+          protocols&                operator=(protocols const& p);
+
+         public:
+                                    ~protocols();
+          protocols&                instance();
+          void                      reg(QString const& name,
+                                      factory const& fac,
+                                      unsigned short osi_from,
+                                      unsigned short osi_to);
+          void                      unreg(QString const& name);
+        };
+      }
+    }
+  }
+}
+
+#endif /* !CCB_IO_PROTOCOLS_HH_ */
---
+++ src/io/protocols.cc
@@ -0,0 +1,100 @@
+/*
+** Copyright 2011 Merethis
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <assert.h>
+#include <stdlib.h>
+#include "io/protocols.hh"
+
+using namespace com::centreon::broker::io;
+
+/**************************************
+*                                     *
+*           Private Methods           *
+*                                     *
+**************************************/
+
+/**
+ *  Default constructor.
+ */
+protocols::protocols() {}
+
+/**
+ *  @brief Copy constructor.
+ *
+ *  Should not be used. Any attempt to use this constructor will result
+ *  in a call to abort().
+ *
+ *  @param[in] p Unused.
+ */
+protocols::protocols(protocols const& p) {
+  (void)p;
+  assert(false);
+  abort();
+}
+
+/**
+ *  @brief Assignment operator.
+ *
+ *  Should not be used. Any attempt to use this method will result in a
+ *  call to abort().
+ *
+ *  @param[in] p Unused.
+ *
+ *  @return This object.
+ */
+protocols& protocols::operator=(protocols const& p) {
+  (void)p;
+  assert(false);
+  abort();
+  return (*this);
+}
+
+/**
+ *  Register a protocol.
+ *
+ *  @param[in] name         Protocol name.
+ *  @param[in] endpntfactry Factory that allocates new endpoint of the
+ *                          matching protocol type.
+ *  @param[in] osi_from     OSI layer this protocol supports.
+ *  @param[in] osi_to       OSI layer this protocol supports.
+ */
+void protocols::reg(QString const& name,
+                    factory const& fac,
+                    unsigned short osi_from,
+                    unsigned short osi_to) {
+  // Set protocol structure.
+  protocol p;
+  p.endpntfactry = QSharedPointer<factory>(fac.clone());
+  p.osi_from = osi_from;
+  p.osi_to = osi_to;
+
+  // Register protocol in protocol list.
+  _protocols[name] = p;
+
+  return ;
+}
+
+/**
+ *  Unregister a protocol.
+ *
+ *  @param[in] name Protocol name.
+ */
+void protocols::unreg(QString const& name) {
+  _protocols.remove(name);
+  return ;
+}
