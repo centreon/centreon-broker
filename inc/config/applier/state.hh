e32f5e833ebe5b002d1995ab571b5b09c5e5baf4
Mathieu Kermagoret <mkermagoret@merethis.com>
2011-05-26 11:32:53 +0000

    Added main configuration applier skeleton. Currently only uses the logger applier.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@630 7c7d615a-03b3-410d-a316-1135b1bd9499

---
+++ inc/config/applier/state.hh
@@ -0,0 +1,52 @@
+/*
+** Copyright 2011 Merethis
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCB_CONFIG_APPLIER_STATE_HH_
+# define CCB_CONFIG_APPLIER_STATE_HH_
+
+# include "config/state.hh"
+
+namespace                 com {
+  namespace               centreon {
+    namespace             broker {
+      namespace           config {
+        namespace         applier {
+          /**
+           *  @class state state.hh "config/applier/state.hh"
+           *  @brief Apply a configuration.
+           *
+           *  Apply some configuration state.
+           */
+          class           state {
+           private:
+                          state();
+                          state(state const& s);
+            state&        operator=(state const& s);
+
+           public:
+                          ~state();
+            void          apply(com::centreon::broker::config::state const& s);
+            static state& instance();
+          };
+        }
+      }
+    }
+  }
+}
+
+#endif /* !CCB_CONFIG_APPLIER_STATE_HH_ */
---
+++ src/config/applier/state.cc
@@ -0,0 +1,119 @@
+/*
+** Copyright 2011 Merethis
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <assert.h>
+#include <QCoreApplication>
+#include <QMutex>
+#include <QMutexLocker>
+#include <QScopedPointer>
+#include <stdlib.h>
+#include "config/applier/logger.hh"
+#include "config/applier/state.hh"
+
+using namespace com::centreon::broker::config::applier;
+
+/**************************************
+*                                     *
+*           Private Methods           *
+*                                     *
+**************************************/
+
+/**
+ *  Default constructor.
+ */
+state::state() {}
+
+/**
+ *  @brief Copy constructor.
+ *
+ *  Any call to this constructor will result in a call to abort().
+ *
+ *  @param[in] s Object to copy.
+ */
+state::state(state const& s) {
+  (void)s;
+  assert(false);
+  abort();
+}
+
+/**
+ *  @brief Assignment operator.
+ *
+ *  Any call to this method will result in a call to abort().
+ *
+ *  @param[in] s Object to copy.
+ *
+ *  @return This object.
+ */
+state& state::operator=(state const& s) {
+  (void)s;
+  assert(false);
+  abort();
+  return (*this);
+}
+
+/**************************************
+*                                     *
+*           Public Methods            *
+*                                     *
+**************************************/
+
+/**
+ *  Destructor.
+ */
+state::~state() {}
+
+/**
+ *  Apply a configuration state.
+ *
+ *  @param[in] s State to apply.
+ */
+void state::apply(com::centreon::broker::config::state const& s) {
+  // Unload old modules.
+  // XXX
+
+  // Load new modules.
+  // XXX
+
+  // Apply logging configuration.
+  logger::instance().apply(s.loggers());
+
+  // Close old inputs.
+  // XXX
+
+  // Close old outputs.
+  // XXX
+
+  // Open new outputs.
+  // XXX
+
+  // Open new inputs.
+  // XXX
+
+  return ;
+}
+
+/**
+ *  Get the instance of this object.
+ *
+ *  @return Class instance.
+ */
+state& state::instance() {
+  static state gl_state;
+  return (gl_state);
+}
