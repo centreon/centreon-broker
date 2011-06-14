684ac7623c21e80f7b76b8dabc1dada88b5f914f
Mathieu Kermagoret <mkermagoret@merethis.com>
2011-06-14 14:11:33 +0000

    Fix in NDO layer which caught thrown exceptions.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@722 7c7d615a-03b3-410d-a316-1135b1bd9499

--- ndo/src/output.cc
+++ ndo/src/output.cc
@@ -104,214 +104,211 @@ QSharedPointer<io::data> output::read() {
 void output::write(QSharedPointer<io::data> i) {
   logging::debug << logging::MEDIUM << "NDO: writing data";
   events::event* e((events::event*)i.data());
-  try {
-    std::stringstream buffer;
-    switch (e->type()) {
-     case events::event::ACKNOWLEDGEMENT:
-      buffer << NDO_API_ACKNOWLEDGEMENTDATA << ":\n";
-      handle_event<events::acknowledgement>(
-        *static_cast<events::acknowledgement*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::COMMENT:
-      buffer << NDO_API_COMMENTDATA << ":\n";
-      handle_event<events::comment>(
-        *static_cast<events::comment*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::CUSTOMVARIABLE:
-      buffer << NDO_API_RUNTIMEVARIABLES << ":\n";
-      handle_event<events::custom_variable>(
-        *static_cast<events::custom_variable*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::CUSTOMVARIABLESTATUS:
-      buffer << NDO_API_CONFIGVARIABLES << ":\n";
-      handle_event<events::custom_variable_status>(
-        *static_cast<events::custom_variable_status*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::DOWNTIME:
-      buffer << NDO_API_DOWNTIMEDATA << ":\n";
-      handle_event<events::downtime>(
-        *static_cast<events::downtime*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::EVENTHANDLER:
-      buffer << NDO_API_EVENTHANDLERDATA << ":\n";
-      handle_event<events::event_handler>(
-        *static_cast<events::event_handler*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::FLAPPINGSTATUS:
-      buffer << NDO_API_FLAPPINGDATA << ":\n";
-      handle_event<events::flapping_status>(
-        *static_cast<events::flapping_status*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::HOST:
-      buffer << NDO_API_HOSTDEFINITION << ":\n";
-      handle_event<events::host>(
-        *static_cast<events::host*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::HOSTCHECK:
-      buffer << NDO_API_HOSTCHECKDATA << ":\n";
-      handle_event<events::host_check>(
-        *static_cast<events::host_check*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::HOSTDEPENDENCY:
-      buffer << NDO_API_HOSTDEPENDENCYDEFINITION << ":\n";
-      handle_event<events::host_dependency>(
-        *static_cast<events::host_dependency*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::HOSTGROUP:
-      buffer << NDO_API_HOSTGROUPDEFINITION << ":\n";
-      handle_event<events::host_group>(
-        *static_cast<events::host_group*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::HOSTGROUPMEMBER:
-      buffer << NDO_API_HOSTGROUPMEMBERDEFINITION << ":\n";
-      handle_event<events::host_group_member>(
-        *static_cast<events::host_group_member*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::HOSTPARENT:
-      buffer << NDO_API_HOSTPARENT << ":\n";
-      handle_event<events::host_parent>(
-        *static_cast<events::host_parent*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::HOSTSTATE:
-      buffer << NDO_API_STATECHANGEDATA << ":\n";
-      handle_event<events::host_state>(
-        *static_cast<events::host_state*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::HOSTSTATUS:
-      buffer << NDO_API_HOSTSTATUSDATA << ":\n";
-      handle_event<events::host_status>(
-        *static_cast<events::host_status*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::INSTANCE:
-      buffer << NDO_API_PROCESSDATA << ":\n";
-      handle_event<events::instance>(
-        *static_cast<events::instance*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::INSTANCESTATUS:
-      buffer << NDO_API_PROGRAMSTATUSDATA << ":\n";
-      handle_event<events::instance_status>(
-        *static_cast<events::instance_status*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::LOG:
-      buffer << NDO_API_LOGDATA << ":\n";
-      handle_event<events::log_entry>(
-        *static_cast<events::log_entry*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::MODULE:
-      buffer << NDO_API_COMMANDDEFINITION << ":\n";
-      handle_event<events::module>(
-        *static_cast<events::module*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::NOTIFICATION:
-      buffer << NDO_API_NOTIFICATIONDATA << ":\n";
-      handle_event<events::notification>(
-        *static_cast<events::notification*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::PERFDATA:
-      buffer << NDO_API_PERFDATA << ":\n";
-      handle_event<events::perfdata>(
-        *static_cast<events::perfdata*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::SERVICE:
-      buffer << NDO_API_SERVICEDEFINITION << ":\n";
-      handle_event<events::service>(
-        *static_cast<events::service*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::SERVICECHECK:
-      buffer << NDO_API_SERVICECHECKDATA << ":\n";
-      handle_event<events::service_check>(
-        *static_cast<events::service_check*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::SERVICEDEPENDENCY:
-      buffer << NDO_API_SERVICEDEPENDENCYDEFINITION << ":\n";
-      handle_event<events::service_dependency>(
-        *static_cast<events::service_dependency*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-     case events::event::SERVICEGROUP:
-      buffer << NDO_API_SERVICEGROUPDEFINITION << ":\n";
-      handle_event<events::service_group>(
-        *static_cast<events::service_group*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::SERVICEGROUPMEMBER:
-      buffer << NDO_API_SERVICEGROUPMEMBERDEFINITION << ":\n";
-      handle_event<events::service_group_member>(
-        *static_cast<events::service_group_member*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::SERVICESTATE:
-      buffer << NDO_API_ADAPTIVESERVICEDATA << ":\n";
-      handle_event<events::service_state>(
-        *static_cast<events::service_state*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-     case events::event::SERVICESTATUS:
-      buffer << NDO_API_SERVICESTATUSDATA << ":\n";
-      handle_event<events::service_status>(
-        *static_cast<events::service_status*>(e),
-        buffer);
-      buffer << NDO_API_ENDDATA << "\n";
-      break ;
-    }
-    buffer << "\n";
-
-    // Send data.
-    QSharedPointer<io::raw> data(new io::raw);
-    data->append(buffer.str().c_str());
-    _to->write(data.staticCast<io::data>());
-    logging::debug << logging::MEDIUM << "data successfully sent";
+  std::stringstream buffer;
+  switch (e->type()) {
+   case events::event::ACKNOWLEDGEMENT:
+    buffer << NDO_API_ACKNOWLEDGEMENTDATA << ":\n";
+    handle_event<events::acknowledgement>(
+      *static_cast<events::acknowledgement*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::COMMENT:
+    buffer << NDO_API_COMMENTDATA << ":\n";
+    handle_event<events::comment>(
+      *static_cast<events::comment*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::CUSTOMVARIABLE:
+    buffer << NDO_API_RUNTIMEVARIABLES << ":\n";
+    handle_event<events::custom_variable>(
+      *static_cast<events::custom_variable*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::CUSTOMVARIABLESTATUS:
+    buffer << NDO_API_CONFIGVARIABLES << ":\n";
+    handle_event<events::custom_variable_status>(
+      *static_cast<events::custom_variable_status*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::DOWNTIME:
+    buffer << NDO_API_DOWNTIMEDATA << ":\n";
+    handle_event<events::downtime>(
+      *static_cast<events::downtime*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::EVENTHANDLER:
+    buffer << NDO_API_EVENTHANDLERDATA << ":\n";
+    handle_event<events::event_handler>(
+      *static_cast<events::event_handler*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::FLAPPINGSTATUS:
+    buffer << NDO_API_FLAPPINGDATA << ":\n";
+    handle_event<events::flapping_status>(
+      *static_cast<events::flapping_status*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::HOST:
+    buffer << NDO_API_HOSTDEFINITION << ":\n";
+    handle_event<events::host>(
+      *static_cast<events::host*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::HOSTCHECK:
+    buffer << NDO_API_HOSTCHECKDATA << ":\n";
+    handle_event<events::host_check>(
+      *static_cast<events::host_check*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::HOSTDEPENDENCY:
+    buffer << NDO_API_HOSTDEPENDENCYDEFINITION << ":\n";
+    handle_event<events::host_dependency>(
+      *static_cast<events::host_dependency*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::HOSTGROUP:
+    buffer << NDO_API_HOSTGROUPDEFINITION << ":\n";
+    handle_event<events::host_group>(
+      *static_cast<events::host_group*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::HOSTGROUPMEMBER:
+    buffer << NDO_API_HOSTGROUPMEMBERDEFINITION << ":\n";
+    handle_event<events::host_group_member>(
+      *static_cast<events::host_group_member*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::HOSTPARENT:
+    buffer << NDO_API_HOSTPARENT << ":\n";
+    handle_event<events::host_parent>(
+      *static_cast<events::host_parent*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::HOSTSTATE:
+    buffer << NDO_API_STATECHANGEDATA << ":\n";
+    handle_event<events::host_state>(
+      *static_cast<events::host_state*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::HOSTSTATUS:
+    buffer << NDO_API_HOSTSTATUSDATA << ":\n";
+    handle_event<events::host_status>(
+      *static_cast<events::host_status*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::INSTANCE:
+    buffer << NDO_API_PROCESSDATA << ":\n";
+    handle_event<events::instance>(
+      *static_cast<events::instance*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::INSTANCESTATUS:
+    buffer << NDO_API_PROGRAMSTATUSDATA << ":\n";
+    handle_event<events::instance_status>(
+      *static_cast<events::instance_status*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::LOG:
+    buffer << NDO_API_LOGDATA << ":\n";
+    handle_event<events::log_entry>(
+      *static_cast<events::log_entry*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::MODULE:
+    buffer << NDO_API_COMMANDDEFINITION << ":\n";
+    handle_event<events::module>(
+      *static_cast<events::module*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::NOTIFICATION:
+    buffer << NDO_API_NOTIFICATIONDATA << ":\n";
+    handle_event<events::notification>(
+      *static_cast<events::notification*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::PERFDATA:
+    buffer << NDO_API_PERFDATA << ":\n";
+    handle_event<events::perfdata>(
+      *static_cast<events::perfdata*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::SERVICE:
+    buffer << NDO_API_SERVICEDEFINITION << ":\n";
+    handle_event<events::service>(
+      *static_cast<events::service*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::SERVICECHECK:
+    buffer << NDO_API_SERVICECHECKDATA << ":\n";
+    handle_event<events::service_check>(
+      *static_cast<events::service_check*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::SERVICEDEPENDENCY:
+    buffer << NDO_API_SERVICEDEPENDENCYDEFINITION << ":\n";
+    handle_event<events::service_dependency>(
+      *static_cast<events::service_dependency*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+   case events::event::SERVICEGROUP:
+    buffer << NDO_API_SERVICEGROUPDEFINITION << ":\n";
+    handle_event<events::service_group>(
+      *static_cast<events::service_group*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::SERVICEGROUPMEMBER:
+    buffer << NDO_API_SERVICEGROUPMEMBERDEFINITION << ":\n";
+    handle_event<events::service_group_member>(
+      *static_cast<events::service_group_member*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::SERVICESTATE:
+    buffer << NDO_API_ADAPTIVESERVICEDATA << ":\n";
+    handle_event<events::service_state>(
+      *static_cast<events::service_state*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
+   case events::event::SERVICESTATUS:
+    buffer << NDO_API_SERVICESTATUSDATA << ":\n";
+    handle_event<events::service_status>(
+      *static_cast<events::service_status*>(e),
+      buffer);
+    buffer << NDO_API_ENDDATA << "\n";
+    break ;
   }
-  catch (...) {}
+  buffer << "\n";
+
+  // Send data.
+  QSharedPointer<io::raw> data(new io::raw);
+  data->append(buffer.str().c_str());
+  _to->write(data.staticCast<io::data>());
+  logging::debug << logging::MEDIUM << "data successfully sent";
 
   return ;
 }
