f63ccaad4532a10e56a39f32d08e45486402b9f2
Mathieu Kermagoret <mkermagoret@merethis.com>
2011-07-08 12:11:23 +0000

    Added unit test related to commit 810.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@811 7c7d615a-03b3-410d-a316-1135b1bd9499

---
+++ core/test/logging/logging/concurrent.cc
@@ -0,0 +1,101 @@
+/*
+** Copyright 2011 Merethis
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <QDir>
+#include <QRegExp>
+#include <QString>
+#include <QThreadPool>
+#include <QtCore>
+#include "com/centreon/broker/logging/file.hh"
+#include "com/centreon/broker/logging/logging.hh"
+
+using namespace com::centreon::broker;
+
+#define MESSAGE "0123456789abcdefghijklmnopqrstuvwxyz"
+#define TEMP_FILE "centreon_broker_unit_test.tmp"
+#define THREAD_COUNT 50
+#define WRITE_COUNT 200
+
+/**
+ *  Log some messages.
+ */
+static void log_messages() {
+  for (unsigned int i = 0; i < WRITE_COUNT; ++i)
+    logging::error << logging::HIGH << MESSAGE;
+  return ;
+}
+
+/**
+ *  Check that logging is thread-safe.
+ *
+ *  @return 0 on success.
+ */
+int main() {
+  // Build filename.
+  QString filename(QDir::tempPath());
+  filename.append("/" TEMP_FILE);
+
+  // Remove old file.
+  QFile::remove(filename);
+
+  // Initialize file backend.
+  logging::file::with_timestamp(true);
+  QSharedPointer<logging::file> backend(new logging::file(filename));
+
+  // Add backend to logging list.
+  logging::log_on(backend);
+
+  // Reserve threads.
+  for (unsigned int i = 0; i < THREAD_COUNT; ++i)
+    QThreadPool::globalInstance()->reserveThread();
+
+  // Run tasks.
+  for (unsigned int i = 0; i < THREAD_COUNT; ++i)
+    QtConcurrent::run(log_messages);
+
+  // Wait for tasks to finish.
+  QThreadPool::globalInstance()->waitForDone();
+
+  // Remove backend from logging.
+  logging::log_on(backend, 0, logging::NONE);
+  backend.clear();
+
+  // Release threads.
+  QThreadPool::globalInstance()->releaseThread();
+
+  // Return value.
+  int retval(0);
+
+  // Message regexp.
+  QRegExp regex(".*" MESSAGE ".*");
+
+  // Read file.
+  QFile f(filename);
+  retval |= !f.open(QIODevice::ReadOnly);
+  if (!retval) {
+    f.readLine();
+    for (unsigned int i = 0;
+         !retval && (i < THREAD_COUNT * WRITE_COUNT);
+         ++i)
+      retval |= (regex.indexIn(f.readLine()) == -1);
+    f.readLine();
+    retval |= !f.readLine().isEmpty();
+  }
+
+  return (retval);
+}
