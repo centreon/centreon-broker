bae8919012e4c311d5b4d1332251d0ddf33506ae
Mathieu Kermagoret <mkermagoret@merethis.com>
2011-09-08 09:38:42 +0000

    Fix input failover thread creation which caused heavy memory consumption.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@846 7c7d615a-03b3-410d-a316-1135b1bd9499

--- core/src/config/applier/endpoint.cc
+++ core/src/config/applier/endpoint.cc
@@ -184,7 +184,7 @@ processing::failover* endpoint::_create_endpoint(config::endpoint const& cfg,
   }
 
   // Return failover thread.
-  QScopedPointer<processing::failover> fo(new processing::failover(true));
+  QScopedPointer<processing::failover> fo(new processing::failover(is_output));
   fo->set_retry_interval(cfg.retry_interval);
   fo->set_endpoint(endp);
   fo->set_failover(failovr);
