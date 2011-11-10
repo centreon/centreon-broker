0fc86a33f3b21bb694228df315436c544019dd19
Mathieu Kermagoret <mkermagoret@merethis.com>
2011-11-10 09:52:00 +0000

    Added TCP acceptor unit test.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@998 7c7d615a-03b3-410d-a316-1135b1bd9499

---
+++ tcp/test/acceptor/close_with_open_stream.cc
@@ -0,0 +1,126 @@
+/*
+** Copyright 2011 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <QCoreApplication>
+#include <QTcpSocket>
+#include <QThread>
+#include <QTimer>
+#include "com/centreon/broker/config/applier/init.hh"
+#include "com/centreon/broker/tcp/acceptor.hh"
+#include "test/common.hh"
+
+using namespace com::centreon::broker;
+
+/**
+ *  Thread that will listen on a port.
+ */
+class            concurrent : public QThread {
+ private:
+  tcp::acceptor* _acceptor;
+  unsigned short _port;
+
+ public:
+  /**
+   *  Default constructor.
+   */
+                 concurrent() : _acceptor(NULL) {}
+
+  /**
+   *  Destructor.
+   */
+                 ~concurrent() {
+    delete _acceptor;
+  }
+
+  /**
+   *  Close acceptor.
+   */
+  void           close() {
+    _acceptor->close();
+    return ;
+  }
+
+  /**
+   *  Thread entry point.
+   */
+  void           run() {
+    _acceptor = new tcp::acceptor;
+    _acceptor->listen_on(_port);
+    try {
+      QSharedPointer<io::stream> s(_acceptor->open());
+      if (!s.isNull())
+        s->read();
+    }
+    catch (...) {}
+    return ;
+  }
+
+  /**
+   *  Set port to listen on.
+   *
+   *  @param[in] port Port to listen on.
+   */
+  void           set_port(unsigned short port) {
+    _port = port;
+    return ;
+  }
+};
+
+/**
+ *  Check that close works with open stream.
+ *
+ *  @param[in] argc Argument count.
+ *  @param[in] argv Argument values.
+ *
+ *  @return 0 on success.
+ */
+int main(int argc, char* argv[]) {
+  // Qt core object.
+  QCoreApplication app(argc, argv);
+
+  // Initialization.
+  config::applier::init();
+
+  // Thread that will listen on a port.
+  concurrent c;
+  unsigned short port(random_port());
+  c.set_port(port);
+  QObject::connect(&c, SIGNAL(finished()), &app, SLOT(quit()));
+  QObject::connect(&c, SIGNAL(started()), &app, SLOT(quit()));
+  QObject::connect(&c, SIGNAL(terminated()), &app, SLOT(quit()));
+  c.start();
+  app.exec();
+
+  // Wait for thread to listen.
+  QTimer::singleShot(500, &app, SLOT(quit()));
+  app.exec();
+
+  // Open new stream.
+  int retval(0);
+  QTcpSocket sock;
+  sock.connectToHost("localhost", port);
+  if (!sock.waitForConnected())
+    retval = 1;
+
+  // Concurrently close acceptor.
+  c.close();
+
+  // For for thread to finish.
+  return (!c.wait(2000) || retval);
+}
