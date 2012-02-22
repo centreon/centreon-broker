88b1c9d9b9e3c226e0b2ad8db0000c06cbc53500
Mathieu Kermagoret <mkermagoret@merethis.com>
2012-02-22 13:14:51 +0000

    Added log size limitation.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@1063 7c7d615a-03b3-410d-a316-1135b1bd9499

--- core/inc/com/centreon/broker/logging/file.hh
+++ core/inc/com/centreon/broker/logging/file.hh
@@ -1,5 +1,5 @@
 /*
-** Copyright 2009-2011 Merethis
+** Copyright 2009-2012 Merethis
 **
 ** This file is part of Centreon Broker.
 **
@@ -17,47 +17,53 @@
 ** <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CCB_LOGGING_FILE_HH_
-# define CCB_LOGGING_FILE_HH_
+#ifndef CCB_LOGGING_FILE_HH
+#  define CCB_LOGGING_FILE_HH
 
-# include <QFile>
-# include "com/centreon/broker/logging/backend.hh"
-# include "com/centreon/broker/namespace.hh"
+#  include <QFile>
+#  include "com/centreon/broker/logging/backend.hh"
+#  include "com/centreon/broker/namespace.hh"
 
 CCB_BEGIN()
 
-namespace         logging {
+namespace              logging {
   /**
    *  @class file file.hh "com/centreon/broker/logging/file.hh"
    *  @brief Log messages to a file.
    *
    *  Log messages to a file.
    */
-  class         file : public backend {
-   private:
-    QFile       _file;
-    bool        _special;
-    static bool _with_timestamp;
-    static bool _with_thread_id;
-                file(file const& f);
-    file&       operator=(file const& f);
-    void        _write(char const* data) throw ();
-
+  class                file : public backend {
    public:
-                file(QString const& path);
-                file(FILE* special);
-                ~file();
-    void        log_msg(char const* msg,
-                  unsigned int len,
-                  type log_type,
-                  level l) throw ();
-    static bool with_thread_id();
-    static void with_thread_id(bool enable);
-    static bool with_timestamp();
-    static void with_timestamp(bool enable);
+                       file(
+                         QString const& path,
+                         unsigned long long max = 100000000000ull);
+                       file(FILE* special);
+                       ~file();
+    void               log_msg(char const* msg,
+                         unsigned int len,
+                         type log_type,
+                         level l) throw ();
+    static bool        with_thread_id();
+    static void        with_thread_id(bool enable);
+    static bool        with_timestamp();
+    static void        with_timestamp(bool enable);
+
+   private:
+                       file(file const& f);
+    file&              operator=(file const& f);
+    void               _internal_copy(file const& f);
+    void               _reopen();
+    void               _write(char const* data) throw ();
+    QFile              _file;
+    unsigned long long _max;
+    bool               _special;
+    static bool        _with_timestamp;
+    static bool        _with_thread_id;
+    unsigned long long _written;
   };
 }
 
 CCB_END()
 
-#endif /* !CCB_LOGGING_FILE_HH_ */
+#endif // !CCB_LOGGING_FILE_HH
--- core/src/logging/file.cc
+++ core/src/logging/file.cc
@@ -1,5 +1,5 @@
 /*
-** Copyright 2009-2011 Merethis
+** Copyright 2009-2012 Merethis
 **
 ** This file is part of Centreon Broker.
 **
@@ -24,6 +24,11 @@
 #include "com/centreon/broker/logging/file.hh"
 
 using namespace com::centreon::broker::logging;
+
+// Messages.
+#define LOG_CLOSE_STR "Centreon Broker log file closed\n"
+#define LOG_OPEN_STR "Centreon Broker log file opened\n"
+#define LOG_ROTATION_STR "Centreon Broker log file rotation\n"
 
 // Should thread ID be printed ?
 bool file::_with_thread_id(false);
@@ -54,56 +59,6 @@ struct integer_width {
 
 /**************************************
 *                                     *
-*           Private Methods           *
-*                                     *
-**************************************/
-
-/**
- *  Copy constructor.
- *
- *  @param[in] f Unused.
- */
-file::file(file const& f) : backend() {
-  (void)f;
-  assert(false);
-  abort();
-}
-
-/**
- *  Assignment operator.
- *
- *  @param[in] f Unused.
- *
- *  @return This object.
- */
-file& file::operator=(file const& f) {
-  (void)f;
-  assert(false);
-  abort();
-  return (*this);
-}
-
-/**
- *  Write an amount of data to the file.
- *
- *  @param[in] data Data to write.
- */
-void file::_write(char const* data) throw () {
-  qint64 to_write(strlen(data));
-  qint64 rb(_file.write(data, to_write));
-  to_write -= rb;
-  data += rb;
-  while ((to_write > 0) && (rb >= 0)) {
-    _file.waitForBytesWritten(-1);
-    rb = _file.write(data, to_write);
-    to_write -= rb;
-    data += rb;
-  }
-  return ;
-}
-
-/**************************************
-*                                     *
 *           Public Methods            *
 *                                     *
 **************************************/
@@ -112,13 +67,18 @@ void file::_write(char const* data) throw () {
  *  Regular file constructor.
  *
  *  @param[in] path Path to the log file.
+ *  @param[in] max  Maximum file size of log file.
  */
-file::file(QString const& path) : _file(path), _special(false) {
+file::file(QString const& path, unsigned long long max)
+  : _file(path), _max(0), _special(false), _written(0) {
   if (!_file.open(QIODevice::WriteOnly | QIODevice::Append))
     throw (exceptions::msg() << "log: could not open file '" << path
              << "': " << _file.errorString());
-  _write("Centreon Broker log file opened\n");
+  _max = ((max < 1000000) ? 1000000 : max)
+         - sizeof(LOG_ROTATION_STR) + 1;
+  _write(LOG_OPEN_STR);
   _file.flush();
+  _written = _file.size();
 }
 
 /**
@@ -137,7 +97,7 @@ file::file(FILE* special) : _special(true) {
  */
 file::~file() {
   if (!_special)
-    _write("Centreon Broker log file closed\n");
+    _write(LOG_CLOSE_STR);
   _file.flush();
   _file.close();
 }
@@ -237,5 +197,97 @@ bool file::with_timestamp() {
  */
 void file::with_timestamp(bool enable) {
   _with_timestamp = enable;
+  return ;
+}
+
+/**************************************
+*                                     *
+*           Private Methods           *
+*                                     *
+**************************************/
+
+/**
+ *  Copy constructor.
+ *
+ *  @param[in] f Unused.
+ */
+file::file(file const& f) : backend() {
+  _internal_copy(f);
+}
+
+/**
+ *  Assignment operator.
+ *
+ *  @param[in] f Unused.
+ *
+ *  @return This object.
+ */
+file& file::operator=(file const& f) {
+  _internal_copy(f);
+  return (*this);
+}
+
+/**
+ *  Copy internal data members.
+ *
+ *  @param[in] f Unused.
+ */
+void file::_internal_copy(file const& f) {
+  (void)f;
+  assert(!"logging file is not copyable");
+  abort();
+  return ;
+}
+
+/**
+ *  Reopen log file.
+ */
+void file::_reopen() {
+  // Write last log line.
+  _written = 0;
+  _write(LOG_ROTATION_STR);
+  _file.flush();
+  _file.close();
+
+  // Move log file to .old
+  QString backup_name(_file.fileName());
+  backup_name.append(".old");
+  QFile::remove(backup_name);
+  QFile::rename(_file.fileName(), backup_name);
+
+  // Reopen file.
+  _file.open(QIODevice::WriteOnly | QIODevice::Truncate);
+  _written = 0;
+  _write(LOG_OPEN_STR);
+
+  return ;
+}
+
+/**
+ *  Write an amount of data to the file.
+ *
+ *  @param[in] data Data to write.
+ */
+void file::_write(char const* data) throw () {
+  // Check sizes.
+  qint64 to_write(strlen(data));
+  if (!_special && (_written + to_write > _max))
+    _reopen();
+
+  // Write data.
+  qint64 wb(_file.write(data, to_write));
+  to_write -= wb;
+  data += wb;
+  if (wb > 0)
+    _written += wb;
+  while ((to_write > 0) && (wb >= 0)) {
+    _file.waitForBytesWritten(-1);
+    wb = _file.write(data, to_write);
+    to_write -= wb;
+    data += wb;
+    if (wb > 0)
+      _written += wb;
+  }
+
   return ;
 }
