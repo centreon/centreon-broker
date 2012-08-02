ec31a8c8202c78b5b3f94932f99dd4c47f2b3deb
Mathieu Kermagoret <mkermagoret@merethis.com>
2012-08-02 09:28:48 +0000

    Added unit test to check that child TCP connections are properly closed when acceptor is closed.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/branches/2.2@1164 7c7d615a-03b3-410d-a316-1135b1bd9499

--- tcp/test/acceptor/close_concurrent.cc
+++ tcp/test/acceptor/close_concurrent.cc
@@ -17,33 +17,36 @@
 ** <http://www.gnu.org/licenses/>.
 */
 
+#include <cstdlib>
+#include <iostream>
 #include <QCoreApplication>
 #include <QThread>
 #include <QTimer>
 #include "com/centreon/broker/config/applier/init.hh"
+#include "com/centreon/broker/io/exceptions/shutdown.hh"
 #include "com/centreon/broker/tcp/acceptor.hh"
+#include "com/centreon/broker/tcp/connector.hh"
 #include "test/common.hh"
 
 using namespace com::centreon::broker;
 
+// Exit flag.
+static volatile bool should_exit(false);
+
 /**
  *  Thread that will listen on a port.
  */
-class            concurrent : public QThread {
- private:
-  tcp::acceptor* _acceptor;
-  unsigned short _port;
-
- public:
+class            concurrent_acceptor : public QThread {
+public:
   /**
    *  Default constructor.
    */
-                 concurrent() : _acceptor(NULL) {}
+                 concurrent_acceptor() : _acceptor(NULL) {}
 
   /**
    *  Destructor.
    */
-                 ~concurrent() {
+                 ~concurrent_acceptor() {
     delete _acceptor;
   }
 
@@ -62,9 +65,16 @@ class            concurrent : public QThread {
     _acceptor = new tcp::acceptor;
     _acceptor->listen_on(_port);
     try {
-      _acceptor->open();
+      while (!should_exit)
+        _acceptor->open();
     }
-    catch (...) {}
+    catch (io::exceptions::shutdown const& e) {
+      (void)e;
+    }
+    catch (std::exception const& e) {
+      std::cerr << e.what() << std::endl;
+      exit(EXIT_FAILURE);
+    }
     return ;
   }
 
@@ -77,6 +87,67 @@ class            concurrent : public QThread {
     _port = port;
     return ;
   }
+
+private:
+  tcp::acceptor* _acceptor;
+  unsigned short _port;
+};
+
+/**
+ *  Thread that will connect to the current host on a port.
+ */
+class             concurrent_connector : public QThread {
+public:
+  /**
+   *  Default constructor.
+   */
+                  concurrent_connector() : _connector(NULL) {}
+
+  /**
+   *  Destructor.
+   */
+                  ~concurrent_connector() {
+    delete _connector;
+  }
+
+  /**
+   *  Thread entry point.
+   */
+  void            run() {
+    _connector = new tcp::connector;
+    _connector->connect_to("localhost", _port);
+    misc::shared_ptr<io::stream> s;
+    try {
+      while (!s.data())
+        s = _connector->open();
+    }
+    catch (std::exception const& e) {
+      std::cerr << e.what() << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    try {
+      while (1) {
+        misc::shared_ptr<io::data> d;
+        s->read(d);
+      }
+    }
+    catch (...) {}
+    return ;
+  }
+
+  /**
+   *  Set the port to connect on.
+   *
+   *  @param[in] port Port to connect on.
+   */
+  void            set_port(unsigned short port) {
+    _port = port;
+    return ;
+  }
+
+private:
+  tcp::connector* _connector;
+  unsigned short  _port;
 };
 
 /**
@@ -94,22 +165,43 @@ int main(int argc, char* argv[]) {
   // Initialization.
   config::applier::init();
 
+  // Random port.
+  unsigned short port(random_port());
+
   // Thread that will listen on a port.
-  concurrent c;
-  c.set_port(random_port());
-  QObject::connect(&c, SIGNAL(finished()), &app, SLOT(quit()));
-  QObject::connect(&c, SIGNAL(started()), &app, SLOT(quit()));
-  QObject::connect(&c, SIGNAL(terminated()), &app, SLOT(quit()));
-  c.start();
+  concurrent_acceptor accptr;
+  accptr.set_port(port);
+  QObject::connect(&accptr, SIGNAL(finished()), &app, SLOT(quit()));
+  QObject::connect(&accptr, SIGNAL(started()), &app, SLOT(quit()));
+  QObject::connect(&accptr, SIGNAL(terminated()), &app, SLOT(quit()));
+  accptr.start();
   app.exec();
 
   // Wait for thread to listen.
   QTimer::singleShot(500, &app, SLOT(quit()));
   app.exec();
 
-  // Concurrently close acceptor.
-  c.close();
+  // Launch connections.
+  concurrent_connector cnnctrs[5];
+  for (unsigned int i(0); i < sizeof(cnnctrs) / sizeof(*cnnctrs); ++i) {
+    cnnctrs[i].set_port(port);
+    cnnctrs[i].start();
+  }
 
-  // For for thread to finish.
-  return (!c.wait(2000));
+  // Wait for threads to connect.
+  QTimer::singleShot(500, &app, SLOT(quit()));
+
+  // Concurrently close acceptor.
+  should_exit = true;
+  accptr.close();
+
+  // For for connecting threads to finish.
+  int retval(0);
+  for (unsigned int i(0); i < sizeof(cnnctrs) / sizeof(*cnnctrs); ++i)
+    retval |= !cnnctrs[i].wait(1000);
+
+  // Wait for acceptor thread to finish.
+  retval |= !accptr.wait(2000);
+
+  return (retval);
 }
