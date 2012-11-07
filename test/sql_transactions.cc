61b1056efe3b006a9f1ab5b4eb8b7b78b26d3cda
Mathieu Kermagoret <mkermagoret@merethis.com>
2012-11-07 16:29:31 +0000

    Fix SQL transactions unit tests to clean DB after execution.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@1385 7c7d615a-03b3-410d-a316-1135b1bd9499

--- test/sql_transactions.cc
+++ test/sql_transactions.cc
@@ -157,7 +157,7 @@ int main() {
   // Cleanup.
   daemon.stop();
   config_remove(engine_config_path.c_str());
-  //config_db_close(DB_NAME);
+  config_db_close(DB_NAME);
   free_hosts(hosts);
   free_services(services);
 
