811f5cc3763a8f2403a83eee97b2303f08a7a592
Mathieu Kermagoret <mkermagoret@merethis.com>
2012-11-27 09:30:04 +0000

    Added missing files regarding splitted files.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@1402 7c7d615a-03b3-410d-a316-1135b1bd9499

---
+++ file/inc/com/centreon/broker/file/cfile.hh
@@ -0,0 +1,57 @@
+/*
+** Copyright 2012 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCB_FILE_CFILE_HH
+#  define CCB_FILE_CFILE_HH
+
+#  include <cstdio>
+#  include "com/centreon/broker/namespace.hh"
+
+CCB_BEGIN()
+
+namespace              file {
+  /**
+   *  @class cfile cfile.hh "com/centreon/broker/file/cfile.hh"
+   *  @brief Wrapper for C-style FILE streams.
+   *
+   *  Wrap calls that work on C FILE streams.
+   */
+  class                cfile {
+  public:
+                       cfile();
+                       ~cfile() throw ();
+    void               close() throw ();
+    void               open(char const* path, char const* mode);
+    unsigned long      read(void* buffer, unsigned long max_size);
+    void               seek(long offset, int whence = SEEK_SET);
+    long               tell();
+    unsigned long      write(void const* buffer, unsigned long size);
+
+  private:
+                       cfile(cfile const& right);
+    cfile&             operator=(cfile const& right);
+    void               _internal_copy(cfile const& right);
+
+    FILE*              _stream;
+  };
+}
+
+CCB_END()
+
+#endif // !CCB_FILE_CFILE_HH
---
+++ file/src/cfile.cc
@@ -0,0 +1,189 @@
+/*
+** Copyright 2012 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <cassert>
+#include <cerrno>
+#include <cstdlib>
+#include "com/centreon/broker/file/cfile.hh"
+#include "com/centreon/broker/exceptions/msg.hh"
+#include "com/centreon/broker/io/exceptions/shutdown.hh"
+
+using namespace com::centreon::broker::file;
+
+/**************************************
+*                                     *
+*           Public Methods            *
+*                                     *
+**************************************/
+
+/**
+ *  Default constructor.
+ */
+cfile::cfile() : _stream(NULL) {}
+
+/**
+ *  Destructor.
+ */
+cfile::~cfile() throw () {
+  close();
+}
+
+/**
+ *  Close file.
+ */
+void cfile::close() throw () {
+  if (_stream) {
+    fclose(_stream);
+    _stream = NULL;
+  }
+  return ;
+}
+
+/**
+ *  Open a file.
+ *
+ *  @param[in] path Path to the file.
+ *  @param[in] mode Open mode.
+ */
+void cfile::open(char const* path, char const* mode) {
+  close();
+  _stream = fopen(path, mode);
+  if (!_stream) {
+    char const* msg(strerror(errno));
+    throw (exceptions::msg() << "cannot open '" << path << "' (mode "
+           << mode << "): " << msg);
+  }
+  return ;
+}
+
+/**
+ *  Read data from file.
+ *
+ *  @param[out] buffer   Destination buffer.
+ *  @param[in]  max_size Maximum size in bytes to read.
+ *
+ *  @return Number of bytes written.
+ */
+unsigned long cfile::read(void* buffer, unsigned long max_size) {
+  size_t retval(fread(buffer, 1, max_size, _stream));
+  if (retval <= 0) {
+    if (feof(_stream))
+      throw (io::exceptions::shutdown(true, true));
+    else if ((EAGAIN == errno) || (EINTR == errno))
+      retval = 0;
+    else {
+      char const* msg(strerror(errno));
+      throw (exceptions::msg() << "error while reading file: " << msg);
+    }
+  }
+  return (retval);
+}
+
+/**
+ *  Seek to a specific position.
+ *
+ *  @param[in] offset Offset.
+ *  @param[in] whence Base position.
+ */
+void cfile::seek(long offset, int whence) {
+  int retval;
+  while ((retval = fseek(_stream, offset, whence))
+         && (EAGAIN == errno)
+         && (EINTR == errno))
+    ;
+  if (retval) {
+    char const* msg(strerror(errno));
+    throw (exceptions::msg() << "cannot seek in file to position ("
+           << whence << ", " << offset << "): " << msg);
+  }
+  return ;
+}
+
+/**
+ *  Return the current position in the file.
+ *
+ *  @return Current offset in the file.
+ */
+long cfile::tell() {
+  long retval(ftell(_stream));
+  if (-1 == retval) {
+    char const* msg(strerror(errno));
+    throw (exceptions::msg() << "cannot tell position in file: "
+           << msg);
+  }
+  return (retval);
+}
+
+/**
+ *  Write data to the file.
+ *
+ *  @param[in] buffer Data.
+ *  @param[in] size   Size of data.
+ *
+ *  @return Number of bytes written.
+ */
+unsigned long cfile::write(void const* buffer, unsigned long size) {
+  size_t retval(fwrite(buffer, 1, size, _stream));
+  if (ferror(_stream)) {
+    char const* msg(strerror(errno));
+    throw (exceptions::msg() << "cannot write " << size
+           << " bytes to file: " << msg);
+  }
+  return (retval);
+}
+
+/**************************************
+*                                     *
+*           Private Methods           *
+*                                     *
+**************************************/
+
+/**
+ *  Copy constructor.
+ *
+ *  @param[in] right Object to copy.
+ */
+cfile::cfile(cfile const& right) {
+  _internal_copy(right);
+}
+
+/**
+ *  Assignment operator.
+ *
+ *  @param[in] right Object to copy.
+ *
+ *  @return This object.
+ */
+cfile& cfile::operator=(cfile const& right) {
+  if (this != &right)
+    _internal_copy(right);
+  return (*this);
+}
+
+/**
+ *  Copy internal data members.
+ *
+ *  @param[in] right Object to copy.
+ */
+void cfile::_internal_copy(cfile const& right) {
+  (void)right;
+  assert(!"cfile is not copyable");
+  abort();
+  return ;
+}
