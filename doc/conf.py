d15af9729f8083ef59d0bde764259728c43e1426
Dorian Guillois <dguillois@merethis.com>
2012-12-19 15:44:22 +0000

    Add intersphinx documentation.
    git-svn-id: http://svn.modules.centreon.com/centreon-broker/trunk@1464 7c7d615a-03b3-410d-a316-1135b1bd9499

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,11 @@ on_centreon_rtd = os.environ.get('CENTREON_RTD', None) == 'True'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinx.ext.intersphinx']
+
+intersphinx_mapping = {
+  'centreon-engine' : ('http://doc-dev.merethis.net/docs/centreon-engine/en/latest', None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
