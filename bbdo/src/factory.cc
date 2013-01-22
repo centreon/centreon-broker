4e50475150dd779f679416b08e9d1ec41617fb69
Matthieu Kermagoret <mkermagoret@merethis.com>
2013-01-22 17:27:07 +0100

    BBDO: added protocol factory. This refs #4121.

---
+++ bbdo/src/factory.cc
@@ -0,0 +1,113 @@
+/*
+** Copyright 2013 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include "com/centreon/broker/bbdo/factory.hh"
+
+using namespace com::centreon::broker;
+using namespace com::centreon::broker::bbdo;
+
+/**************************************
+*                                     *
+*           Public Methods            *
+*                                     *
+**************************************/
+
+/**
+ *  Default constructor.
+ */
+factory::factory() {}
+
+/**
+ *  Copy constructor.
+ *
+ *  @param[in] right Object to copy.
+ */
+factory::factory(factory const& right) : io::factory(right) {}
+
+/**
+ *  Destructor.
+ */
+factory::~factory() {}
+
+/**
+ *  Assignment operator.
+ *
+ *  @param[in] right Object to copy.
+ *
+ *  @return This object.
+ */
+factory& factory::operator=(factory const& right) {
+  io::factory::operator=(right);
+  return (*this);
+}
+
+/**
+ *  Clone the factory object.
+ *
+ *  @return Clone of this factory.
+ */
+io::factory* factory::clone() const {
+  return (new factory(*this));
+}
+
+/**
+ *  @brief Check if a configuration supports this protocol.
+ *
+ *  The endpoint 'protocol' tag must have the 'bbdo' value.
+ *
+ *  @param[in] cfg       Object configuration.
+ *  @param[in] is_input  true if endpoint must act as event source.
+ *  @param[in] is_output true if endpoint must act as event destination.
+ *
+ *  @return true if the configuration has this protocol.
+ */
+bool factory::has_endpoint(
+                config::endpoint const& cfg,
+                bool is_input,
+                bool is_output) const {
+  (void)is_input;
+  (void)is_output;
+  QMap<QString, QString>::const_iterator it(cfg.params.find("protocol"));
+  return ((it != cfg.params.end()) && (it.value() == "bbdo"));
+}
+
+/**
+ *  Create a new endpoint from a configuration.
+ *
+ *  @param[in]  cfg         Endpoint configuration.
+ *  @param[in]  is_input    true if endpoint must act as event source.
+ *  @param[in]  is_output   true if endpoint must act as event
+ *                          destination.
+ *  @param[in]  temporary   Temporary stream to write data when memory
+ *                          queue is full.
+ *  @param[out] is_acceptor Set to true if the endpoint is an acceptor.
+ *
+ *  @return Endpoint matching configuration.
+ */
+io::endpoint* factory::new_endpoint(
+                         config::endpoint& cfg,
+                         bool is_input,
+                         bool is_output,
+                         io::endpoint const* temporary,
+                         bool& is_acceptor) const {
+  (void)cfg;
+  io::endpoint* retval(NULL);
+  // XXX
+  return (retval);
+}
