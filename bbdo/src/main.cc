65ec22002969d3f84f8423db18a7b7735c1e4b84
Matthieu Kermagoret <mkermagoret@merethis.com>
2013-01-22 16:01:37 +0100

    BBDO: added empty module to the build process. This refs #4121.

---
+++ bbdo/src/main.cc
@@ -0,0 +1,56 @@
+/*
+** Copyright 2013 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include "com/centreon/broker/io/protocols.hh"
+
+using namespace com::centreon::broker;
+
+// Load count.
+static unsigned int instances(0);
+
+extern "C" {
+  /**
+   *  Module deinitialization routine.
+   */
+  void broker_module_deinit() {
+    // Decrement instance number.
+    if (!--instances)
+      // Deregister BBDO protocol.
+      io::protocols::instance().unreg("BBDO");
+    return ;
+  }
+
+  /**
+   *  Module initialization routine.
+   *
+   *  @param[in] arg Configuration object.
+   */
+  void broker_module_init(void const* arg) {
+    (void)arg;
+
+    // Increment instance number.
+    if (!instances++) {
+      // Register BBDO protocol.
+      // XXX
+      // Initialize mappings.
+      // XXX
+    }
+    return ;
+  }
+}
