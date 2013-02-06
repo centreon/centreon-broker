6535cf4398d67afab44426a8a43ae35459c844a0
Matthieu Kermagoret <mkermagoret@merethis.com>
2013-02-06 09:47:08 +0100

    TLS: working on new module. This refs #4181.

---
+++ tls/src/main.cc
@@ -0,0 +1,60 @@
+/*
+** Copyright 2013 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include "com/centreon/broker/io/protocols.hh"
+//#include "com/centreon/broker/tls/factory.hh"
+
+using namespace com::centreon::broker;
+
+// Load count.
+static unsigned int instances(0);
+
+extern "C" {
+  /**
+   *  Module deinitialization routine.
+   */
+  void broker_module_deinit() {
+    // Decrement instance number.
+    if (!--instances)
+      // Unregister TLS layer.
+      io::protocols::instance().unreg("TLS");
+    return ;
+  }
+
+  /**
+   *  Module initialization routine.
+   *
+   *  @param[in] arg Configuration object.
+   */
+  void broker_module_init(void const* arg) {
+    (void)arg;
+
+    // Increment instance number.
+    if (!instances++)
+      // Register TLS layer.
+      /*io::protocols::instance().reg(
+                                    "TLS",
+                                    tls::factory(),
+                                    5,
+                                    5);
+      */
+      ;
+    return ;
+  }
+}
