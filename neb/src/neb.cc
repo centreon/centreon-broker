8b036dcbd7e96798091828a07f8c0ddc1377e5e1
Matthieu Kermagoret <mkermagoret@merethis.com>
2013-03-13 08:58:45 +0100

    cbmod: Broker stalled when Nagios in daemon mode.
    This commit refs #4253, as this bug was introduced when attempting
    to get INITIAL logs. The fix was to initialize Broker a bit earlier
    during the monitoring engine initialization (on
    NEBTYPE_PROCESS_START instead of NEBTYPE_PROCESS_EVENTLOOPSTART).
    However Nagios daemonization takes place between these two events.
    Most notably, Nagios fork()s and therefore Centreon Broker threads
    get deactivated.
    
    This commit restore the initialization to
    NEBTYPE_PROCESS_EVENTLOOPSTART (after the potential daemonization)
    and fixes log callback registration to get INITIAL logs still. This
    fixes #4301.

--- neb/src/neb.cc
+++ neb/src/neb.cc
@@ -1,5 +1,5 @@
 /*
-** Copyright 2009-2012 Merethis
+** Copyright 2009-2013 Merethis
 **
 ** This file is part of Centreon Broker.
 **
@@ -111,6 +111,9 @@ extern "C" {
       // Unregister callbacks.
       neb::unregister_callbacks();
 
+      // Clean objects.
+      neb::gl_acknowledgements.clear();
+
       // Unload singletons.
       broker::config::applier::deinit();
 
@@ -157,151 +160,171 @@ extern "C" {
    *  @return 0 on success, any other value on failure.
    */
   int nebmodule_init(int flags, char const* args, void* handle) {
-    // Initialization.
-    broker::config::applier::init();
-
-    // Save module handle and flags for future use.
-    neb::gl_mod_flags = flags;
-    neb::gl_mod_handle = handle;
-
-    // Set module informations.
-    neb_set_module_info(
-      neb::gl_mod_handle,
-      NEBMODULE_MODINFO_TITLE,
-      "Centreon Broker's cbmod");
-    neb_set_module_info(
-      neb::gl_mod_handle,
-      NEBMODULE_MODINFO_AUTHOR,
-      "Merethis");
-    neb_set_module_info(
-      neb::gl_mod_handle,
-      NEBMODULE_MODINFO_COPYRIGHT,
-      "Copyright 2009-2012 Merethis");
-    neb_set_module_info(
-      neb::gl_mod_handle,
-      NEBMODULE_MODINFO_VERSION,
-      CENTREON_BROKER_VERSION);
-    neb_set_module_info(
-      neb::gl_mod_handle,
-      NEBMODULE_MODINFO_LICENSE,
-      "GPL version 2");
-    neb_set_module_info(
-      neb::gl_mod_handle,
-      NEBMODULE_MODINFO_DESC,
-      "cbmod is part of CentreonBroker and is designed to " \
-      "convert internal Nagios events to a proper data "    \
-      "stream that can then be parsed by CentreonBroker's " \
-      "cbd.");
-
-    // Initialize Qt if not already done by parent process.
-    if (!QCoreApplication::instance()) {
-      gl_initialized_qt = true;
-      new QCoreApplication(gl_qt_argc, (char**)gl_qt_argv);
-      signal(SIGCHLD, SIG_DFL);
-    }
-
-    // Reset locale.
-    setlocale(LC_NUMERIC, "C");
-
-    // Disable timestamp printing in logs (cause starvation when forking).
-    logging::file::with_timestamp(false);
-
-    // Default logging object.
-    neb::monitoring_logger monlog;
-
     try {
+      // Initialization.
+      broker::config::applier::init();
+
+      // Save module handle and flags for future use.
+      neb::gl_mod_flags = flags;
+      neb::gl_mod_handle = handle;
+
+      // Set module informations.
+      neb_set_module_info(
+        neb::gl_mod_handle,
+        NEBMODULE_MODINFO_TITLE,
+        "Centreon Broker's cbmod");
+      neb_set_module_info(
+        neb::gl_mod_handle,
+        NEBMODULE_MODINFO_AUTHOR,
+        "Merethis");
+      neb_set_module_info(
+        neb::gl_mod_handle,
+        NEBMODULE_MODINFO_COPYRIGHT,
+        "Copyright 2009-2013 Merethis");
+      neb_set_module_info(
+        neb::gl_mod_handle,
+        NEBMODULE_MODINFO_VERSION,
+        CENTREON_BROKER_VERSION);
+      neb_set_module_info(
+        neb::gl_mod_handle,
+        NEBMODULE_MODINFO_LICENSE,
+        "GPL version 2");
+      neb_set_module_info(
+        neb::gl_mod_handle,
+        NEBMODULE_MODINFO_DESC,
+        "cbmod is part of Centreon Broker and is designed to "    \
+        "convert internal Centreon Engine / Nagios events to a "  \
+        "proper data stream that can then be parsed by Centreon " \
+        "Broker's cbd.");
+
+      // Initialize Qt if not already done by parent process.
+      if (!QCoreApplication::instance()) {
+        gl_initialized_qt = true;
+        new QCoreApplication(gl_qt_argc, (char**)gl_qt_argv);
+        signal(SIGCHLD, SIG_DFL);
+      }
+
+      // Reset locale.
+      setlocale(LC_NUMERIC, "C");
+
+      // Disable timestamp printing in logs (cause starvation when forking).
+      logging::file::with_timestamp(false);
+
       // Default logging object.
-      {
-        // Debug ?
-        bool debug;
-        char const* dbg_flag("-d ");
-        if (args && !strncmp(args, dbg_flag, strlen(dbg_flag))) {
-          debug = true;
-          args += strlen(dbg_flag);
+      neb::monitoring_logger monlog;
+
+      try {
+        // Default logging object.
+        {
+          // Debug ?
+          bool debug;
+          char const* dbg_flag("-d ");
+          if (args && !strncmp(args, dbg_flag, strlen(dbg_flag))) {
+            debug = true;
+            args += strlen(dbg_flag);
+          }
+          else
+            debug = false;
+
+          // Add log.
+          logging::manager::instance().log_on(
+                                         monlog,
+                                         (debug
+                                          ? logging::config_type
+                                          | logging::debug_type
+                                          | logging::error_type
+                                          | logging::info_type
+                                          : logging::config_type
+                                          | logging::error_type
+                                          | logging::info_type),
+                                         (debug ?
+                                          logging::low
+                                          : logging::high));
+        }
+
+        // Set configuration file.
+        if (args) {
+          char const* config_file("config_file=");
+          size_t config_file_size(strlen(config_file));
+          if (!strncmp(args, config_file, config_file_size))
+            args += config_file_size;
+          neb::gl_configuration_file = args;
         }
         else
-          debug = false;
-
-        // Add log.
-        logging::manager::instance().log_on(
-                                       monlog,
-                                       (debug
-                                        ? logging::config_type
-                                        | logging::debug_type
-                                        | logging::error_type
-                                        | logging::info_type
-                                        : logging::config_type
-                                        | logging::error_type
-                                        | logging::info_type),
-                                       (debug ?
-                                        logging::low
-                                        : logging::high));
-      }
-
-      // Set configuration file.
-      if (args) {
-        char const* config_file("config_file=");
-        size_t config_file_size(strlen(config_file));
-        if (!strncmp(args, config_file, config_file_size))
-          args += config_file_size;
-        neb::gl_configuration_file = args;
-      }
-      else
-        throw (exceptions::msg()
+          throw (exceptions::msg()
                  << "main: no configuration file provided");
 
-      // Try configuration parsing.
-      broker::config::parser p;
-      broker::config::state s;
-      p.parse(neb::gl_configuration_file, s);
+        // Try configuration parsing.
+        broker::config::parser p;
+        broker::config::state s;
+        p.parse(neb::gl_configuration_file, s);
 
-      // Apply loggers.
-      broker::config::applier::logger::instance().apply(s.loggers());
+        // Apply loggers.
+        broker::config::applier::logger::instance().apply(s.loggers());
 
-      // Remove monitoring log.
+        // Remove monitoring log.
+        logging::manager::instance().log_on(monlog, 0);
+      }
+      catch (std::exception const& e) {
+        logging::error(logging::high) << e.what();
+        logging::manager::instance().log_on(monlog, 0);
+        return (-1);
+      }
+      catch (...) {
+        logging::error(logging::high)
+          << "main: configuration file parsing failed";
+        logging::manager::instance().log_on(monlog, 0);
+        return (-1);
+      }
+
+      // Remove old monitoring object.
       logging::manager::instance().log_on(monlog, 0);
+
+      // Register process and log callback.
+      neb::gl_registered_callbacks.push_back(
+             misc::shared_ptr<neb::callback>(
+               new neb::callback(
+                          NEBCALLBACK_PROCESS_DATA,
+                          neb::gl_mod_handle,
+                          &neb::callback_process)));
+      neb::gl_registered_callbacks.push_back(
+             misc::shared_ptr<neb::callback>(
+               new neb::callback(
+                          NEBCALLBACK_LOG_DATA,
+                          neb::gl_mod_handle,
+                          &neb::callback_log)));
+
+      // Process Qt events if necessary.
+      if (gl_initialized_qt) {
+        union {
+          void (* code)(void*);
+          void*   data;
+        } val;
+        val.code = &process_qcore;
+        schedule_new_event(
+          99,
+          1,
+          time(NULL) + 1,
+          1,
+          1,
+          NULL,
+          1,
+          val.data,
+          NULL,
+          0);
+      }
     }
     catch (std::exception const& e) {
-      logging::error(logging::high) << e.what();
-      logging::manager::instance().log_on(monlog, 0);
+      logging::error(logging::high)
+        << "main: cbmod loading failed: " << e.what();
+      nebmodule_deinit(0, 0);
       return (-1);
     }
     catch (...) {
       logging::error(logging::high)
-        << "main: configuration file parsing failed";
-      logging::manager::instance().log_on(monlog, 0);
+        << "main: cbmod loading failed due to an unknown exception";
+      nebmodule_deinit(0, 0);
       return (-1);
-    }
-
-    // Remove old monitoring object.
-    logging::manager::instance().log_on(monlog, 0);
-
-    // Register process callback.
-    if (neb_register_callback(
-          NEBCALLBACK_PROCESS_DATA,
-          neb::gl_mod_handle,
-          0,
-          &neb::callback_process) != OK)
-      return (-1);
-
-    // Process Qt events if necessary.
-    if (gl_initialized_qt) {
-      union {
-        void (* code)(void*);
-        void*   data;
-      } val;
-      val.code = &process_qcore;
-      schedule_new_event(
-        99,
-        1,
-        time(NULL) + 1,
-        1,
-        1,
-        NULL,
-        1,
-        val.data,
-        NULL,
-        0);
     }
 
     return (0);
