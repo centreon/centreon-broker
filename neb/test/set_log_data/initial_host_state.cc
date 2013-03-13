048dc41e4ff465e349668e2031737969a52e7072
Matthieu Kermagoret <mkermagoret@merethis.com>
2013-03-13 09:39:34 +0100

    NEB: improve initial host state log parsing test.

--- neb/test/set_log_data/initial_host_state.cc
+++ neb/test/set_log_data/initial_host_state.cc
@@ -1,5 +1,6 @@
 /*
-** Copyright 2011 Merethis
+** Copyright 2011-2013 Merethis
+**
 ** This file is part of Centreon Broker.
 **
 ** Centreon Broker is free software: you can redistribute it and/or
@@ -16,6 +17,9 @@
 ** <http://www.gnu.org/licenses/>.
 */
 
+#include <cstdlib>
+#include <iostream>
+#include "com/centreon/broker/exceptions/msg.hh"
 #include "com/centreon/broker/neb/log_entry.hh"
 #include "com/centreon/broker/neb/set_log_data.hh"
 
@@ -24,21 +28,51 @@ using namespace com::centreon::broker;
 /**
  *  Check that a initial host state log is properly parsed.
  *
- *  @return 0 on success.
+ *  @return EXIT_SUCCESS on success.
  */
 int main() {
-  // Log entry.
-  neb::log_entry le;
+  int retval(EXIT_FAILURE);
 
-  // Parse a initial host state line.
-  neb::set_log_data(le,
-    "INITIAL HOST STATE: myserver;UP;HARD;1;PING OK");
-
-  // Check that it was properly parsed.
-  return ((le.host_name != "myserver")
+  try {
+    // #1
+    {
+      neb::log_entry le;
+      neb::set_log_data(
+        le,
+        "INITIAL HOST STATE: myserver;UP;HARD;1;PING OK");
+      if ((le.host_name != "myserver")
           || (le.log_type != 1) // HARD
           || (le.msg_type != 9) // INITIAL HOST STATE
           || (le.output != "PING OK")
           || (le.retry != 1)
-          || (le.status != 0)); // UP
+          || (le.status != 0)) // UP
+        throw (exceptions::msg() << "log entry #1 mismatch");
+    }
+
+    // #2
+    {
+      neb::log_entry le;
+      neb::set_log_data(
+        le,
+        "INITIAL HOST STATE: SERVER007;UNKNOWN;SOFT;2;");
+      if ((le.host_name != "SERVER007")
+          || (le.log_type != 0) // SOFT
+          || (le.msg_type != 9) // INITIAL HOST STATE
+          || (le.output != "")
+          || (le.retry != 2)
+          || (le.status != 3)) // UNKNOWN
+        throw (exceptions::msg() << "log entry #2 mismatch");
+    }
+
+    // Success.
+    retval = EXIT_SUCCESS;
+  }
+  catch (std::exception const& e) {
+    std::cout << e.what() << std::endl;
+  }
+  catch (...) {
+    std::cout << "unknown exception" << std::endl;
+  }
+
+  return (retval);
 }
