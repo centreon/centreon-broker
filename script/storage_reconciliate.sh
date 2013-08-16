1bc2d75dc6e0fdb29a33305ff18a1cf30283206c
Matthieu Kermagoret <mkermagoret@merethis.com>
2013-08-16 11:03:08 +0200

    Updated storage reconciliation script.

--- script/storage_reconciliate.sh
+++ script/storage_reconciliate.sh
@@ -7,6 +7,8 @@ dbuser=
 dbpassword=
 dbname=
 execute=0
+clean=0
+rebuild=0
 tmpfile="/tmp/storage_reconciliate_metrics.tmp"
 
 # Browse arguments.
@@ -27,6 +29,10 @@ while [ $# -ge 1 ] ; do
     dbname="$1"
   elif [ x"$1" = x"--execute" ] ; then
     execute=1
+  elif [ x"$1" = x"--clean" ] ; then
+    clean=1
+  elif [ x"$1" = x"--rebuild" ] ; then
+    rebuild=1
   fi
   shift
 done
@@ -48,14 +54,18 @@ if [ \( x"$help" = x1 \) \
   echo "  --dbname <name>           DB name."
   echo "  --execute                 Run SQL queries on the DB. If not specified"
   echo "                            queries are only printed."
+  echo "  --clean                   Delete old entries from the metrics table."
+  echo "  --rebuild                 Rebuild graphs of metrics that were"
+  echo "                            reconciliated."
   echo ""
   exit 0
 fi
 
 # Get full metric list.
-mysql -B -N -h "$dbhost" -u "$dbuser" -p"$dbpassword" -e "SELECT index_id, metric_id, metric_name FROM metrics" "$dbname" > "$tmpfile"
+mysql -B -N -h "$dbhost" -u "$dbuser" -p"$dbpassword" -e "SELECT index_id, metric_id, metric_name FROM metrics ORDER BY index_id" "$dbname" > "$tmpfile"
 
 # Browse all metrics.
+lastindexid=
 while read line ; do
   # Split components.
   indexid=`echo "$line" | cut -f 1`
@@ -63,17 +73,36 @@ while read line ; do
   metricname=`echo "$line" | cut -f 3-`
 
   # Modify metric name.
-  oldmetricname=`echo "$metricname" | sed 's|/|#S#|g' | sed 's|\\\\|#BS#|g' | sed 's|%|#P#|g' | sed 's|[^0-9_a-zA-Z]|-|g'`
+  oldmetricname=`echo "$metricname" | sed -e 's|/|#S#|g' -e 's|\\\\|#BS#|g' -e 's|%|#P#|g' -e 's|[^0-9_a-zA-Z#]|-|g'`
 
   # Process if metric name is modified.
   if [ x"$metricname" != x"$oldmetricname" ] ; then
-    oldmetricid=`mysql -B -N -h "$dbhost" -u "$dbuser" -p"$dbpassword" -e "SELECT metric_id FROM metrics WHERE index_id=$indexid AND metric_name='$oldmetricname'" "$dbname" 2> /dev/null`
+    oldmetricid=`mysql -B -N -h "$dbhost" -u "$dbuser" -p"$dbpassword" -e "SELECT metric_id FROM metrics WHERE index_id=$indexid AND metric_name='$oldmetricname'" "$dbname"`
     if [ x"$oldmetricid" != x ] ; then
       echo "(index $indexid, metric $oldmetricid, name $oldmetricname)  =>  (index $indexid, metric $metricid, name $metricname)"
+
+      # Move data entries to the new metric.
       if [ x"$execute" = x1 ] ; then
-        #mysql -B -N -h "$dbhost" -u "$dbuser" -p"$dbpassword" -e "UPDATE data_bin SET id_metric=$metricid WHERE id_metric=$oldmetricid" "$dbname"
+        mysql -B -N -h "$dbhost" -u "$dbuser" -p"$dbpassword" -e "UPDATE data_bin SET id_metric=$metricid WHERE id_metric=$oldmetricid" "$dbname"
       else
         echo "  UPDATE data_bin SET id_metric=$metricid WHERE id_metric=$oldmetricid"
+      fi
+
+      # Remove the old metric.
+      if [ x"$clean" = x1 ] ; then
+        mysql -B -N -h "$dbhost" -u "$dbuser" -p"$dbpassword" -e "DELETE FROM metrics WHERE metric_id=$oldmetricid" "$dbname"
+      else
+        echo "  DELETE FROM metrics WHERE metric_id=$oldmetricid"
+      fi
+
+      # Rebuild graphs.
+      if [ x"$indexid" != x"$lastindexid" ] ; then
+        if [ x"$rebuild" = x1 ] ; then
+          mysql -B -N -h "$dbhost" -u "$dbuser" -p"$dbpassword" -e "UPDATE index_data SET must_be_rebuild=1 WHERE index_id=$indexid" "$dbname"
+        else
+          echo "UPDATE index_data SET must_be_rebuild=1 WHERE index_id=$indexid"
+        fi
+        lastindexid="$indexid"
       fi
     fi
   fi
