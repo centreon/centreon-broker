a30455506f5d50f4a095ac05268bd2be03f12b23
Dorian Guillois <dguillois@merethis.com>
2013-10-09 17:13:46 +0200

    Add live filtering unit test.

---
+++ test/filtering_to_sql.cc
@@ -0,0 +1,131 @@
+/*
+** Copyright 2012-2013 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QVariant>
+#include <sstream>
+#include "com/centreon/broker/exceptions/msg.hh"
+#include "test/config.hh"
+#include "test/engine.hh"
+#include "test/external_command.hh"
+#include "test/generate.hh"
+#include "test/misc.hh"
+#include "test/vars.hh"
+
+using namespace com::centreon::broker;
+
+#define DB_NAME "broker_filtering_to_sql"
+
+/**
+ *  Check that filtering properly works.
+ *
+ *  @return EXIT_SUCCESS on success.
+ */
+int main() {
+  // Return value.
+  int retval(EXIT_FAILURE);
+
+  // Variables that need cleaning.
+  std::list<host> hosts;
+  std::list<service> services;
+  std::string engine_config_path(tmpnam(NULL));
+  external_command commander;
+  engine daemon;
+
+  try {
+    // Prepare database.
+    QSqlDatabase db(config_db_open(DB_NAME));
+
+    // Prepare monitoring engine configuration parameters.
+    generate_hosts(hosts, 10);
+    generate_services(services, hosts, 5);
+    commander.set_file(tmpnam(NULL));
+    std::string additional_config;
+    {
+      std::ostringstream oss;
+      oss << commander.get_engine_config()
+          << "broker_module=" << CBMOD_PATH << " "
+          << PROJECT_SOURCE_DIR << "/test/cfg/filtering_to_sql.xml\n";
+      additional_config = oss.str();
+    }
+
+    // Generate monitoring engine configuration files.
+    config_write(
+      engine_config_path.c_str(),
+      additional_config.c_str(),
+      &hosts,
+      &services);
+
+    // Start monitoring engine.
+    std::string engine_config_file(engine_config_path);
+    engine_config_file.append("/nagios.cfg");
+    daemon.set_config_file(engine_config_file);
+    daemon.start();
+    sleep_for(10 * MONITORING_ENGINE_INTERVAL_LENGTH);
+
+    // Check 'instances' table.
+    {
+      std::ostringstream query;
+      query << "SELECT * FROM instances";
+      QSqlQuery q(db);
+      if (!q.exec(query.str().c_str()))
+        throw (exceptions::msg() << "cannot read instances from DB: "
+               << q.lastError().text().toStdString().c_str());
+      if (q.next())
+        throw (exceptions::msg() << "'instances' contains entries "
+               << "whereas they should have been filtered");
+    }
+
+    // Check 'logs' table.
+    {
+      std::ostringstream query;
+      query << "SELECT * FROM logs";
+      QSqlQuery q(db);
+      if (!q.exec(query.str().c_str()))
+        throw (exceptions::msg() << "cannot read logs from DB: "
+               << q.lastError().text().toStdString().c_str());
+      if (q.next())
+        throw (exceptions::msg() << "'logs' contains entries "
+               << "whereas they should have been filtered");
+    }
+
+    // Success.
+    retval = EXIT_SUCCESS;
+  }
+  catch (std::exception const& e) {
+    std::cerr << e.what() << std::endl;
+  }
+  catch (...) {
+    std::cerr << "unknown exception" << std::endl;
+  }
+
+  // Cleanup.
+  daemon.stop();
+  config_remove(engine_config_path.c_str());
+  config_db_close(DB_NAME);
+  free_hosts(hosts);
+  free_services(services);
+
+  return (retval);
+}
