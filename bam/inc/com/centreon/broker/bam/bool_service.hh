2dcaafd3df482a2945d83a6db0fa61b3f50304a9
Matthieu Kermagoret <mkermagoret@merethis.com>
2014-03-10 10:10:45 +0100

    BAM: add the bool_service class.

---
+++ bam/inc/com/centreon/broker/bam/bool_service.hh
@@ -0,0 +1,69 @@
+/*
+** Copyright 2014 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCB_BAM_BOOL_SERVICE_HH
+#  define CCB_BAM_BOOL_SERVICE_HH
+
+#  include "com/centreon/broker/bam/bool_value.hh"
+#  include "com/centreon/broker/bam/service_listener.hh"
+#  include "com/centreon/broker/namespace.hh"
+
+CCB_BEGIN()
+
+namespace         bam {
+  /**
+   *  @class bool_service bool_service.hh "com/centreon/broker/bam/bool_service.hh"
+   *  @brief Evaluation of a service state.
+   *
+   *  This class compares the state of a service to compute a boolean
+   *  value.
+   */
+  class           bool_service : public bool_value,
+                                 public service_listener {
+  public:
+                  bool_service();
+                  bool_service(bool_service const& right);
+                  ~bool_service();
+    bool_service& operator=(bool_service const& right);
+    void          child_has_update(
+                    misc::shared_ptr<computable>& child);
+    void          set_expected_state(short expected);
+    void          set_host_id(unsigned int host_id);
+    void          set_service_id(unsigned int service_id);
+    void          set_value_if_state_match(bool value);
+    void          service_update(
+                    misc::shared_ptr<neb::service_status> const& status);
+    bool          value_hard();
+    bool          value_soft();
+
+  private:
+    void          _internal_copy(bool_service const& right);
+
+    unsigned int  _host_id;
+    unsigned int  _service_id;
+    short         _state_expected;
+    short         _state_hard;
+    short         _state_soft;
+    bool          _value_if_state_match;
+  };
+}
+
+CCB_END()
+
+#endif // !CCB_BAM_BOOL_SERVICE_HH
