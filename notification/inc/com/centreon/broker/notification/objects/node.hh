69c66a45ad9b6312f9c5ce7ce9829616038d279d
Alexandre Fouillé <afouille@merethis.com>
2014-08-11 16:37:42 +0200

    Node object.

--- notification/inc/com/centreon/broker/notification/objects/node.hh
+++ notification/inc/com/centreon/broker/notification/objects/node.hh
@@ -20,6 +20,7 @@
 #ifndef CCB_NOTIFICATION_NODE_HH
 #  define CCB_NOTIFICATION_NODE_HH
 
+#  include <ctime>
 #  include "com/centreon/broker/namespace.hh"
 
 CCB_BEGIN()
@@ -27,7 +28,25 @@ CCB_BEGIN()
 namespace        notification {
 
   class         node {
+  public:
+    node();
+    node(node const& obj);
+    node& operator=(node const& obj);
 
+    int       get_notification_number() const throw();
+    void      set_notification_number(int value);
+    time_t    get_last_notification_time() const throw();
+    void      set_last_notification_time(time_t value);
+    short     get_hard_state() const throw();
+    void      set_hard_state(short value);
+    short     get_soft_state() const throw();
+    void      set_soft_state(short value);
+
+  private:
+    int       _notification_number;
+    time_t    _last_notification_time;
+    short     _hard_state;
+    short     _soft_state;
   };
 
 }
--- notification/src/objects/node.cc
+++ notification/src/objects/node.cc
@@ -20,3 +20,57 @@
 #include "com/centreon/broker/notification/objects/node.hh"
 
 using namespace com::centreon::broker::notification;
+
+node::node() :
+  _notification_number(0),
+  _last_notification_time(),
+  _hard_state(0),
+  _soft_state(0) {}
+
+node::node(node const& obj) :
+  _notification_number(obj._notification_number),
+  _last_notification_time(obj._last_notification_time),
+  _hard_state(obj._hard_state),
+  _soft_state(obj._soft_state) {}
+
+node& node::operator=(node const& obj) {
+  if (this != &obj) {
+    _notification_number = obj._notification_number;
+    _last_notification_time = obj._last_notification_time;
+    _hard_state = obj._hard_state;
+    _soft_state = obj._soft_state;
+  }
+  return *this;
+}
+
+int node::get_notification_number() const throw() {
+  return (_notification_number);
+}
+
+void node::set_notification_number(int value) {
+  _notification_number = value;
+}
+
+time_t node::get_last_notification_time() const throw() {
+  return (_last_notification_time);
+}
+
+void node::set_last_notification_time(time_t value) {
+  _last_notification_time = value;
+}
+
+short node::get_hard_state() const throw() {
+  return (_hard_state);
+}
+
+void node::set_hard_state(short value) {
+  _hard_state = value;
+}
+
+short node::get_soft_state() const throw() {
+  return (_soft_state);
+}
+
+void node::set_soft_state(short value) {
+  _soft_state = value;
+}
