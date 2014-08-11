0fca117b74cdab14141e387da591f518f12ab60e
Alexandre Fouillé <afouille@merethis.com>
2014-08-11 14:19:15 +0200

    Dummy notification objects.

---
+++ notification/inc/com/centreon/broker/notification/objects/timeperiod.hh
@@ -0,0 +1,37 @@
+/*
+** Copyright 2011-2013 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCB_NOTIFICATION_TIMEPERIOD_HH
+#  define CCB_NOTIFICATION_TIMEPERIOD_HH
+
+#  include "com/centreon/broker/namespace.hh"
+
+CCB_BEGIN()
+
+namespace       notification {
+
+  class         timeperiod {
+
+  };
+
+}
+
+CCB_END()
+
+#endif // !CCB_NOTIFICATION_TIMEPERIOD_HH
