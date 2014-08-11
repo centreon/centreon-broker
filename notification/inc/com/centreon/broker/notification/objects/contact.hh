718add0713296e738a459a3fead4ad93fff3359f
Alexandre Fouillé <afouille@merethis.com>
2014-08-11 17:53:06 +0200

    Contact object (wip).

--- notification/inc/com/centreon/broker/notification/objects/contact.hh
+++ notification/inc/com/centreon/broker/notification/objects/contact.hh
@@ -20,6 +20,9 @@
 #ifndef CCB_NOTIFICATION_CONTACT_HH
 #  define CCB_NOTIFICATION_CONTACT_HH
 
+#  include <map>
+#  include <vector>
+#  include <string>
 #  include "com/centreon/broker/namespace.hh"
 
 CCB_BEGIN()
@@ -27,7 +30,36 @@ CCB_BEGIN()
 namespace       notification {
 
   class         contact {
+  public:
+    contact();
+    contact(contact const& obj);
+    contact& operator=(contact const& obj);
 
+    std::vector<std::string>& get_address() const throw();
+    void                     add_address(std::string&);
+
+
+
+  private:
+    std::vector<std::string> _address;
+    std::string              _alias;
+    bool                     _can_submit_commands;
+    group                    _contactgroups;
+    std::string              _contact_name;
+    std::map<std::string, std::string>
+                             _customvariables;
+    std::string              _email;
+    bool                     _host_notifications_enabled;
+    group                    _host_notification_commands;
+    unsigned int             _host_notification_options;
+    std::string              _host_notification_period;
+    bool                     _retain_nonstatus_information;
+    bool                     _retain_status_information;
+    std::string              _pager;
+    group                    _service_notification_commands;
+    unsigned int             _service_notification_options;
+    std::string              _service_notification_period;
+    bool                     _service_notifications_enabled;
   };
 
 }
--- notification/src/objects/contact.cc
+++ notification/src/objects/contact.cc
@@ -20,3 +20,48 @@
 #include "com/centreon/broker/notification/objects/contact.hh"
 
 using namespace com::centreon::broker::notification;
+
+contact::contact() {}
+
+contact::contact(contact const& obj) :
+  _address(obj._address),
+  _alias(obj._alias),
+  _can_submit_commands(obj._can_submit_commands),
+  _contact_name(obj._contact_name),
+  _customvariables(obj._custombariables),
+  _email(obj._email),
+  _host_notifications_enabled(obj._host_notifications_enabled),
+  _host_notification_commands(obj._host_notification_commands),
+  _host_notification_options(obj._host_notification_options),
+  _host_notification_period(obj._host_notification_period),
+  _retain_nonstatus_information(obj._retain_nonstatus_information),
+  _retain_status_information(obj._retain_status_information),
+  _pager(obj._pager),
+  _service_notification_commands(obj._service_notification_commands),
+  _service_notification_options(obj._service_notification_options),
+  _service_notification_period(obj._service_notification_period),
+  _service_notifications_enabled(obj._service_notifications_enabled) {}
+
+contact& contact::operator=(contact const& obj) {
+  if (this != &obj) {
+    _address =  obj._address;
+    _alias = obj._alias;
+    _can_submit_commands = obj._can_submit_commands;
+    _contact_name = obj._contact_name;
+    _customvariables = obj._custombariables;
+    _email = obj._email;
+    _host_notifications_enabled = obj._host_notifications_enabled;
+    _host_notification_commands = obj._host_notification_commands;
+    _host_notification_options = obj._host_notification_options;
+    _host_notification_period = obj._host_notification_period;
+    _retain_nonstatus_information = obj._retain_nonstatus_information;
+    _retain_status_information = obj._retain_status_information;
+    _pager = obj._pager;
+    _service_notification_commands = obj._service_notification_commands;
+    _service_notification_options = obj._service_notification_options;
+    _service_notification_period = obj._service_notification_period;
+    _service_notifications_enabled = obj._service_notifications_enabled;
+  }
+  return *this;
+}
+
