87aa5474c8edf8aba7d2523caa77352bd56e1256
Alexandre Fouillé <afouille@merethis.com>
2014-08-13 17:01:54 +0200

    Dependency object.

--- notification/inc/com/centreon/broker/notification/objects/dependency.hh
+++ notification/inc/com/centreon/broker/notification/objects/dependency.hh
@@ -20,6 +20,8 @@
 #ifndef CCB_NOTIFICATION_DEPENDENCY_HH
 #  define CCB_NOTIFICATION_DEPENDENCY_HH
 
+#  include <string>
+#  include "com/centreon/broker/notification/objects/group.hh"
 #  include "com/centreon/broker/namespace.hh"
 
 CCB_BEGIN()
@@ -27,7 +29,83 @@ CCB_BEGIN()
 namespace       notification {
 
   class         dependency {
+  public:
+    enum        type {
+      unknown = 0,
+      host,
+      service
+    };
 
+    enum        action_on {
+      none = 0,
+      host_up = (1 << 0),
+      host_down = (1 << 1),
+      host_unreachable = (1 << 2),
+      host_pending = (1 << 3),
+      service_ok = (1 << 4),
+      service_unknown = (1 << 5),
+      service_warning = (1 << 6),
+      service_critical = (1 << 7),
+      service_pending = (1 << 8)
+    };
+
+    enum        dependency_kind {
+      unknown_dependency = 0,
+      notification_dependency,
+      execution_dependency
+    };
+
+    dependency();
+    dependency(dependency const& obj);
+    dependency& operator=(dependency const& obj);
+
+    bool                   is_host_dependency() const throw();
+    void                   set_is_host_dependency(bool val) throw();
+    bool                   is_service_dependency() const throw();
+    void                   set_is_service_dependency(bool val) throw();
+
+    std::string const&    get_period() const throw();
+    void                  set_period(std::string const& val);
+    dependency_kind       get_kind() const throw();
+    void                  set_kind(dependency_kind val) throw();
+    group const&          get_dependent_hostgroups() const throw();
+    void                  set_dependent_hostgroups(group const& val);
+    group const&          get_dependent_hosts() const throw();
+    void                  set_dependent_hosts(group const& val);
+    group const&          get_dependent_servicegroups() const throw();
+    void                  set_dependent_servicegroups(group const& val);
+    group const&          get_dependent_service_description() const throw();
+    void                  set_dependent_service_description(group const& val);
+    unsigned int          get_execution_failure_options() const throw();
+    void                  set_execution_failure_options(unsigned int val) throw();
+    group const&          get_hostgroups() const throw();
+    void                  set_hostgroups(group const& val);
+    group const&          get_hosts() const throw();
+    void                  set_hosts(group const& val);
+    bool                  get_inherits_parent() const throw();
+    void                  set_inherits_parent(bool val) throw();
+    unsigned int          get_notification_failure_options() const throw();
+    void                  set_notification_failure_options(unsigned int val) throw();
+    group const&          get_servicegroups() const throw();
+    void                  set_servicegroups(group const& val);
+    group const&          get_service_description() const throw();
+    void                  set_service_description(group const& val);
+
+  private:
+    type                   _type;
+    std::string            _dependency_period;
+    dependency_kind        _dependency_type;
+    group                  _dependent_hostgroups;
+    group                  _dependent_hosts;
+    group                  _dependent_servicegroups;
+    group                  _dependent_service_description;
+    unsigned int           _execution_failure_options;
+    group                  _hostgroups;
+    group                  _hosts;
+    bool                   _inherits_parent;
+    unsigned int           _notification_failure_options;
+    group                  _servicegroups;
+    group                  _service_description;
   };
 
 }
--- notification/src/objects/dependency.cc
+++ notification/src/objects/dependency.cc
@@ -20,3 +20,172 @@
 #include "com/centreon/broker/notification/objects/dependency.hh"
 
 using namespace com::centreon::broker::notification;
+
+dependency::dependency() :
+  _type(unknown),
+  _dependency_type(unknown_dependency),
+  _execution_failure_options(0),
+  _inherits_parent(false),
+  _notification_failure_options(0) {}
+
+dependency::dependency(dependency const& obj) :
+  _type(obj._type),
+  _dependency_period(obj._dependency_period),
+  _dependency_type(obj._dependency_type),
+  _dependent_hostgroups(obj._dependent_hostgroups),
+  _dependent_hosts(obj._dependent_hosts),
+  _dependent_servicegroups(obj._dependent_servicegroups),
+  _dependent_service_description(obj._dependent_service_description),
+  _execution_failure_options(obj._execution_failure_options),
+  _hostgroups(obj._hostgroups),
+  _hosts(obj._hosts),
+  _inherits_parent(obj._inherits_parent),
+  _notification_failure_options(obj._notification_failure_options),
+  _servicegroups(obj._servicegroups),
+  _service_description(obj._service_description) {}
+
+dependency& dependency::operator=(dependency const& obj) {
+  if (this != &obj) {
+    _type = obj._type;
+    _dependency_period = obj._dependency_period;
+    _dependency_type = obj._dependency_type;
+    _dependent_hostgroups = obj._dependent_hostgroups;
+    _dependent_hosts = obj._dependent_hosts;
+    _dependent_servicegroups = obj._dependent_servicegroups;
+    _dependent_service_description = obj._dependent_service_description;
+    _execution_failure_options = obj._execution_failure_options;
+    _hostgroups = obj._hostgroups;
+    _hosts = obj._hosts;
+    _inherits_parent = obj._inherits_parent;
+    _notification_failure_options = obj._notification_failure_options;
+    _servicegroups = obj._servicegroups;
+    _service_description = obj._service_description;
+  }
+  return (*this);
+}
+
+bool dependency::is_host_dependency() const throw() {
+  return (_type == host);
+}
+
+void dependency::set_is_host_dependency(bool val) throw() {
+  if (val)
+    _type = host;
+  else
+    _type = service;
+}
+
+bool dependency::is_service_dependency() const throw() {
+  return (_type == service);
+}
+
+void dependency::set_is_service_dependency(bool val) throw() {
+  if (val)
+    _type = service;
+  else
+    _type = host;
+}
+
+std::string const& dependency::get_period() const throw() {
+  return (_dependency_period);
+}
+
+void dependency::set_period(std::string const& val) {
+  _dependency_period = val;
+}
+
+dependency::dependency_kind dependency::get_kind() const throw() {
+  return (_dependency_type);
+}
+
+void dependency::set_kind(dependency::dependency_kind val) throw() {
+  _dependency_type = val;
+}
+
+group const& dependency::get_dependent_hostgroups() const throw() {
+  return (_dependent_hostgroups);
+}
+
+void dependency::set_dependent_hostgroups(group const& val) {
+  _dependent_hostgroups = val;
+}
+
+group const& dependency::get_dependent_hosts() const throw() {
+  return (_dependent_hosts);
+}
+
+void dependency::set_dependent_hosts(group const& val) {
+  _dependent_hosts = val;
+}
+
+group const& dependency::get_dependent_servicegroups() const throw() {
+  return (_dependent_servicegroups);
+}
+
+void dependency::set_dependent_servicegroups(group const& val) {
+  _dependent_servicegroups = val;
+}
+
+group const& dependency::get_dependent_service_description() const throw() {
+  return (_dependent_service_description);
+}
+
+void dependency::set_dependent_service_description(group const& val) {
+  _dependent_service_description = val;
+}
+
+unsigned int dependency::get_execution_failure_options() const throw() {
+  return (_execution_failure_options);
+}
+
+void dependency::set_execution_failure_options(unsigned int val) throw() {
+  _execution_failure_options = val;
+}
+
+group const& dependency::get_hostgroups() const throw() {
+  return (_hostgroups);
+}
+
+void dependency::set_hostgroups(group const& val) {
+  _hostgroups = val;
+}
+
+group const& dependency::get_hosts() const throw() {
+  return (_hosts);
+}
+
+void dependency::set_hosts(group const& val) {
+  _hosts = val;
+}
+
+bool dependency::get_inherits_parent() const throw() {
+  return (_inherits_parent);
+}
+
+void dependency::set_inherits_parent(bool val) throw() {
+  _inherits_parent = val;
+}
+
+unsigned int dependency::get_notification_failure_options() const throw() {
+  return (_notification_failure_options);
+}
+
+void dependency::set_notification_failure_options(unsigned int val) throw() {
+  _notification_failure_options = val;
+}
+
+group const& dependency::get_servicegroups() const throw() {
+  return (_servicegroups);
+}
+
+void dependency::set_servicegroups(group const& val) {
+  _servicegroups = val;
+}
+
+group const& dependency::get_service_description() const throw() {
+  return (_service_description);
+}
+
+void dependency::set_service_description(group const& val) {
+  _service_description = val;
+}
