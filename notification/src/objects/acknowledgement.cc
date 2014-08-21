a1e143eecbff560c20a5812ae971e6f87496f42e
Alexandre Fouillé <afouille@merethis.com>
2014-08-21 15:34:29 +0200

    Downtime and acknowledgement objects.

---
+++ notification/src/objects/acknowledgement.cc
@@ -0,0 +1,34 @@
+/*
+** Copyright 2011-2013 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include "com/centreon/broker/notification/objects/acknowledgement.hh"
+
+using namespace com::centreon::broker::notification;
+
+acknowledgement::acknowledgement() {}
+
+acknowledgement::acknowledgement(acknowledgement const& obj) {}
+
+acknowledgement& acknowledgement::operator=(acknowledgement const& obj) {
+  if (this != &obj) {
+
+  }
+  return (*this);
+}
+
