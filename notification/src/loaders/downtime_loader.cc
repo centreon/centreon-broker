70abc764697efbd70d5623dce3ca32cfa141a955
Alexandre Fouillé <afouille@merethis.com>
2014-08-21 15:59:29 +0200

    Add downtime_loader and downtime_builder.

---
+++ notification/src/loaders/downtime_loader.cc
@@ -0,0 +1,35 @@
+/*
+** Copyright 2011-2013 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <utility>
+#include <vector>
+#include <sstream>
+#include <QVariant>
+#include <QSqlError>
+#include "com/centreon/broker/exceptions/msg.hh"
+#include "com/centreon/broker/notification/objects/downtime.hh"
+#include "com/centreon/broker/notification/loaders/downtime_loader.hh"
+
+using namespace com::centreon::broker::notification;
+
+downtime_loader::downtime_loader() {}
+
+void downtime_loader::load(QSqlDatabase* db, downtime_builder* output) {
+
+}
