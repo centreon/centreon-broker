75194d733735a255867b7cf8056a4b927e3b8d4a
Alexandre Fouillé <afouille@merethis.com>
2014-08-25 17:00:58 +0200

    Added the qHash function for std::string.

---
+++ notification/src/objects/qhash_func.cc
@@ -0,0 +1,26 @@
+/*
+** Copyright 2011-2013 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <QHash>
+#include <QString>
+#include "com/centreon/broker/notification/objects/qhash_func.hh"
+
+uint qHash(std::string const& str) {
+  return (qHash(QString::fromStdString(str)));
+}
