ba9836298d17f5e06f57a68c35b7fa0eb8eb2152
Alexandre Fouillé <afouille@merethis.com>
2014-08-26 11:12:41 +0200

    Added composed dependency builder.

---
+++ notification/inc/com/centreon/broker/notification/builders/composed_dependency_builder.hh
@@ -0,0 +1,66 @@
+/*
+** Copyright 2011-2014 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCB_NOTIFICATION_BUILDERS_COMPOSED_DEPENDENCY_BUILDER_HH
+#  define CCB_NOTIFICATION_BUILDERS_COMPOSED_DEPENDENCY_BUILDER_HH
+
+#  include <vector>
+#  include "com/centreon/broker/namespace.hh"
+#  include "com/centreon/broker/notification/objects/dependency.hh"
+#  include "com/centreon/broker/notification/builders/dependency_builder.hh"
+#  include "com/centreon/broker/notification/builders/composed_builder.hh"
+
+CCB_BEGIN()
+
+namespace       notification {
+
+  class           composed_dependency_builder : public composed_builder<dependency_builder> {
+  public:
+                  composed_dependency_builder();
+    virtual       ~composed_dependency_builder() {}
+
+    virtual void add_dependency(unsigned int id,
+                                shared_ptr<dependency> d) = 0;
+    virtual void dependency_node_id_parent_relation(unsigned int dep_id,
+                                                    node_id id) = 0;
+    virtual void dependency_node_id_child_relation(unsigned int dep_id,
+                                                   node_id id) = 0;
+    virtual void dependency_hostgroup_parent_relation(
+        unsigned int dep_id,
+        unsigned int hostgroup_id) = 0;
+    virtual void dependency_hostgroup_child_relation(
+        unsigned int dep_id,
+        unsigned int hostgroup_id) = 0;
+    virtual void dependency_servicegroup_child_relation(
+        unsigned int dep_id,
+        unsigned int servicegroup_id) = 0;
+    virtual void dependency_servicegroup_parent_relation(
+        unsigned int dep_id,
+        unsigned int servicegroup_id);
+    virtual void set_notification_failure_options(unsigned int dep_id,
+        std::string const& line);
+    virtual void set_execution_failure_options(unsigned int dep_id,
+        std::string const& line);
+  };
+
+}
+
+CCB_END()
+
+#endif // !CCB_NOTIFICATION_BUILDERS_COMPOSED_DEPENDENCY_BUILDER_HH
---
+++ notification/src/builders/composed_dependency_builder.cc
@@ -0,0 +1,95 @@
+/*
+** Copyright 2011-2014 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include "com/centreon/broker/notification/builders/composed_dependency_builder.hh"
+
+using namespace com::centreon::broker::notification;
+
+composed_dependency_builder::composed_dependency_builder() {}
+
+void composed_dependency_builder::add_dependency(unsigned int id,
+                                                 shared_ptr<dependency> d) {
+  for (composed_builder<dependency_builder>::iterator it(begin()),
+       it_end(end()); it != it_end; ++it)
+    (*it)->add_dependency(id, d);
+}
+
+void composed_dependency_builder::dependency_node_id_parent_relation(
+    unsigned int dep_id,
+    node_id id) {
+  for (composed_builder<dependency_builder>::iterator it(begin()),
+       it_end(end()); it != it_end; ++it)
+    (*it)->dependency_node_id_parent_relation(dep_id, id);
+}
+
+void composed_dependency_builder::dependency_node_id_child_relation(
+    unsigned int dep_id,
+    node_id id) {
+  for (composed_builder<dependency_builder>::iterator it(begin()),
+       it_end(end()); it != it_end; ++it)
+    (*it)->dependency_node_id_child_relation(dep_id, id);
+}
+
+void composed_dependency_builder::dependency_hostgroup_parent_relation(
+    unsigned int dep_id,
+    unsigned int hostgroup_id) {
+  for (composed_builder<dependency_builder>::iterator it(begin()),
+       it_end(end()); it != it_end; ++it)
+    (*it)->dependency_hostgroup_parent_relation(dep_id, hostgroup_id);
+}
+
+void composed_dependency_builder::dependency_hostgroup_child_relation(
+    unsigned int dep_id,
+    unsigned int hostgroup_id) {
+  for (composed_builder<dependency_builder>::iterator it(begin()),
+       it_end(end()); it != it_end; ++it)
+    (*it)->dependency_hostgroup_child_relation(dep_id, hostgroup_id);
+}
+
+void composed_dependency_builder::dependency_servicegroup_child_relation(
+    unsigned int dep_id,
+    unsigned int servicegroup_id) {
+  for (composed_builder<dependency_builder>::iterator it(begin()),
+       it_end(end()); it != it_end; ++it)
+    (*it)->dependency_servicegroup_child_relation(dep_id, servicegroup_id);
+}
+
+void composed_dependency_builder::dependency_servicegroup_parent_relation(
+    unsigned int dep_id,
+    unsigned int servicegroup_id) {
+  for (composed_builder<dependency_builder>::iterator it(begin()),
+       it_end(end()); it != it_end; ++it)
+    (*it)->dependency_servicegroup_parent_relation(dep_id, servicegroup_id);
+}
+
+void composed_dependency_builder::set_notification_failure_options(
+    unsigned int dep_id,
+    std::string const& line) {
+  for (composed_builder<dependency_builder>::iterator it(begin()),
+       it_end(end()); it != it_end; ++it)
+    (*it)->set_notification_failure_options(dep_id, line);
+}
+
+void composed_dependency_builder::set_execution_failure_options(
+    unsigned int dep_id,
+    std::string const& line) {
+  for (composed_builder<dependency_builder>::iterator it(begin()),
+       it_end(end()); it != it_end; ++it)
+    (*it)->set_execution_failure_options(dep_id, line);
+}
