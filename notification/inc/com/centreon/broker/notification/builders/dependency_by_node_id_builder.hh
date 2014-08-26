673292fd44aeaf4e137d2fe36e6dfcb3b1d2158e
Alexandre Fouillé <afouille@merethis.com>
2014-08-26 14:59:09 +0200

    Added dependency_by_node_id_builder.

---
+++ notification/inc/com/centreon/broker/notification/builders/dependency_by_node_id_builder.hh
@@ -0,0 +1,76 @@
+/*
+** Copyright 2011-2014 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCB_NOTIFICATION_BUILDERS_DEPENDENCY_BY_NODE_ID_BUILDER_HH
+#  define CCB_NOTIFICATION_BUILDERS_DEPENDENCY_BY_NODE_ID_BUILDER_HH
+
+#  include <QHash>
+#  include "com/centreon/shared_ptr.hh"
+#  include "com/centreon/broker/namespace.hh"
+#  include "com/centreon/broker/notification/objects/dependency.hh"
+#  include "com/centreon/broker/notification/objects/node_id.hh"
+
+CCB_BEGIN()
+
+namespace       notification {
+
+  class         dependency_by_node_id_builder {
+  public:
+                dependency_by_node_id_builder(
+                      QMultiHash<node_id, shared_ptr<dependency> >& by_child_id,
+                      QMultiHash<node_id, shared_ptr<dependency> >& by_parent_id);
+    virtual     ~dependency_by_node_id_builder() {}
+
+    virtual void add_dependency(unsigned int id,
+                                shared_ptr<dependency> d);
+    virtual void dependency_node_id_parent_relation(unsigned int dep_id,
+                                                    node_id id);
+    virtual void dependency_node_id_child_relation(unsigned int dep_id,
+                                                   node_id id);
+    virtual void dependency_hostgroup_parent_relation(
+          unsigned int dep_id,
+          unsigned int hostgroup_id);
+    virtual void dependency_hostgroup_child_relation(
+          unsigned int dep_id,
+          unsigned int hostgroup_id);
+    virtual void dependency_servicegroup_child_relation(
+          unsigned int dep_id,
+          unsigned int servicegroup_id);
+    virtual void dependency_servicegroup_parent_relation(
+          unsigned int dep_id,
+          unsigned int servicegroup_id);
+
+    virtual void set_notification_failure_options(unsigned int dep_id,
+                                                  std::string const& line);
+    virtual void set_execution_failure_options(unsigned int dep_id,
+                                               std::string const& line);
+
+  private:
+                dependency_by_node_id_builder();
+
+     QHash<unsigned int, shared_ptr<dependency> >  _cache;
+     QMultiHash<node_id, shared_ptr<dependency> >& _table_child_id;
+     QMultiHash<node_id, shared_ptr<dependency> >& _table_parent_id;
+  };
+
+}
+
+CCB_END()
+
+#endif // !CCB_NOTIFICATION_BUILDERS_DEPENDENCY_BY_NODE_ID_BUILDER_HH
---
+++ notification/src/builders/dependency_by_node_id_builder.cc
@@ -0,0 +1,82 @@
+/*
+** Copyright 2011-2014 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include "com/centreon/broker/notification/builders/dependency_by_node_id_builder.hh"
+
+using namespace com::centreon::broker::notification;
+
+dependency_by_node_id_builder::dependency_by_node_id_builder(
+    QMultiHash<node_id, shared_ptr<dependency> >& by_child_id,
+    QMultiHash<node_id, shared_ptr<dependency> >& by_parent_id) :
+  _table_child_id(by_child_id),
+  _table_parent_id(by_parent_id) {}
+
+void dependency_by_node_id_builder::add_dependency(
+    unsigned int id,
+    shared_ptr<dependency> d) {
+  _cache[id] = d;
+}
+
+void dependency_by_node_id_builder::dependency_node_id_parent_relation(
+    unsigned int dep_id,
+    node_id id) {
+  _table_parent_id.insert(id, _cache[dep_id]);
+}
+
+void dependency_by_node_id_builder::dependency_node_id_child_relation(
+    unsigned int dep_id,
+    node_id id) {
+  _table_child_id.insert(id, _cache[dep_id]);
+}
+
+void dependency_by_node_id_builder::dependency_hostgroup_parent_relation(
+      unsigned int dep_id,
+      unsigned int hostgroup_id) {
+
+}
+
+void dependency_by_node_id_builder::dependency_hostgroup_child_relation(
+      unsigned int dep_id,
+      unsigned int hostgroup_id) {
+
+}
+
+void dependency_by_node_id_builder::dependency_servicegroup_child_relation(
+      unsigned int dep_id,
+      unsigned int servicegroup_id) {
+
+}
+
+void dependency_by_node_id_builder::dependency_servicegroup_parent_relation(
+      unsigned int dep_id,
+      unsigned int servicegroup_id) {
+
+}
+
+void dependency_by_node_id_builder::set_notification_failure_options(
+    unsigned int dep_id,
+    std::string const& line) {
+
+}
+
+void dependency_by_node_id_builder::set_execution_failure_options(
+    unsigned int dep_id,
+    std::string const& line) {
+
+}
