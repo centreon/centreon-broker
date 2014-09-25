1191db7eac4613c18cbca703c14a50930d57093d
Alexandre Fouillé <afouille@merethis.com>
2014-09-25 11:48:06 +0200

    Notification: Add the rune run_queue class.

---
+++ notification/inc/com/centreon/broker/notification/run_queue.hh
@@ -0,0 +1,61 @@
+/*
+** Copyright 2011-2014 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCB_NOTIFICATION_RUN_QUEUE_HH
+#  define CCB_NOTIFICATION_RUN_QUEUE_HH
+
+#  include <ctime>
+#  include <map>
+#  include "com/centreon/broker/namespace.hh"
+#  include "com/centreon/broker/notification/action.hh"
+
+CCB_BEGIN()
+
+namespace           notification {
+  /**
+   *  @class run_queue run_queue.hh "com/centreon/broker/notification/run_queue.hh"
+   *  @brief A run queue of actions.
+   *
+   *  Store action tried by time of beginning.
+   */
+  class               run_queue {
+  public:
+    typedef           std::multimap<time_t, action> action_map;
+    typedef           action_map::iterator iterator;
+    typedef           action_map::const_iterator const_iterator;
+
+                      run_queue();
+                      run_queue(run_queue const& obj);
+    run_queue&        operator=(run_queue const& obj);
+
+    void              run(time_t at, action a);
+
+    iterator          begin();
+    const_iterator    begin() const;
+    iterator          end();
+    const_iterator    end() const;
+
+  private:
+    action_map        _actions;
+  };
+}
+
+CCB_END()
+
+#endif // !CCB_NOTIFICATION_RUN_QUEUE_HH
---
+++ notification/src/run_queue.cc
@@ -0,0 +1,56 @@
+/*
+** Copyright 2009-2014 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include "com/centreon/broker/notification/run_queue.hh"
+
+using namespace com::centreon::broker::notification;
+using namespace com::centreon::broker::notification::objects;
+
+run_queue::run_queue() {}
+
+run_queue::run_queue(run_queue const& obj) {
+  run_queue::operator=(obj);
+}
+
+run_queue& run_queue::operator=(run_queue const& obj) {
+  if (this != &obj) {
+    _actions = obj._actions;
+  }
+  return (*this);
+}
+
+void run_queue::run(time_t at, action a) {
+  _actions.insert(std::make_pair(at, a));
+}
+
+run_queue::iterator run_queue::begin() {
+  return (_actions.begin());
+}
+
+run_queue::const_iterator run_queue::begin() const {
+  return (_actions.begin());
+}
+
+run_queue::iterator run_queue::end() {
+  return (_actions.end());
+}
+
+run_queue::const_iterator run_queue::end() const {
+  return (_actions.end());
+}
