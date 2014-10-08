d7e0a6598c6a5326f5fe925fcd71eb6acb87bd72
Alexandre Fouillé <afouille@merethis.com>
2014-10-08 10:25:30 +0200

    Notification: Add notification_rule_loader and notification_rule_builder.

---
+++ notification/inc/com/centreon/broker/notification/builders/notification_rule_builder.hh
@@ -0,0 +1,47 @@
+/*
+** Copyright 2011-2014 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCB_NOTIFICATION_BUILDERS_NOTIFICATION_RULE_BUILDER_HH
+#  define CCB_NOTIFICATION_BUILDERS_NOTIFICATION_RULE_BUILDER_HH
+
+#  include "com/centreon/broker/namespace.hh"
+#  include "com/centreon/broker/notification/objects/node_id.hh"
+#  include "com/centreon/broker/notification/objects/notification_method.hh"
+
+CCB_BEGIN()
+
+namespace           notification {
+  /**
+   *  @class notification_rule_builder notification_rule_builder.hh "com/centreon/broker/notification/builders/notification_rule_builder.hh"
+   */
+  class             notification_rule_builder {
+  public:
+    virtual         ~notification_rule_builder() {}
+
+    virtual void    add_rule(unsigned int method_id,
+                             unsigned int timeperiod_id,
+                             unsigned int contact_id,
+                             objects::node_id node_id) {}
+  };
+
+}
+
+CCB_END()
+
+#endif // !CCB_NOTIFICATION_BUILDERS_NOTIFICATION_RULE_BUILDER_HH
---
+++ notification/src/loaders/notification_rule_loader.cc
@@ -0,0 +1,53 @@
+/*
+** Copyright 2011-2013 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <sstream>
+#include <QVariant>
+#include <QSet>
+#include <QSqlError>
+#include "com/centreon/broker/exceptions/msg.hh"
+#include "com/centreon/broker/notification/loaders/notification_rule_loader.hh"
+
+using namespace com::centreon::broker::notification;
+using namespace com::centreon::broker::notification::objects;
+
+notification_rule_loader::notification_rule_loader() {}
+
+void notification_rule_loader::load(QSqlDatabase *db, notification_rule_builder *output) {
+  // If we don't have any db or output, don't do anything.
+  if (!db || !output)
+    return;
+
+  QSqlQuery query(*db);
+
+  // Performance improvement, as we never go back.
+  query.setForwardOnly(true);
+
+  if (!query.exec("SELECT rule_id, method_id, timeperiod_id, contact_id, "
+                  "host_id, service_id FROM cfg_configuration_method"))
+    throw (exceptions::msg()
+      << "Notification: cannot select rt_notification_rules in loader: "
+      << query.lastError().text());
+
+  while (query.next())
+    output->add_rule(query.value(1).toUInt(),
+                     query.value(2).toUInt(),
+                     query.value(3).toUInt(),
+                     node_id(query.value(4).toUInt(), query.value(5).toUInt()));
+}
