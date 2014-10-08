cb6568d9e85d0aaf218dbc99fb57d5a165a60ec2
Alexandre Fouillé <afouille@merethis.com>
2014-10-08 09:22:55 +0200

    Notification: Create the notification method object.

---
+++ notification/inc/com/centreon/broker/notification/objects/notification_method.hh
@@ -0,0 +1,80 @@
+/*
+** Copyright 2011-2013 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCB_NOTIFICATION_NOTIFICATION_METHOD_HH
+#  define CCB_NOTIFICATION_NOTIFICATION_METHOD_HH
+
+#  include <string>
+#  include "com/centreon/broker/namespace.hh"
+
+CCB_BEGIN()
+
+namespace         notification {
+  namespace       objects {
+    /**
+     *  @class notification_method notification_method.hh "com/centreon/broker/notification/objects/notification_method.hh"
+     *  @brief The notification method object.
+     *
+     */
+    class           notification_method {
+    public:
+                    notification_method();
+                    notification_method(notification_method const& obj);
+      notification_method&
+                    operator=(notification_method const& obj);
+
+      std::string const&
+                    get_name() const throw();
+      void          set_name(std::string const& val);
+
+      unsigned int  get_command_id() const throw();
+      void          set_command_id(unsigned int id) throw();
+
+      unsigned int  get_interval() const throw();
+      void          set_interval(unsigned int val) throw();
+
+      std::string const&
+                    get_status() const throw();
+      void          set_status(std::string const& val);
+
+      std::string const&
+                    get_types() const throw();
+      void          set_types(std::string const& val);
+
+      unsigned int  get_start() const throw();
+      void          set_start(unsigned int val) throw();
+
+      unsigned int  get_end() const throw();
+      void          set_end(unsigned int val) throw();
+
+    private:
+      std::string   _name;
+      unsigned int  _command_id;
+      unsigned int  _interval;
+      std::string   _status;
+      std::string   _types;
+      unsigned int  _start;
+      unsigned int  _end;
+    };
+  }
+}
+
+CCB_END()
+
+#endif // !CCB_NOTIFICATION_NOTIFICATION_METHOD_HH
---
+++ notification/src/objects/notification_method.cc
@@ -0,0 +1,97 @@
+/*
+** Copyright 2011-2013 Merethis
+**
+** This file is part of Centreon Broker.
+**
+** Centreon Broker is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Broker is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Broker. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include "com/centreon/broker/notification/objects/notification_method.hh"
+
+using namespace com::centreon::broker::notification::objects;
+
+notification_method::notification_method() {}
+
+notification_method::notification_method(notification_method const& obj) {
+  notification_method::operator=(obj);
+}
+
+notification_method& notification_method::operator=(notification_method const& obj) {
+  if (this != &obj) {
+    _name = obj._name;
+    _command_id = obj._command_id;
+    _interval = obj._interval;
+    _status = obj._status;
+    _types = obj._types;
+    _start = obj._start;
+    _end = obj._end;
+  }
+  return (*this);
+}
+
+std::string const& notification_method::get_name() const throw() {
+  return (_name);
+}
+
+void notification_method::set_name(std::string const& val) {
+  _name = val;
+}
+
+unsigned int notification_method::get_command_id() const throw() {
+  return (_command_id);
+}
+
+void notification_method::set_command_id(unsigned int id) throw() {
+  _command_id = id;
+}
+
+unsigned int notification_method::get_interval() const throw() {
+  return (_interval);
+}
+
+void notification_method::set_interval(unsigned int val) throw() {
+  _interval = val;
+}
+
+std::string const& notification_method::get_status() const throw() {
+  return (_status);
+}
+
+void notification_method::set_status(std::string const& val) {
+  _status = val;
+}
+
+std::string const& notification_method::get_types() const throw() {
+  return (_types);
+}
+
+void notification_method::set_types(std::string const& val) {
+  _types = val;
+}
+
+unsigned int notification_method::get_start() const throw() {
+  return (_start);
+}
+
+void notification_method::set_start(unsigned int val) throw() {
+  _start = val;
+}
+
+unsigned int notification_method::get_end() const throw() {
+  return (_end);
+}
+
+void notification_method::set_end(unsigned int val) throw() {
+  _end = val;
+}
