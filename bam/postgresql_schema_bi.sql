c4606f7daf8f8141323f477e8701b1c6535dec98
Matthieu Kermagoret <mkermagoret@merethis.com>
2014-10-22 10:11:37 +0200

    BAM: move DB schemas to module directory.

---
+++ bam/postgresql_schema_bi.sql
@@ -0,0 +1,188 @@
+-- ------------------------------------
+--                                   --
+-- Centreon Broker's database schema --
+--                                   --
+--                                   --
+--    Business Activity Reporting    --
+-- ------------------------------------
+
+-- ba
+-- ba_availabilities
+-- ba_events
+-- ba_events_durations
+-- bv
+-- kpi
+-- kpi_events
+-- relations_ba_bv
+-- relations_ba_kpi_events
+
+
+--
+-- Business Views.
+--
+CREATE TABLE bv (
+  bv_id serial,
+  bv_name varchar(45) default NULL,
+
+  bv_description text default NULL,
+
+  PRIMARY KEY (bv_id),
+  UNIQUE (bv_name)
+);
+
+--
+-- Business Activities.
+--
+CREATE TABLE ba (
+  ba_id serial,
+  ba_name varchar(45) default NULL,
+
+  ba_description text default NULL,
+  sla_month_percent_1 float default NULL,
+  sla_month_percent_2 float default NULL,
+  sla_month_duration_1 int default NULL,
+  sla_month_duration_2 int default NULL,
+
+  PRIMARY KEY (ba_id),
+  UNIQUE (ba_name)
+);
+
+--
+-- Key Performance Indicators.
+--
+CREATE TABLE kpi (
+  kpi_id serial,
+  kpi_name varchar(45) default NULL,
+
+  ba_id int default NULL,
+  ba_name varchar(45) default NULL,
+  host_id int default NULL,
+  host_name varchar(45) default NULL,
+  service_id int default NULL,
+  service_description varchar(45) default NULL,
+  kpi_ba_id int default NULL,
+  kpi_ba_name varchar(45) default NULL,
+  meta_service_id int default NULL,
+  meta_service_name varchar(45),
+  boolean_id int default NULL,
+  boolean_name varchar(45),
+  impact_warning float default NULL,
+  impact_critical float default NULL,
+  impact_unknown float default NULL,
+
+  PRIMARY KEY (kpi_id),
+  UNIQUE (kpi_name),
+  FOREIGN KEY (ba_id) REFERENCES ba (ba_id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (kpi_ba_id) REFERENCES ba (ba_id)
+    ON DELETE CASCADE
+);
+
+--
+-- Relations between BA and BV.
+--
+CREATE TABLE relations_ba_bv (
+  ba_bv_id serial,
+  bv_id int NOT NULL,
+  ba_id int NOT NULL,
+
+  PRIMARY KEY (ba_bv_id),
+  FOREIGN KEY (bv_id) REFERENCES bv (bv_id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (ba_id) REFERENCES ba (ba_id)
+    ON DELETE CASCADE
+);
+
+--
+-- BA events.
+--
+CREATE TABLE ba_events (
+  ba_event_id serial,
+  ba_id int NOT NULL,
+  start_time int NOT NULL,
+
+  end_time int default NULL,
+  status smallint default NULL,
+  in_downtime boolean default NULL,
+
+  PRIMARY KEY (ba_event_id),
+  FOREIGN KEY (ba_id) REFERENCES ba (ba_id)
+    ON DELETE CASCADE
+);
+
+--
+-- KPI events.
+--
+CREATE TABLE kpi_events (
+  kpi_event_id serial,
+  kpi_id int NOT NULL,
+  start_time int NOT NULL,
+
+  end_time int default NULL,
+  status smallint default NULL,
+  in_downtime boolean default NULL,
+  impact_level smallint default NULL,
+  first_output text default NULL,
+  first_perfdata varchar(45) default NULL,
+
+  PRIMARY KEY (kpi_event_id),
+  FOREIGN KEY (kpi_id) REFERENCES kpi (kpi_id)
+    ON DELETE CASCADE
+);
+
+--
+-- Relations between BA events and KPI events.
+--
+CREATE TABLE relations_ba_kpi_events (
+  ba_kpi_event_id serial,
+  ba_event_id int NOT NULL,
+  kpi_event_id int NOT NULL,
+
+  PRIMARY KEY (ba_kpi_event_id),
+  FOREIGN KEY (ba_event_id) REFERENCES ba_events (ba_event_id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (kpi_event_id) REFERENCES kpi_events (kpi_event_id)
+    ON DELETE CASCADE
+);
+
+--
+-- BA events durations.
+--
+CREATE TABLE ba_events_durations (
+  ba_event_id serial,
+  timeperiod_id int NOT NULL,
+
+  start_time int default NULL,
+  end_time int default NULL,
+  duration int default NULL,
+  sla_duration int default NULL,
+  timeperiod_is_default boolean default NULL,
+
+  UNIQUE (ba_event_id, timeperiod_id),
+  FOREIGN KEY (ba_event_id) REFERENCES ba_events (ba_event_id)
+    ON DELETE CASCADE
+);
+
+--
+-- BA availabilities.
+--
+CREATE TABLE ba_availabilities (
+  ba_id int NOT NULL,
+  time_id int NOT NULL,
+  timeperiod_id int NOT NULL,
+
+  available int default NULL,
+  unavailable int default NULL,
+  degraded int default NULL,
+  unknown int default NULL,
+  downtime int default NULL,
+  alert_unavailable_opened int default NULL,
+  alert_degraded_opened int default NULL,
+  alert_unknown_opened int default NULL,
+  alert_downtime_opened int default NULL,
+  timeperiod_is_default boolean default NULL,
+
+  UNIQUE (ba_id, time_id, timeperiod_id),
+  FOREIGN KEY (ba_id) REFERENCES ba (ba_id)
+    ON DELETE CASCADE
+);
