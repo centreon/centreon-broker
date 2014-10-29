759bd46daf2d796eb8c4b8942cd34c05cbfcc500
Matthieu Kermagoret <mkermagoret@merethis.com>
2014-10-29 17:35:50 +0100

    Test: disable some unnecessary log files.

--- test/disable_metrics_graphs.cc
+++ test/disable_metrics_graphs.cc
@@ -1,5 +1,5 @@
 /*
-** Copyright 2012-2013 Merethis
+** Copyright 2012-2014 Merethis
 **
 ** This file is part of Centreon Broker.
 **
@@ -89,7 +89,7 @@ int main() {
           << "  <include>" PROJECT_SOURCE_DIR "/test/cfg/broker_modules.xml</include>\n"
           << "  <instance>42</instance>\n"
           << "  <instance_name>MyBroker</instance_name>\n"
-        //<< "  <!--\n"
+          << "  <!--\n"
           << "  <logger>\n"
           << "    <type>file</type>\n"
           << "    <name>cbmod.log</name>\n"
@@ -99,7 +99,7 @@ int main() {
           << "    <info>1</info>\n"
           << "    <level>3</level>\n"
           << "  </logger>\n"
-        //<< "  -->\n"
+          << "  -->\n"
           << "  <output>\n"
           << "    <name>EngineToStorageUnitTest</name>\n"
           << "    <type>storage</type>\n"
