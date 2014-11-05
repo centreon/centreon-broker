6c2daedd85ffb7ad60067d5ed43dfe88b223339a
Alexandre Fouillé <afouille@merethis.com>
2014-11-05 16:27:17 +0100

    Bam: Bam rebuild (wip).

--- test/bam_rebuild.cc
+++ test/bam_rebuild.cc
@@ -54,8 +54,6 @@ int main() {
   bool error(true);
 
   // Variables that need cleaning.
-  std::list<host> hosts;
-  std::list<service> services;
   cbd broker;
   test_db db;
 
@@ -63,12 +61,96 @@ int main() {
     // Prepare database.
     db.open(NULL, BI_DB_NAME, CENTREON_DB_NAME);
 
+    // Create BAs.
+    {
+      QString query(
+                "INSERT INTO mod_bam (ba_id, name, description,"
+                "                     sla_month_percent_warn, sla_month_percent_crit,"
+                "                     sla_month_duration_warn, sla_month_duration_crit,"
+                "                     must_be_rebuild)"
+                "  VALUES (1, 'BA1', 'DESC1', 90, 80, 70, 60, 1),"
+                "         (2, 'BA2', 'DESC2', 80, 70, 60, 50, 1)");
+      QSqlQuery q(*db.centreon_db());
+      if (!q.exec(query))
+        throw (exceptions::msg() << "could not create BAs: "
+               << q.lastError().text());
+    }
+
+    // Create BA events.
+    {
+      QString query(
+                "INSERT INTO mod_bam_reporting_ba_events (ba_id, start_time, end_time)"
+                "  VALUES (1, 0, 30),"
+                "         (2, 0, 50),"
+                "         (1, 30, 120),"
+                "         (2, 50, 160)");
+      QSqlQuery q(*db.bi_db());
+      if (!q.exec(query))
+        throw (exceptions::msg() << "could not create BA events: "
+               << q.lastError().text());
+    }
+
+    // Create timeperiods
+    {
+      QString query(
+                "INSERT INTO timeperiod (tp_id, tp_name, tp_sunday, tp_monday,"
+                "                        tp_tuesday, tp_wednesday, tp_thursday,"
+                "                        tp_friday, tp_saturday"
+                "  VALUES (1, '24x7', '00:00-24:00', '00:00-24:00', '00:00-24:00',"
+                "          '00:00-24:00', '00:00-24:00', '00:00-24:00', '00:00-24:00')");
+      QSqlQuery q(*db.centreon_db());
+      if (!q.exec(query))
+        throw (exceptions::msg() << "could not create timeperiods: "
+               << q.lastError().text());
+    }
+
+    // Create BA-Timeperiods relations
+    {
+      QString query(
+                "INSERT INTO mod_bam_ba_tp_rel (ba_id, timeperiod_id, is_default)"
+                "  VALUES (1, 1, true),"
+                "         (2, 1, false)");
+      QSqlQuery q(*db.centreon_db());
+      if (!q.exec(query))
+        throw (exceptions::msg() << "could not BAs timeperiods relations: "
+               << q.lastError().text());
+    }
+
     // Start Broker daemon.
     broker.set_config_file(
       PROJECT_SOURCE_DIR "/test/cfg/bam.xml");
     broker.start();
     sleep_for(2 * MONITORING_ENGINE_INTERVAL_LENGTH);
     broker.update();
+
+    // Let the broker do its things.
+    sleep_for(3 * MONITORING_ENGINE_INTERVAL_LENGTH);
+
+    // See if the ba events durations were created.
+    {
+      QString query(
+                "SELECT * FROM mod_bam_reporting_ba_events_durations WHERE must_be_rebuild = 1");
+      QSqlQuery q(*db.bi_db());
+      if (!q.exec(query))
+        throw (exceptions::msg() << "could not get the number of BA: "
+               << q.lastError().text());
+      if (q.size() != 0)
+        throw (exceptions::msg() << "the BAs must_be_rebuild field were "
+                                    "not updated.");
+    }
+
+    // See if the ba were marked as rebuilt.
+    {
+      QString query(
+                "SELECT * FROM mod_bam WHERE must_be_rebuild = 1");
+      QSqlQuery q(*db.centreon_db());
+      if (!q.exec(query))
+        throw (exceptions::msg() << "could not get the number of BA: "
+               << q.lastError().text());
+      if (q.size() != 0)
+        throw (exceptions::msg() << "the bas must_be_rebuild field were "
+                                    "not updated.");
+    }
 
     // Success.
     error = false;
@@ -82,8 +164,6 @@ int main() {
 
   // Cleanup.
   broker.stop();
-  free_hosts(hosts);
-  free_services(services);
 
   return (error ? EXIT_FAILURE : EXIT_SUCCESS);
 }
