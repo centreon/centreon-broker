86b53a09e95d518c2fcfa91fbc070c821f446fef
Alexandre Fouille <afouille@centreon.com>
2015-03-11 14:38:18 +0100

    Core: Don't push an empty event when read_timeout wasn't given.

--- core/src/processing/failover.cc
+++ core/src/processing/failover.cc
@@ -561,7 +561,9 @@ void failover::run() {
               misc::shared_ptr<io::data> data;
               _from->read(data, _next_timeout, &timed_out);
               _unprocessed.push_back(data);
-              if (_next_timeout < ::time(NULL)) {
+              if ((_next_timeout != (time_t)-1)
+                    &&_next_timeout < ::time(NULL)
+                    && !timed_out) {
                 timed_out = true;
                 _unprocessed.push_back(misc::shared_ptr<io::data>());
               }
