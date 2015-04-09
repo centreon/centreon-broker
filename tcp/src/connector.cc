26824e04b6312352e03848d16ee0c2a51515e534
Alexandre Fouille <afouille@centreon.com>
2015-04-09 11:38:22 +0200

    TCP: fix error in the filling of the timeval structure.

--- tcp/src/connector.cc
+++ tcp/src/connector.cc
@@ -141,8 +141,8 @@ misc::shared_ptr<io::stream> connector::open() {
   if (_write_timeout >= 0) {
 #ifndef _WIN32
     struct timeval t;
-    t.tv_sec = _write_timeout % 1000000;
-    t.tv_usec = _write_timeout / 1000000;
+    t.tv_sec = _write_timeout / 1000000;
+    t.tv_usec = _write_timeout % 1000000;
     setsockopt(
       _socket->socketDescriptor(),
       SOL_SOCKET,
