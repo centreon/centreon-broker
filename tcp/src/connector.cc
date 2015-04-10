668557b85b9d17677a755178fefadce2fd5dc2cb
Alexandre Fouillé <afouille@merethis.net>
2015-04-10 14:11:25 +0200

    Tcp: Fix socket_write_timeout option.

--- tcp/src/connector.cc
+++ tcp/src/connector.cc
@@ -155,6 +155,8 @@ misc::shared_ptr<io::stream> connector::open() {
   // Return stream.
   misc::shared_ptr<stream> s(new stream(_socket, _mutex));
   s->set_timeout(_timeout);
+  if (_write_timeout >= 0)
+    s->set_write_timeout(_write_timeout / 1000);
   return (s);
 }
 
