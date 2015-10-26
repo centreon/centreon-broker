7d9dc54ebcac54ac95525e775ec1e4a8ca493ba6
Matthieu Kermagoret <mkermagoret@merethis.com>
2015-10-26 16:02:00 +0100

    test: test most fields of the hosts table.

--- test/rtmonitoring/v2/hosts.cc
+++ test/rtmonitoring/v2/hosts.cc
@@ -54,17 +54,16 @@ static void postcheck(
               test::centengine& engine,
               test::time_points& tpoints,
               test::db& db,
-              test::predicate expected[][80]) {
+              test::predicate expected[][79]) {
   static std::string check_query(
     "SELECT host_id, name, instance_id, acknowledged,"
     "       acknowledgement_type, action_url, active_checks, address,"
     "       alias, check_attempt, check_command, check_freshness,"
     "       check_interval, check_period, check_type, checked,"
     "       command_line, default_active_checks,"
-    "       default_event_handler_enabled, default_failure_prediction,"
-    "       default_flap_detection, default_notify,"
-    "       default_passive_checks, enabled, event_handler,"
-    "       event_handler_enabled, execution_time, failure_prediction,"
+    "       default_event_handler_enabled, default_flap_detection,"
+    "       default_notify, default_passive_checks, enabled,"
+    "       event_handler, event_handler_enabled, execution_time,"
     "       first_notification_delay, flap_detection,"
     "       flap_detection_on_down, flap_detection_on_unreachable,"
     "       flap_detection_on_up, flapping, freshness_threshold,"
@@ -85,8 +84,11 @@ static void postcheck(
     "       stalk_on_up, state, state_type, statusmap_image"
     "  FROM hosts"
     "  ORDER BY host_id ASC");
+  engine.reload();
   test::sleep_for(3);
   tpoints.store();
+  expected[0][44]
+    = test::predicate(tpoints.prelast(), tpoints.last() + 1);
   db.check_content(check_query, expected);
   std::cout << "  passed\n";
   return ;
@@ -140,52 +142,447 @@ int main() {
     cbmod_cfg.set("TCP_HOST", "localhost");
     cbmod_cfg.set("TCP_PORT", "5572");
     test::centengine_config engine_config;
-    engine_config.generate_hosts(3);
-    engine_config.generate_services(5);
+    engine_config.generate_hosts(5);
+    engine_config.generate_services(3);
     engine_config.set_cbmod_cfg_file(cbmod_cfg.generate());
+    {
+      int i(0);
+      for (test::centengine_config::objlist::iterator
+             it(engine_config.get_hosts().begin()),
+             end(engine_config.get_hosts().end());
+           it != end;
+           ++it, ++i) {
+        it->set("active_checks_enabled", "0");
+        it->set("address", "hostaddress");
+        it->set("alias", it->get("host_name"));
+        it->set("check_command", " ");
+        it->set("check_freshness", "0");
+        it->set("check_interval", "5");
+        it->set("check_period", " ");
+        it->set("event_handler_enabled", "0");
+        it->set("first_notification_delay", "0");
+        it->set("flap_detection_enabled", "0");
+        it->set("flap_detection_options", "n");
+        it->set("max_check_attempts", "5");
+        it->set("notifications_enabled", "0");
+        it->set("notification_interval", "5");
+        it->set("notification_options", "n");
+        it->set("notification_period", " ");
+        it->set("obsess_over_host", "0");
+        it->set("passive_checks_enabled", "0");
+        it->set("retain_nonstatus_information", "0");
+        it->set("retain_status_information", "0");
+        it->set("retry_interval", "1");
+        it->set("stalking_options", "n");
+      }
+    }
     test::centengine engine(&engine_config);
 
     // Time points.
     test::time_points tpoints;
 
     // Check default entries.
-    precheck(tpoints, "default");
+    precheck(tpoints, "enabled, last_update");
     engine.start();
     test::sleep_for(1);
-    test::predicate expected[][80] = {
+    test::predicate expected[][79] = {
       { 1u, "1", 42, false,
-        0, "", true, "",
-        "", 0, "", false,
+        0, "", false, "hostaddress",
+        "1", 1, "", false,
         5, "", 0, false,
-        "", true,
+        test::predicate(test::predicate::type_null), false,
         false, false,
-        false, false,
-        false, true, "",
-        false, 0.0, false,
+        false, false, true,
+        "", false, 0.0,
         0, false,
         false, false,
-        false, false, false,
+        false, false, 0.0,
         0.0, "", "",
         test::predicate(test::predicate::type_null), 0, test::predicate(test::predicate::type_null),
         test::predicate(test::predicate::type_null), test::predicate(test::predicate::type_null), test::predicate(test::predicate::type_null),
-        test::predicate(test::predicate::type_null), test::predicate(test::predicate::type_null), test::predicate(test::predicate::type_null), 0.0,
-        0.0, 3, test::predicate(test::predicate::type_null),
+        test::predicate(test::predicate::type_null), test::predicate(test::predicate::type_null), test::predicate(tpoints.last(), tpoints.last() + 2), 0.0,
+        0.0, 5, test::predicate(test::predicate::type_null),
         test::predicate(test::predicate::type_null), false, "",
         "", 5, 0,
-        "", true, false,
+        "", false, false,
         false, false, false,
         false, false, "",
-        true, 0.0, "", test::predicate(test::predicate::type_null),
-        true, true,
-        5, 0,
-        true, false, false,
-        false, 0, 1 }, //, ""},
+        false, 0.0, "", test::predicate(test::predicate::type_null),
+        false, false,
+        1, 0,
+        false, false, false,
+        false, 4, 1, ""},
+      { test::predicate() },
+      { test::predicate() },
       { test::predicate() },
       { test::predicate() },
       { test::predicate() }
     };
-    memcpy(expected[1], expected[0], sizeof(expected[0]));
-    memcpy(expected[2], expected[0], sizeof(expected[0]));
+    std::string host_ids_str[5];
+    host_ids_str[0] = "1";
+    for (int i(1); i < 5; ++i) {
+      memcpy(expected[i], expected[0], sizeof(expected[0]));
+      std::ostringstream oss;
+      oss << i + 1;
+      host_ids_str[i] = oss.str();
+      expected[i][0] = i + 1;
+      expected[i][1] = host_ids_str[i].c_str();
+      expected[i][8] = host_ids_str[i].c_str();
+    }
+    postcheck(engine, tpoints, db, expected);
+
+    // Tested host.
+    test::centengine_object& h(*++engine_config.get_hosts().begin());
+
+    // Check name.
+    precheck(tpoints, "name");
+    h.set("host_name", "renamed");
+    expected[0][1] = "renamed";
+    {
+      test::centengine_config::objlist::iterator
+        it(++engine_config.get_services().begin());
+      for (int i(0); i < 3; ++i, ++it)
+        it->set("host_name", "renamed");
+    }
+    postcheck(engine, tpoints, db, expected);
+
+    // Check action_url.
+    precheck(tpoints, "action_url");
+    h.set("action_url", "http://www.centreon.com");
+    expected[0][5] = "http://www.centreon.com";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check address.
+    precheck(tpoints, "address");
+    h.set("address", "10.10.10.10");
+    expected[0][7] = "10.10.10.10";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check alias.
+    precheck(tpoints, "alias");
+    h.set("alias", "myalias");
+    expected[0][8] = "myalias";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check check_command.
+    precheck(tpoints, "check_command");
+    h.set("check_command", "default_command");
+    expected[0][10] = "default_command";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check check_freshness.
+    precheck(tpoints, "check_freshness");
+    h.set("check_freshness", "1");
+    expected[0][11] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check check_interval.
+    precheck(tpoints, "check_interval");
+    h.set("check_interval", "2");
+    expected[0][12] = 2;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check check_period.
+    precheck(tpoints, "check_period #1");
+    {
+      test::centengine_object
+        tp(test::centengine_object::timeperiod_type);
+      tp.set("timeperiod_name", "test_timeperiod");
+      tp.set("alias", "test_timeperiod");
+      tp.set("monday", "00:00-24:00");
+      engine_config.get_timeperiods().push_back(tp);
+    }
+    h.set("check_period", "test_timeperiod");
+    expected[0][13] = "test_timeperiod";
+    postcheck(engine, tpoints, db, expected);
+    precheck(tpoints, "check_period #2");
+    h.set("check_period", "default_timeperiod");
+    expected[0][13] = "default_timeperiod";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check event_handler.
+    precheck(tpoints, "event_handler");
+    h.set("event_handler", "default_command");
+    expected[0][23] = "default_command";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check event_handler_enabled.
+    precheck(
+      tpoints,
+      "event_handler_enabled, default_event_handler_enabled #1");
+    h.set("event_handler_enabled", "1");
+    expected[0][18] = true;
+    expected[0][24] = true;
+    postcheck(engine, tpoints, db, expected);
+    precheck(
+      tpoints,
+      "event_handler_enabled, default_event_handler_enabled #2");
+    h.set("event_handler_enabled", "0");
+    expected[0][18] = false;
+    expected[0][24] = false;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check first_notification_delay.
+    precheck(tpoints, "first_notification_delay #1");
+    h.set("first_notification_delay", "42");
+    expected[0][26] = 42.0;
+    postcheck(engine, tpoints, db, expected);
+    precheck(tpoints, "first_notification_delay #2");
+    h.set("first_notification_delay", "0");
+    expected[0][26] = 0.0;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check flap_detection.
+    precheck(tpoints, "flap_detection, default_flap_detection");
+    h.set("flap_detection_enabled", "1");
+    expected[0][19] = true;
+    expected[0][27] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check flap_detection_on_down.
+    precheck(tpoints, "flap_detection_on_down");
+    h.set("flap_detection_options", "d");
+    expected[0][28] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check flap_detection_on_unreachable.
+    precheck(tpoints, "flap_detection_on_unreachable");
+    h.set("flap_detection_options", "d,u");
+    expected[0][29] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check flap_detection_on_up.
+    precheck(tpoints, "flap_detection_on_up");
+    h.set("flap_detection_options", "a");
+    expected[0][30] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check freshness threshold.
+    precheck(tpoints, "freshness_threshold");
+    h.set("freshness_threshold", "20");
+    expected[0][32] = 20.0;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check high_flap_threshold.
+    precheck(tpoints, "high_flap_threshold");
+    h.set("high_flap_threshold", "30");
+    expected[0][33] = 30.0;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check icon_image.
+    precheck(tpoints, "icon_image");
+    h.set("icon_image", "my_icon_image");
+    expected[0][34] = "my_icon_image";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check icon_image_alt.
+    precheck(tpoints, "icon_image_alt");
+    h.set("icon_image_alt", "my_alternative_icon_image");
+    expected[0][35] = "my_alternative_icon_image";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check low_flap_threshold.
+    precheck(tpoints, "low_flap_threshold");
+    h.set("low_flap_threshold", "10");
+    expected[0][46] = 10.0;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check max_check_attempts.
+    precheck(tpoints, "max_check_attempts");
+    h.set("max_check_attempts", "2");
+    expected[0][47] = 2;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check notes.
+    precheck(tpoints, "notes");
+    h.set("notes", "my personal notes");
+    expected[0][51] = "my personal notes";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check notes_url.
+    precheck(tpoints, "notes_url");
+    h.set("notes_url", "http://www.centreon.com/");
+    expected[0][52] = "http://www.centreon.com/";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check notification_interval.
+    precheck(tpoints, "notification_interval");
+    h.set("notification_interval", "2");
+    expected[0][53] = 2.0;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check notification_period.
+    precheck(tpoints, "notification_period");
+    h.set("notification_period", "default_timeperiod");
+    expected[0][55] = "default_timeperiod";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check notify_on_down.
+    precheck(tpoints, "notify_on_down");
+    h.set("notification_options", "d");
+    expected[0][57] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check notify_on_downtime.
+    precheck(tpoints, "notify_on_downtime");
+    h.set("notification_options", "d,s");
+    expected[0][58] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check notify_on_flapping.
+    precheck(tpoints, "notify_on_flapping");
+    h.set("notification_options", "d,s,f");
+    expected[0][59] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check notify_on_recovery.
+    precheck(tpoints, "notify_on_recovery");
+    h.set("notification_options", "d,s,f,r");
+    expected[0][60] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check notify_on_unreachable.
+    precheck(tpoints, "notify_on_unreachable");
+    h.set("notification_options", "a");
+    expected[0][61] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check notify, default_notify.
+    precheck(tpoints, "notify, default_notify");
+    h.set("notifications_enabled", "1");
+    expected[0][20] = true;
+    expected[0][56] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check obsess_over_host.
+    precheck(tpoints, "obsess_over_host");
+    h.set("obsess_over_host", "1");
+    expected[0][62] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check passive_checks, default_passive_checks.
+    precheck(tpoints, "passive_checks, default_passive_checks");
+    h.set("passive_checks_enabled", "1");
+    expected[0][21] = true;
+    expected[0][64] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check retain_nonstatus_information.
+    precheck(tpoints, "retain_nonstatus_information");
+    h.set("retain_nonstatus_information", "1");
+    expected[0][68] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check retain_status_information.
+    precheck(tpoints, "retain_status_information");
+    h.set("retain_status_information", "1");
+    expected[0][69] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check retry_interval.
+    precheck(tpoints, "retry_interval");
+    h.set("retry_interval", "2");
+    expected[0][70] = 2;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check stalk_on_down.
+    precheck(tpoints, "stalk_on_down");
+    h.set("stalking_options", "d");
+    expected[0][73] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check stalk_on_unreachable.
+    precheck(tpoints, "stalk_on_unreachable");
+    h.set("stalking_options", "d,u");
+    expected[0][74] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check stalk_on_up.
+    precheck(tpoints, "stalk_on_up");
+    h.set("stalking_options", "a");
+    expected[0][75] = true;
+    postcheck(engine, tpoints, db, expected);
+
+    // Check statusmap_image.
+    precheck(tpoints, "statusmap_image");
+    h.set("statusmap_image", "my_map_image.png");
+    expected[0][78] = "my_map_image.png";
+    postcheck(engine, tpoints, db, expected);
+
+    // Check state.
+
+    // Check state type.
+
+    // Check acknowledged.
+    // XXX
+
+    // Check acknowledgement_type.
+    // XXX
+
+    // Check check_type.
+    // XXX
+
+    // Check check_attempt.
+    // XXX
+
+    // Check checked.
+    // XXX
+
+    // Check command_line.
+    // XXX
+
+    // Check execution_time.
+    // XXX
+
+    // Check flapping.
+    // XXX
+
+    // Check last_check.
+
+    // Check last_hard_state.
+
+    // Check last_hard_state_change.
+
+    // Check last_notification.
+
+    // Check last_state_change.
+
+    // Check last_time_down.
+
+    // Check last_time_unreachable.
+
+    // Check last_time_up.
+
+    // Check last_update.
+
+    // Check latency.
+
+    // Check next_check.
+
+    // Check next_host_notification.
+
+    // Check no_more_notifications.
+
+    // Check notification_number.
+
+    // Check output.
+
+    // Check percent_state_change.
+
+    // Check perfdata.
+
+    // Check real_state.
+
+    // Check scheduled_downtime_depth.
+
+    // Check active_checks.
+    precheck(
+      tpoints,
+      "active_checks, default_active_checks, should_be_scheduled");
+    h.set("active_checks_enabled", "1");
+    expected[0][6] = true;
+    expected[0][17] = true;
+    expected[0][72] = true;
     postcheck(engine, tpoints, db, expected);
 
     // Success.
