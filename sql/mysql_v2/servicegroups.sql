758b616defc4e5801a88e205e041fa3e142903ea
Matthieu Kermagoret <mkermagoret@merethis.com>
2015-11-05 11:06:34 +0100

    SQL: remove UNIQUE on group names.

--- sql/mysql_v2/servicegroups.sql
+++ sql/mysql_v2/servicegroups.sql
@@ -5,6 +5,5 @@ CREATE TABLE servicegroups (
   servicegroup_id int NOT NULL,
   name varchar(255) NOT NULL,
 
-  PRIMARY KEY (servicegroup_id),
-  UNIQUE (name)
+  PRIMARY KEY (servicegroup_id)
 ) ENGINE=InnoDB;
--- sql/oracle_v2/hostgroups.sql
+++ sql/oracle_v2/hostgroups.sql
@@ -1,10 +1,9 @@
 --
--- Host groups.
+-- Groups of hosts.
 --
 CREATE TABLE hostgroups (
   hostgroup_id int NOT NULL,
   name varchar(255) NOT NULL,
 
-  PRIMARY KEY (hostgroup_id),
-  UNIQUE (name)
+  PRIMARY KEY (hostgroup_id)
 );
