67069159d39b6e394cf841341d0cadbb6a45ad97
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-01-12 09:17:28 +0100

    RRD: set default step to 5 minutes.

--- rrd/src/creator.cc
+++ rrd/src/creator.cc
@@ -1,5 +1,5 @@
 /*
-** Copyright 2013-2014 Centreon
+** Copyright 2013-2016 Centreon
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -121,7 +121,7 @@ void creator::create(
                 short value_type) {
   // Fill template informations.
   if (!step)
-    step = 5 * 60 * 60; // Default to every 5 minutes.
+    step = 5 * 60; // Default to every 5 minutes.
   if (!length)
     length = 31 * 24 * 60 * 60 / step; // Default to one month long.
   tmpl_info info;
