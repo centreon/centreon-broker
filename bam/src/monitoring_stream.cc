98a9229d66a6e1accb58ac185e4adc851689dafc
afouille <afouille@merethis.net>
2016-07-07 17:24:49 +0200

    BAM: fix mismatching commands.

--- bam/src/monitoring_stream.cc
+++ bam/src/monitoring_stream.cc
@@ -375,7 +375,7 @@ int monitoring_stream::write(misc::shared_ptr<io::data> const& data) {
     if (dwn.in_downtime)
       oss << "[" << now << "] SCHEDULE_SVC_DOWNTIME;_Module_BAM_1;ba_"
           << dwn.ba_id << ";;" << timestamp::max()
-          << ";1;0;0;BAM Module;"
+          << ";1;0;0;Centreon Broker BAM Module;"
              "Automatic downtime triggered by BA downtime inheritance";
     else
       oss << "[" << now << "] DEL_SVC_DOWNTIME_FULL;_Module_BAM_1;ba_"
