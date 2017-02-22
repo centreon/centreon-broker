c85a7c4e6a4b4e4859977dfdd6c036a466a26f92
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-02-22 15:47:24 +0100

    storage: moving perfdata parser tests to gtest.

--- storage/test/parser/parse_perfdata.cc
+++ storage/test/parser/parse_perfdata.cc
@@ -1,5 +1,5 @@
 /*
-** Copyright 2011-2013 Centreon
+** Copyright 2011-2013,2017 Centreon
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -16,38 +16,32 @@
 ** For more information : contact@centreon.com
 */
 
-#include <cmath>
-#include <cstdlib>
+#include <gtest/gtest.h>
 #include <QList>
-#include "com/centreon/broker/config/applier/init.hh"
 #include "com/centreon/broker/storage/parser.hh"
 #include "com/centreon/broker/storage/perfdata.hh"
 
 using namespace com::centreon::broker;
-using namespace com::centreon::broker::storage;
 
-/**
- *  Check that the perfdata parser works as expected.
- *
- *  @return 0 on success.
- */
-int main() {
-  // Initialization.
-  config::applier::init();
-
-  // Objects.
-  QList<perfdata> list;
-  parser p;
-
-  // Parse perfdata string.
+// Given an storage::parser object
+// When parse_perfdata() is called with a valid perfdata string
+// Then perfdata are returned in a list
+TEST(StorageParserParsePerfdata, ParseSimple) {
+  // Parse perfdata.
+  QList<storage::perfdata> list;
+  storage::parser p;
   p.parse_perfdata(
     "time=2.45698s;2.000000;5.000000;0.000000;10.000000",
     list);
 
-  // Expected perfdata.
-  perfdata expected;
+  // Check list size.
+  ASSERT_EQ(list.size(), 1);
+
+  // Check content.
+  QList<storage::perfdata>::const_iterator it(list.begin());
+  storage::perfdata expected;
   expected.name("time");
-  expected.value_type(perfdata::gauge);
+  expected.value_type(storage::perfdata::gauge);
   expected.value(2.45698);
   expected.unit("s");
   expected.warning(2.0);
@@ -56,13 +50,5 @@ int main() {
   expected.critical_low(0.0);
   expected.min(0.0);
   expected.max(10.0);
-
-  // Check parsing.
-  bool error((list.size() != 1)
-             || (list.front() != expected));
-
-  // Cleanup.
-  config::applier::deinit();
-
-  return (error ? EXIT_FAILURE : EXIT_SUCCESS);
+  ASSERT_TRUE(expected == *it);
 }
