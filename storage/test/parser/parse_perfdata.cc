e8790e133938e33cc2f1121be88cd2d1c9a8d61a
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-02-22 15:54:36 +0100

    storage: moving perfdata parser tests to gtest.

--- storage/test/parser/parse_perfdata.cc
+++ storage/test/parser/parse_perfdata.cc
@@ -23,10 +23,10 @@
 
 using namespace com::centreon::broker;
 
-// Given an storage::parser object
+// Given a storage::parser object
 // When parse_perfdata() is called with a valid perfdata string
 // Then perfdata are returned in a list
-TEST(StorageParserParsePerfdata, ParseSimple) {
+TEST(StorageParserParsePerfdata, Simple) {
   // Parse perfdata.
   QList<storage::perfdata> list;
   storage::parser p;
@@ -34,10 +34,8 @@ TEST(StorageParserParsePerfdata, ParseSimple) {
     "time=2.45698s;2.000000;5.000000;0.000000;10.000000",
     list);
 
-  // Check list size.
+  // Assertions.
   ASSERT_EQ(list.size(), 1);
-
-  // Check content.
   QList<storage::perfdata>::const_iterator it(list.begin());
   storage::perfdata expected;
   expected.name("time");
@@ -51,4 +49,42 @@ TEST(StorageParserParsePerfdata, ParseSimple) {
   expected.min(0.0);
   expected.max(10.0);
   ASSERT_TRUE(expected == *it);
+  ++it;
+  ASSERT_TRUE(it == list.end());
+}
+
+// Given a storage::parser object
+// When parse_perfdata() is called multiple time with valid strings
+// Then the corresponding perfdata list is returned
+TEST(StorageParserParsePerfdata, Loop) {
+  // Objects.
+  QList<storage::perfdata> list;
+  storage::parser p;
+
+  // Loop.
+  for (unsigned int i(0); i < 10000; ++i) {
+    // Parse perfdata string.
+    list.clear();
+    p.parse_perfdata(
+        "c[time]=2.45698s;2.000000;5.000000;0.000000;10.000000",
+        list);
+
+    // Assertions.
+    ASSERT_EQ(list.size(), 1);
+    QList<storage::perfdata>::const_iterator it(list.begin());
+    storage::perfdata expected;
+    expected.name("time");
+    expected.value_type(storage::perfdata::counter);
+    expected.value(2.45698);
+    expected.unit("s");
+    expected.warning(2.0);
+    expected.warning_low(0.0);
+    expected.critical(5.0);
+    expected.critical_low(0.0);
+    expected.min(0.0);
+    expected.max(10.0);
+    ASSERT_TRUE(expected == *it);
+    ++it;
+    ASSERT_TRUE(it == list.end());
+  }
 }
