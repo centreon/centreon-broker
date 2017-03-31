2c022977a15ff5b53842b0c02b1c99dfc04232e2
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-31 16:14:33 +0200

    core: add new fs_file interface.

---
+++ core/inc/com/centreon/broker/file/fs_file.hh
@@ -0,0 +1,67 @@
+/*
+** Copyright 2017 Centreon
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+**
+** For more information : contact@centreon.com
+*/
+
+#ifndef CCB_FILE_FS_FILE_HH
+#  define CCB_FILE_FS_FILE_HH
+
+#  include <string>
+#  include "com/centreon/broker/namespace.hh"
+
+CCB_BEGIN()
+
+namespace        file {
+  /**
+   *  @class fs_file fs_file.hh "com/centreon/broker/file/fs_file.hh"
+   *  @brief File system (on-disk) file.
+   *
+   *  Interface to manipulate an on-disk file.
+   */
+  class          fs_file {
+   public:
+    enum         open_mode {
+      mode_read = 1,
+      mode_write,
+      mode_read_write,
+      mode_read_write_no_create
+    };
+    enum         seek_whence {
+      whence_start = 1,
+      whence_current,
+      whence_end
+    };
+
+                 fs_file();
+    virtual      ~fs_file();
+    virtual void close() = 0;
+    virtual void open(std::string const& path, open_mode mode) = 0;
+    virtual long read(void* buffer, long max_size) = 0;
+    virtual void seek(
+                   long offset,
+                   seek_whence whence = whence_start) = 0;
+    virtual long tell() = 0;
+    virtual long write(void const* buffer, long size) = 0;
+
+   private:
+                 fs_file(fs_file const& other);
+    fs_file&     operator=(fs_file const& other);
+  };
+}
+
+CCB_END()
+
+#endif // !CCB_FILE_FS_FILE_HH
