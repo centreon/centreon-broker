961ecac4a57b9d587225b517622d424c64dd5e4a
Sylvestre Gallon <ccna.syl@gmail.com>
2019-09-05 08:12:45 +0200

    enh(storage): add more tests.

---
+++ storage/test/remove_graph.cc
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2011 - 2019 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+#include <gtest/gtest.h>
+#include "com/centreon/broker/storage/remove_graph.hh"
+
+using namespace com::centreon::broker;
+
+/**
+ *  Check that remove_graph's assignment operator.
+ */
+TEST(StorageRemoveGraph, Assign) {
+  // Base object.
+  storage::remove_graph r1;
+  r1.id = 42;
+  r1.is_index = false;
+
+  // Second object.
+  storage::remove_graph r2;
+  r2.id = 234573485;
+  r2.is_index = true;
+
+  // Assignment.
+  r2 = r1;
+
+  // Reset base object.
+  r1.id = 36;
+  r1.is_index = true;
+
+  // Check.
+  ASSERT_FALSE(r1.id != 36);
+  ASSERT_FALSE(!r1.is_index);
+  ASSERT_FALSE(r2.id != 42);
+  ASSERT_FALSE(r2.is_index);
+}
+
+/**
+ *  Check that remove_graph copy constructor works properly.
+ */
+TEST(StorageRemoveGraph, CopyCtor) {
+  // Base object.
+  storage::remove_graph r1;
+  r1.id = 42;
+  r1.is_index = false;
+
+  // Copy object.
+  storage::remove_graph r2(r1);
+
+  // Reset base object.
+  r1.id = 36;
+  r1.is_index = true;
+
+  // Check.
+  ASSERT_FALSE(r1.id != 36);
+  ASSERT_FALSE(!r1.is_index);
+  ASSERT_FALSE(r2.id != 42);
+  ASSERT_FALSE(r2.is_index);
+}
+
+/**
+ *  Check that the remove_graph object properly default constructs.
+ */
+TEST(StorageRemoveGraph, DefaultCtor) {
+  // Build object.
+  storage::remove_graph r;
+
+  // Check properties values.
+  ASSERT_FALSE(r.id != 0);
+  ASSERT_FALSE(r.is_index);
+}
---
+++ storage/test/status.cc
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2011 - 2019 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+#include <gtest/gtest.h>
+#include "com/centreon/broker/io/events.hh"
+#include "com/centreon/broker/storage/internal.hh"
+#include "com/centreon/broker/storage/status.hh"
+
+using namespace com::centreon::broker;
+
+/**
+ *  Check that the status assignment operator works properly.
+ */
+TEST(StorageStatus, Assign) {
+  // First object.
+  storage::status s1;
+  s1.ctime = 123456789;
+  s1.index_id = 6774;
+  s1.interval = 42;
+  s1.is_for_rebuild = true;
+  s1.rrd_len = 180;
+  s1.state = 3;
+
+  // Second object.
+  storage::status s2;
+  s2.ctime = 654123;
+  s2.index_id = 33;
+  s2.interval = 78;
+  s2.is_for_rebuild = false;
+  s2.rrd_len = 666;
+  s2.state = 1;
+
+  // Assignment.
+  s2 = s1;
+
+  // Change first object.
+  s1.ctime = 741258;
+  s1.index_id = 12;
+  s1.interval = 36;
+  s1.is_for_rebuild = false;
+  s1.rrd_len = 900;
+  s1.state = 2;
+
+  // Check objects properties values.
+  ASSERT_FALSE(s1.ctime != 741258);
+  ASSERT_FALSE(s1.index_id != 12);
+  ASSERT_FALSE(s1.interval != 36);
+  ASSERT_FALSE(s1.is_for_rebuild != false);
+  ASSERT_FALSE(s1.rrd_len != 900);
+  ASSERT_FALSE(s1.state != 2);
+  ASSERT_FALSE(s2.ctime != 123456789);
+  ASSERT_FALSE(s2.index_id != 6774);
+  ASSERT_FALSE(s2.interval != 42);
+  ASSERT_FALSE(s2.is_for_rebuild != true);
+  ASSERT_FALSE(s2.rrd_len != 180);
+  ASSERT_FALSE(s2.state != 3);
+}
+
+/**
+ *  Check that the status copy constructor works properly.
+ */
+TEST(StorageStatus, CopyCtor) {
+  // First object.
+  storage::status s1;
+  s1.ctime = 123456789;
+  s1.index_id = 42;
+  s1.interval = 24;
+  s1.is_for_rebuild = true;
+  s1.rrd_len = 180;
+  s1.state = 1;
+
+  // Second object.
+  storage::status s2(s1);
+
+  // Change first object.
+  s1.ctime = 741258;
+  s1.index_id = 3612;
+  s1.interval = 36;
+  s1.is_for_rebuild = false;
+  s1.rrd_len = 900;
+  s1.state = 2;
+
+  // Check objects properties values.
+  ASSERT_FALSE(s1.ctime != 741258);
+  ASSERT_FALSE(s1.index_id != 3612);
+  ASSERT_FALSE(s1.interval != 36);
+  ASSERT_FALSE(s1.is_for_rebuild != false);
+  ASSERT_FALSE(s1.rrd_len != 900);
+  ASSERT_FALSE(s1.state != 2);
+  ASSERT_FALSE(s2.ctime != 123456789);
+  ASSERT_FALSE(s2.index_id != 42);
+  ASSERT_FALSE(s2.interval != 24);
+  ASSERT_FALSE(s2.is_for_rebuild != true);
+  ASSERT_FALSE(s2.rrd_len != 180);
+  ASSERT_FALSE(s2.state != 1);;
+}
+
+/**
+ *  Check that the status object properly default constructs.
+ */
+TEST(StorageStatus, DefaultCtor) {
+  // Build object.
+  storage::status s;
+
+  auto val(io::events::data_type<io::events::storage, storage::de_status>::value);
+
+  // Check properties values.
+  ASSERT_FALSE(s.ctime != 0);
+  ASSERT_FALSE(s.index_id != 0);
+  ASSERT_FALSE(s.interval != 0);
+  ASSERT_FALSE(s.is_for_rebuild != false);
+  ASSERT_FALSE(s.rrd_len != 0);
+  ASSERT_FALSE(s.state != 0);
+  ASSERT_FALSE(s.type() != val);;
+}
