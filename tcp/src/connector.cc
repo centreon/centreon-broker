d68292012d8395ccdc1a7d42372a4d6586e00a00
Sylvestre Gallon <sgallon@centreon.com>
2019-09-13 14:34:18 +0200

    enh(tcp): lots of changes for tcp.
    Fix connector to work well with timers.
    Add test for acceptor, connector, stream and factory.
    Update licence
    Fix some bugs.

--- tcp/src/connector.cc
+++ tcp/src/connector.cc
@@ -1,20 +1,21 @@
 /*
-** Copyright 2011-2015 Centreon
-**
-** Licensed under the Apache License, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-**     http://www.apache.org/licenses/LICENSE-2.0
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-**
-** For more information : contact@centreon.com
-*/
+ * Copyright 2011 - 2019 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
 
 #include "com/centreon/broker/tcp/connector.hh"
 #include <memory>
@@ -39,33 +40,9 @@ connector::connector()
     : io::endpoint(false), _port(0), _read_timeout(-1), _write_timeout(-1) {}
 
 /**
- *  Copy constructor.
- *
- *  @param[in] other  Object to copy.
- */
-connector::connector(connector const& other) : io::endpoint(other) {
-  _internal_copy(other);
-}
-
-/**
  *  Destructor.
  */
 connector::~connector() {}
-
-/**
- *  Assignment operator.
- *
- *  @param[in] other  Object to copy.
- *
- *  @return This object.
- */
-connector& connector::operator=(connector const& other) {
-  if (this != &other) {
-    io::endpoint::operator=(other);
-    _internal_copy(other);
-  }
-  return *this;
-}
 
 /**
  *  Set connection parameters.
@@ -131,7 +108,7 @@ std::shared_ptr<io::stream> connector::open() {
       << "TCP: successfully connected to " << connection_name;
 
   // Return stream.
-  std::shared_ptr<stream> s(new stream(sock.get(), connection_name));
+  std::shared_ptr<stream> s(new stream(_io_context, sock.get(), connection_name));
   sock.release();
   s->set_read_timeout(_read_timeout);
   s->set_write_timeout(_write_timeout);
@@ -154,22 +131,4 @@ void connector::set_read_timeout(int secs) {
  */
 void connector::set_write_timeout(int secs) {
   _write_timeout = secs;
-}
-
-/**************************************
- *                                     *
- *           Private Methods           *
- *                                     *
- **************************************/
-
-/**
- *  Copy internal data members.
- *
- *  @param[in] other  Object to copy.
- */
-void connector::_internal_copy(connector const& other) {
-  _host = other._host;
-  _port = other._port;
-  _read_timeout = other._read_timeout;
-  _write_timeout = other._write_timeout;
 }
