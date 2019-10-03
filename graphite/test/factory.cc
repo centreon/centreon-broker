daef8fb9ea265554427d241fa3a788d70540cffc
Sylvestre Gallon <ccna.syl@gmail.com>
2019-10-03 10:33:37 +0200

    enh(graphite): lots of minor fixes
    - Add a good test coverage.
    - Speedup metric/status resolution.
    - escape status service name.
    - fix some scheme compilation
    - remove some unused code.

---
+++ graphite/test/factory.cc
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+#include "com/centreon/broker/graphite/factory.hh"
+#include <gtest/gtest.h>
+#include "com/centreon/broker/exceptions/msg.hh"
+
+using namespace com::centreon::broker;
+
+TEST(graphiteFactory, HasEndpoint) {
+  graphite::factory fact;
+  config::endpoint cfg;
+
+  cfg.type = "tcp";
+  ASSERT_FALSE(fact.has_endpoint(cfg));
+  cfg.type = "graphite";
+  ASSERT_TRUE(fact.has_endpoint(cfg));
+  ASSERT_EQ(cfg.cache_enabled, true);
+  ASSERT_EQ(cfg.params["cache"], "yes");
+}
+
+TEST(graphiteFactory, MissingParams) {
+  graphite::factory fact;
+  config::endpoint cfg;
+  std::shared_ptr<persistent_cache> cache;
+  bool is_acceptor;
+
+  fact.clone();
+  ASSERT_THROW(fact.new_endpoint(cfg, is_acceptor, cache), exceptions::msg);
+  cfg.params["db_host"] = "host";
+  ASSERT_NO_THROW(delete fact.new_endpoint(cfg, is_acceptor, cache));
+  cfg.params["db_port"] = "toto";
+  ASSERT_THROW(fact.new_endpoint(cfg, is_acceptor, cache), std::exception);
+  cfg.params["db_port"] = "1234";
+  ASSERT_NO_THROW(delete fact.new_endpoint(cfg, is_acceptor, cache));
+  cfg.params["queries_per_transaction"] = "toto";
+  ASSERT_THROW(fact.new_endpoint(cfg, is_acceptor, cache), std::exception);
+  cfg.params["queries_per_transaction"] = "1234";
+  ASSERT_NO_THROW(delete fact.new_endpoint(cfg, is_acceptor, cache));
+  cfg.params["metric_naming"] = "centreon.metrics.$METRICID$";
+  ASSERT_NO_THROW(delete fact.new_endpoint(cfg, is_acceptor, cache));
+}
