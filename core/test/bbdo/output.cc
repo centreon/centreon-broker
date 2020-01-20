b80d2c3877cfd628ab1e98fc685abdcc9e76a7ca
David Boucher <dboucher@centreon.com>
2020-01-20 13:16:21 +0100

    fix(bbdo/output): Bad string initialization in LongWrite test.

--- core/test/bbdo/output.cc
+++ core/test/bbdo/output.cc
@@ -143,11 +143,10 @@ TEST_F(OutputTest, WriteLongService) {
   modules::loader l;
   l.load_file("./neb/10-neb.so");
 
-  std::shared_ptr<neb::service> svc(new neb::service);
+  auto svc = std::make_shared<neb::service>();
   svc->host_id = 12;
   svc->service_id = 18;
-  svc->output = std::string();
-  svc->output.reserve(70000);
+  svc->output = std::string(70000, 'A');
   char c = 'A';
   for (std::string::iterator i = svc->output.begin(); i != svc->output.end();
        i++) {
