bd932aba20bddc897178dfde1fce32ab7e6d7242
David Boucher <dboucher@centreon.com>
2020-12-10 10:19:14 +0100

    fix(lua): Two bugs fixed because of a rebase

--- lua/src/macro_cache.cc
+++ lua/src/macro_cache.cc
@@ -128,42 +128,6 @@ const std::shared_ptr<neb::host>& macro_cache::get_host(
 }
 
 /**
- *  Get the full service.
- *
- *  @param[in] host_id  The id of the host.
- *  @param[in] service_id  The id of the service.
- *
- *  @return             A shared pointer on the service.
- */
-const std::shared_ptr<neb::service>& macro_cache::get_service(
-    uint64_t host_id,
-    uint64_t service_id) const {
-  auto found = _services.find({host_id, service_id});
-
-  if (found == _services.end())
-    throw exceptions::msg() << "lua: could not find information on service ("
-                            << host_id << "," << service_id << ")";
-  return found->second;
-}
-
-/**
- *  Get the full host.
- *
- *  @param[in] host_id  The id of the host.
- *
- *  @return             A shared pointer on the host.
- */
-const std::shared_ptr<neb::host>& macro_cache::get_host(
-    uint64_t host_id) const {
-  auto found = _hosts.find(host_id);
-
-  if (found == _hosts.end())
-    throw exceptions::msg()
-        << "lua: could not find information on host " << host_id;
-  return found->second;
-}
-
-/**
  *  Get the name of a host.
  *
  *  @param[in] host_id  The id of the host.
