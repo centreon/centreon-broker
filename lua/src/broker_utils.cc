685c3a44f8dccf0ab6af07fe38ac81bcb3301cbd
David Boucher <dboucher@centreon.com>
2021-03-23 18:32:57 +0100

    enh(lua): md5 function added to the lua interpreter.

--- lua/src/broker_utils.cc
+++ lua/src/broker_utils.cc
@@ -26,6 +26,7 @@
 #include <iomanip>
 #include <json11.hpp>
 #include <sstream>
+#include <openssl/md5.h>
 
 #include "com/centreon/broker/io/data.hh"
 #include "com/centreon/broker/io/events.hh"
@@ -542,6 +543,30 @@ static int l_broker_stat(lua_State* L) {
   }
 }
 
+static int l_broker_md5(lua_State* L) {
+  auto digit = [] (unsigned char d) -> char {
+    if (d < 10)
+      return '0' + d;
+    else
+      return 'a' + (d - 10);
+  };
+  size_t len;
+  const unsigned char* str = reinterpret_cast<const unsigned char*>(lua_tolstring(L, -1, &len));
+  unsigned char md5[MD5_DIGEST_LENGTH];
+  MD5(str, len, md5);
+  char result[2 * MD5_DIGEST_LENGTH + 1];
+  char* tmp = result;
+  for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+    *tmp = digit(md5[i] >> 4);
+    ++tmp;
+    *tmp = digit(md5[i] & 0xf);
+    ++tmp;
+  }
+  *tmp = 0;
+  lua_pushstring(L, result);
+  return 1;
+}
+
 /**
  *  Load the Lua interpreter with the standard libraries
  *  and the broker lua sdk.
@@ -554,6 +579,7 @@ void broker_utils::broker_utils_reg(lua_State* L) {
                               {"parse_perfdata", l_broker_parse_perfdata},
                               {"url_encode", l_broker_url_encode},
                               {"stat", l_broker_stat},
+                              {"md5", l_broker_md5},
                               {nullptr, nullptr}};
 
 #ifdef LUA51
