91866567e81644760e965bf7bc9e265896e7b6fd
centreonpm <pmargale@centreon.com>
2021-05-05 16:55:18 +0200

    enh(core/test/time): add unit test

--- core/test/time/timerange.cc
+++ core/test/time/timerange.cc
@@ -37,16 +37,21 @@ TEST(Timerange, ParseWeirdTimerange) {
       "08:00 -12:00\r",
       "08:00 - \n12:00\r",
       "08:00\t-\t12:00\r",
-      "08:00-12:00,09:00-12:00",
-      "08:00-12:00")));
+      "08:00-12:00,09:00-12:00,10:00-12:00",
+      "08:00-12:00 , 09:00-12:00, \r\n 10:00-12:00")));
 
   auto& v = tp->get_timeranges();
   ASSERT_EQ(v[0].front().start(), 28800);
   ASSERT_EQ(v[0].front().end(), 43200);
-  ASSERT_EQ(v[5].front().start(), 32400);
+  ASSERT_EQ(v[5].front().start(), 36000);
   ASSERT_EQ(v[5].front().end(), 43200);
   ASSERT_EQ(v[5].back().start(), 28800);
   ASSERT_EQ(v[5].front().end(), 43200);
+  ASSERT_EQ(v[6].front().start(), 36000);
+  ASSERT_EQ(v[6].front().end(), 43200);
+  ASSERT_EQ(v[6].back().start(), 28800);
+  ASSERT_EQ(v[6].front().end(), 43200);
+
 }
 
 TEST(Timerange, ParseWrongTimerange) {
