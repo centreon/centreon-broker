5164439e956bd126be05992bdc2d28f762030075
David Boucher <dboucher@centreon.com>
2021-05-06 12:14:02 +0200

    fix(time): to_string() method optimized.

--- core/test/time/timerange.cc
+++ core/test/time/timerange.cc
@@ -20,35 +20,42 @@
 #include <gtest/gtest.h>
 #include "com/centreon/broker/time/timerange.hh"
 #include "com/centreon/broker/time/timeperiod.hh"
-  
+
 using namespace com::centreon::broker::time;
 
 
 TEST(Timerange, ParseWeirdTimerange) {
   std::unique_ptr<timeperiod> tp;
-  
-  // Here we tests weird timeranges but they should not throw 
+
+  // Here we tests weird timeranges but they should not throw
   // Timerange parser must understands theses
 
   ASSERT_NO_THROW(tp.reset(new timeperiod(2, "test", "alias",
-
       "\r \r08:00\r-12:00\r",
-
       "\n\n08:00 - 12:00\r",
-
       "08:00 -12:00\r",
-
       "08:00 - \n12:00\r",
-
       "08:00\t-\t12:00\r",
-
-      "08:00-12:00\n",
-
-      "08:00-12:00")));
+      "08:00-12:00,09:00-12:00,10:00-12:00",
+      "08:00-12:00 , 09:00-12:00, \r\n 10:00-12:00")));
 
   auto& v = tp->get_timeranges();
   ASSERT_EQ(v[0].front().start(), 28800);
   ASSERT_EQ(v[0].front().end(), 43200);
+  ASSERT_EQ(v[0].front().to_string(), "08:00-12:00");
+  ASSERT_EQ(v[5].front().start(), 36000);
+  ASSERT_EQ(v[5].front().end(), 43200);
+  ASSERT_EQ(v[5].back().start(), 28800);
+  ASSERT_EQ(v[5].front().end(), 43200);
+  ASSERT_EQ(timerange::build_string_from_timeranges(v[5]),
+      "08:00-12:00, 09:00-12:00, 10:00-12:00");
+  ASSERT_EQ(v[6].front().start(), 36000);
+  ASSERT_EQ(v[6].front().end(), 43200);
+  ASSERT_EQ(v[6].back().start(), 28800);
+  ASSERT_EQ(v[6].front().end(), 43200);
+  ASSERT_EQ(timerange::build_string_from_timeranges(v[6]),
+      "08:00-12:00, 09:00-12:00, 10:00-12:00");
+
 }
 
 TEST(Timerange, ParseWrongTimerange) {
@@ -61,4 +68,5 @@ TEST(Timerange, ParseWrongTimerange) {
   ASSERT_FALSE(t.build_timeranges_from_string("abc08:00-12:00", l));
   ASSERT_FALSE(t.build_timeranges_from_string("\n   abc08:00-12:00", l));
   ASSERT_FALSE(t.build_timeranges_from_string("08abcd:00-12:00", l));
+  ASSERT_FALSE(t.build_timeranges_from_string("  :00-12:00", l));
 }
