61f747be0bac6056a649cd45222cd22b51205a0e
David Boucher <dboucher@centreon.com>
2021-05-10 17:47:52 +0200

    fix(conan): build using conan-center works again
    * fix(conan): json11 dependencies fixed.
    * fix(conan): packages come from conant-center now.
    
    Co-authored-by: Gres Remi <rgres@centreon.com>
    
    REFS: MON-7244

--- cmake.sh
+++ cmake.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
-if [ "$1" = "-f" ] ; then
-  force=1
-  shift
-fi
+show_help() {
+cat << EOF
+Usage: ${0##*/} -n=[yes|no] -v
+This program build Centreon-broker
+    -f|--force    : force rebuild
+    -r|--release  : Build on release mode
+    -h|--help     : help
+EOF
+}
+BUILD_TYPE="Debug"
+for i in "$@"
+do
+  case $i in
+    -f|--force)
+      force=1
+      shift
+      ;;
+    -r|--release)
+      BUILD_TYPE="Release"
+      ;;
+    -h|--help)
+      show_help
+      exit 2
+      ;;
+    *)
+            # unknown option
+    ;;
+  esac
+done
 
 # Am I root?
 my_id=$(id -u)
@@ -35,8 +60,26 @@ if [ -r /etc/centos-release ] ; then
     echo "pip3 already installed"
   fi
 
-  if ! rpm -q gcc-c++ ; then
-    yum -y install gcc-c++
+  good=$(gcc --version | awk '/gcc/ && ($3+0)>5.0{print 1}')
+
+  if [ ! $good ] ; then
+    yum -y install centos-release-scl
+    yum-config-manager --enable rhel-server-rhscl-7-rpms
+    yum -y install devtoolset-9
+    ln -s /usr/bin/cmake3 /usr/bin/cmake
+    source /opt/rh/devtoolset-9/enable
+  fi
+
+  if [ $maj = "centos7" ] ; then
+    curl https://downloads.mariadb.com/MariaDB/mariadb-10.5.8/yum/centos7-amd64/rpms/MariaDB-shared-10.5.8-1.el7.centos.x86_64.rpm --output MariaDB-shared-10.5.8-1.el7.centos.x86_64.rpm
+    curl https://downloads.mariadb.com/MariaDB/mariadb-10.5.8/yum/centos7-amd64/rpms/MariaDB-common-10.5.8-1.el7.centos.x86_64.rpm --output MariaDB-common-10.5.8-1.el7.centos.x86_64.rpm
+    curl https://downloads.mariadb.com/MariaDB/mariadb-10.5.8/yum/centos7-amd64/rpms/MariaDB-compat-10.5.8-1.el7.centos.x86_64.rpm --output MariaDB-compat-10.5.8-1.el7.centos.x86_64.rpm
+    yum install -y MariaDB*.rpm
+  else
+    curl https://downloads.mariadb.com/MariaDB/mariadb-10.5.8/yum/centos8-amd64/rpms/MariaDB-shared-10.5.8-1.el8.x86_64.rpm --output MariaDB-shared-10.5.8-1.el8.x86_64.rpm
+    curl https://downloads.mariadb.com/MariaDB/mariadb-10.5.8/yum/centos8-amd64/rpms/MariaDB-common-10.5.8-1.el8.x86_64.rpm --output MariaDB-common-10.5.8-1.el8.x86_64.rpm
+    curl https://downloads.mariadb.com/MariaDB/mariadb-10.5.8/yum/centos8-amd64/rpms/MariaDB-compat-10.5.8-1.el8.x86_64.rpm --output MariaDB-compat-10.5.8-1.el8.x86_64.rpm
+    dnf install -y MariaDB-*.rpm
   fi
 
   pkgs=(
@@ -44,6 +87,7 @@ if [ -r /etc/centos-release ] ; then
     rrdtool-devel
     gnutls-devel
     lua-devel
+    perl-Thread-Queue
   )
   for i in "${pkgs[@]}"; do
     if ! rpm -q $i ; then
@@ -56,11 +100,17 @@ if [ -r /etc/centos-release ] ; then
   done
 elif [ -r /etc/issue ] ; then
   maj=$(cat /etc/issue | awk '{print $1}')
+  version=$(cat /etc/issue | awk '{print $3}')
+  if [ $version = "9" ] ; then
+    dpkg="dpkg"
+  else
+    dpkg="dpkg --no-pager"
+  fi
   v=$(cmake --version)
   if [[ $v =~ "version 3" ]] ; then
     cmake='cmake'
   elif [ $maj = "Debian" ] ; then
-    if dpkg -l --no-pager cmake ; then
+    if $dpkg -l cmake ; then
       echo "Bad version of cmake..."
       exit 1
     else
@@ -80,6 +130,55 @@ elif [ -r /etc/issue ] ; then
     echo "Bad version of cmake..."
     exit 1
   fi
+
+  if [ $maj = "Debian" ] ; then
+    pkgs=(
+      gcc
+      g++
+      pkg-config
+      libmariadb3
+      librrd-dev
+      libgnutls28-dev
+      ninja-build
+      liblua5.3-dev
+      python3
+      python3-pip
+    )
+    for i in "${pkgs[@]}"; do
+      if ! $dpkg -l $i | grep "^ii" ; then
+        if [ $my_id -eq 0 ] ; then
+          apt install -y $i
+        else
+          echo -e "The package \"$i\" is not installed, you can install it, as root, with the command:\n\tapt install -y $i\n\n"
+          exit 1
+        fi
+      fi
+    done
+  elif [ $maj = "Raspbian" ] ; then
+    pkgs=(
+      gcc
+      g++
+      pkg-config
+      libmariadb3
+      librrd-dev
+      libgnutls28-dev
+      ninja-build
+      liblua5.3-dev
+      python3
+      python3-pip
+    )
+    for i in "${pkgs[@]}"; do
+      if ! $dpkg -l $i | grep "^ii" ; then
+        if [ $my_id -eq 0 ] ; then
+          apt install -y $i
+        else
+          echo -e "The package \"$i\" is not installed, you can install it, as root, with the command:\n\tapt install -y $i\n\n"
+          exit 1
+        fi
+      fi
+    done
+  fi
+
   if [[ ! -x /usr/bin/python3 ]] ; then
     if [ $my_id -eq 0 ] ; then
       apt install -y python3
@@ -90,7 +189,7 @@ elif [ -r /etc/issue ] ; then
   else
     echo "python3 already installed"
   fi
-  if ! dpkg -l --no-pager python3-pip ; then
+  if ! $dpkg -l python3-pip ; then
     if [ $my_id -eq 0 ] ; then
       apt install -y python3-pip
     else
@@ -109,11 +208,9 @@ if [ $my_id -eq 0 ] ; then
 else
   conan="$HOME/.local/bin/conan"
 fi
-if ! $conan remote list | grep ^centreon ; then
-  $conan remote add centreon https://api.bintray.com/conan/centreon/centreon
-fi
 
-good=$(gcc --version | awk '/gcc/ && ($3+0)>5.0{print 1}')
+
+
 
 if [ ! -d build ] ; then
   mkdir build
@@ -126,12 +223,11 @@ if [ "$force" = "1" ] ; then
   mkdir build
 fi
 cd build
-
-if [ $good -eq 1 ] ; then
-  $conan install .. --remote centreon -s compiler.libcxx=libstdc++11
+if [ $maj = "centos7" ] ; then
+    rm -rf ~/.conan/profiles/default
+    $conan install .. -s compiler.libcxx=libstdc++11 --build=missing
 else
-  $conan install .. --remote centreon -s compiler.libcxx=libstdc++
+    $conan install .. -s compiler.libcxx=libstdc++11 --build=missing
 fi
 
 CXXFLAGS="-Wall -Wextra" $cmake -DCMAKE_BUILD_TYPE=Debug -DWITH_PREFIX=/usr -DWITH_PREFIX_BIN=/usr/sbin -DWITH_USER=centreon-broker -DWITH_GROUP=centreon-broker -DWITH_CONFIG_PREFIX=/etc/centreon-broker -DWITH_TESTING=On -DWITH_PREFIX_MODULES=/usr/share/centreon/lib/centreon-broker -DWITH_PREFIX_CONF=/etc/centreon-broker -DWITH_PREFIX_LIB=/usr/lib64/nagios -DWITH_MODULE_SIMU=On $* ..
-
