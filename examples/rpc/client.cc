29f5fc72d53aec49d83aa21a5f9d9981c9a60401
David Boucher <dboucher@centreon.com>
2021-05-12 15:40:07 +0200

    fix(examples): missing files for an example added.

---
+++ examples/rpc/client.cc
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2021 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+#include <grpc/grpc.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <sys/stat.h>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <iostream>
+#include "../src/broker.grpc.pb.h"
+
+using namespace com::centreon::broker;
+
+class BrokerRPCClient {
+  std::unique_ptr<Broker::Stub> _stub;
+
+ public:
+  BrokerRPCClient(std::shared_ptr<grpc::Channel> channel)
+      : _stub(Broker::NewStub(channel)) {}
+
+  bool GetVersion(Version* version) {
+    const ::google::protobuf::Empty e;
+    grpc::ClientContext context;
+    grpc::Status status = _stub->GetVersion(&context, e, version);
+    if (!status.ok()) {
+      std::cout << "GetVersion rpc failed." << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  bool DebugConfReload(GenericResponse* response, std::string const& file) {
+    GenericString request;
+    request.set_allocated_str_arg(new std::string(file));
+
+    grpc::ClientContext context;
+    grpc::Status status = _stub->DebugConfReload(&context, request, response);
+    if (!status.ok()) {
+      std::cout << "ConfReload rpc failed." << std::endl;
+      return false;
+    }
+
+    if (response->ok())
+      return true;
+
+    return false;
+  }
+};
+
+bool exists(const char* name) {
+  struct stat buffer;
+  return stat(name, &buffer) == 0;
+}
+
+int main(int argc, char** argv) {
+  std::cout << "Writing the log config file into /tmp\n";
+  std::ofstream os;
+  os.open("/tmp/broker-log.json", std::ios::out);
+  os << "{ \"log_path\": \"/tmp\", \"console\": false, \"loggers\": [ { "
+        "\"name\": \"bbdo\", \"level\": \"debug\" }, "
+        "{ \"name\": \"core\", \"level\": \"debug\" }, "
+        "{ \"name\": \"sql\", \"level\": \"trace\" }, { \"name\": "
+        "\"perfdata\", \"level\": \"err\" } ] }";
+  os.close();
+
+  std::cout << "Applying the log configuration\n";
+  int32_t status = 0;
+  BrokerRPCClient client(grpc::CreateChannel(
+      "127.0.0.1:51001", grpc::InsecureChannelCredentials()));
+
+  GenericResponse response;
+  status = client.DebugConfReload(&response, "/tmp/broker-log.json");
+  if (!response.ok())
+    std::cout
+        << "Configuration application failed for file /tmp/broker-log.json: "
+        << response.err_msg() << std::endl;
+  else
+    std::cout << "Configuration applied.\n";
+
+  std::remove("/tmp/broker-log.json");
+  exit(status);
+}
