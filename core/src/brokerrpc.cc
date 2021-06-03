81c9d75beff4352791af447185ad367203023580
David Boucher <dboucher@centreon.com>
2021-06-03 19:20:14 +0200

    fix(rpc): memory leak fixed

--- core/src/brokerrpc.cc
+++ core/src/brokerrpc.cc
@@ -26,12 +26,12 @@ using namespace com::centreon::broker;
 brokerrpc::brokerrpc(const std::string& address,
                      uint16_t port,
                      std::string const& broker_name) {
-  broker_impl* service = new broker_impl;
-  service->set_broker_name(broker_name);
+  //broker_impl* service = new broker_impl;
+  _service.set_broker_name(broker_name);
   std::string server_address{fmt::format("{}:{}", address, port)};
   grpc::ServerBuilder builder;
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(service);
+  builder.RegisterService(&_service);
   _server = builder.BuildAndStart();
 }
 
