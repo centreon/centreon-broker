b324fae64636a8d78764356bb5fd2bac74a35a0b
David Boucher <dboucher@centreon.com>
2021-06-14 10:58:04 +0200

    fix(tests): compilation warnings removed

--- core/test/time/timerange.cc
+++ core/test/time/timerange.cc
@@ -41,19 +41,19 @@ TEST(Timerange, ParseWeirdTimerange) {
       "08:00-12:00 , 09:00-12:00, \r\n 10:00-12:00")));
 
   auto& v = tp->get_timeranges();
-  ASSERT_EQ(v[0].front().start(), 28800);
-  ASSERT_EQ(v[0].front().end(), 43200);
+  ASSERT_EQ(v[0].front().start(), 28800u);
+  ASSERT_EQ(v[0].front().end(), 43200u);
   ASSERT_EQ(v[0].front().to_string(), "08:00-12:00");
-  ASSERT_EQ(v[5].front().start(), 36000);
-  ASSERT_EQ(v[5].front().end(), 43200);
-  ASSERT_EQ(v[5].back().start(), 28800);
-  ASSERT_EQ(v[5].front().end(), 43200);
+  ASSERT_EQ(v[5].front().start(), 36000u);
+  ASSERT_EQ(v[5].front().end(), 43200u);
+  ASSERT_EQ(v[5].back().start(), 28800u);
+  ASSERT_EQ(v[5].front().end(), 43200u);
   ASSERT_EQ(timerange::build_string_from_timeranges(v[5]),
       "08:00-12:00, 09:00-12:00, 10:00-12:00");
-  ASSERT_EQ(v[6].front().start(), 36000);
-  ASSERT_EQ(v[6].front().end(), 43200);
-  ASSERT_EQ(v[6].back().start(), 28800);
-  ASSERT_EQ(v[6].front().end(), 43200);
+  ASSERT_EQ(v[6].front().start(), 36000u);
+  ASSERT_EQ(v[6].front().end(), 43200u);
+  ASSERT_EQ(v[6].back().start(), 28800u);
+  ASSERT_EQ(v[6].front().end(), 43200u);
   ASSERT_EQ(timerange::build_string_from_timeranges(v[6]),
       "08:00-12:00, 09:00-12:00, 10:00-12:00");
 
