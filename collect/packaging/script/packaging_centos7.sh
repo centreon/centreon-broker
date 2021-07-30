c0447daf201b925eaa8b1a87d2fdbb28aa859944
Gres Remi <rgres@centreon.com>
2021-07-30 15:03:43 +0000

    enh(centreon-collect): move all github into one             REFS: MON-4724

---
+++ collect/packaging/script/packaging_centos7.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#FIXME autoset variable
+VERSION="21.10.01"
+RELEASE="01"
+
+# generate rpm broker
+cd ../../centreon-broker/packaging/script/
+./rpmbuild.sh $VERSION $RELEASE
+
+# generate rpm clib
+cd /centreon-clib/packaging/script/
+./rpmbuild.sh $VERSION $RELEASE
+rpm -i centreon-clib-$VERSION-$RELEASE.el7.x86_64.rpm
+rpm -i centreon-clib-debuginfo-$VERSION-$RELEASE.el7.x86_64.rpm
+rpm -i centreon-clib-devel-$VERSION-$RELEASE.el7.x86_64.rpm
+
+# generate rpm engine
+cd /centreon-engine/packaging/script/
+./rpmbuild.sh $VERSION $RELEASE
+
+# generate rpm connector
+cd /centreon-connector/packaging/script/
+./rpmbuild.sh $VERSION $RELEASE
+
+
