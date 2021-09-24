382f1d1d116b518a0bd534143745c8a7a7872063
David Boucher <dboucher@centreon.com>
2021-09-24 11:44:31 +0200

    fix(config::applier::endpoint): When a feeder connects to bbroker to get data.
    If it stops, broker stops correctly the feeder and no retention is made.
    If some retention is created, because of a peer too slow or a lot of data
    incoming, this retention is removed when broker stops.

--- core/src/processing/acceptor.cc
+++ core/src/processing/acceptor.cc
@@ -23,7 +23,6 @@
 #include "com/centreon/broker/io/endpoint.hh"
 #include "com/centreon/broker/log_v2.hh"
 #include "com/centreon/broker/misc/misc.hh"
-#include "com/centreon/broker/multiplexing/muxer.hh"
 #include "com/centreon/broker/processing/feeder.hh"
 
 using namespace com::centreon::broker;
