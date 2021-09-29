92dfd1c3513c05de6f6613503bcbfbfda7204e8f
rem31 <73845199+rem31@users.noreply.github.com>
2021-09-29 15:53:42 +0000

    test_new_ci

--- ci/scripts/broker-rpm-package.sh
+++ ci/scripts/broker-rpm-package.sh
@@ -12,8 +12,10 @@ if [ ! -d /root/rpmbuild/SOURCES ] ; then
     mkdir -p /root/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 fi
 
-cd "../"
+cd ".."
+echo "################################################## BUILDING broker " pwd
 mkdir centreon-broker-$VERSION
+echo "################################################## BUILDING broker " ls
 cp -r centreon-broker/* centreon-broker-$VERSION
 tar -czf centreon-broker-$VERSION.tar.gz centreon-broker-$VERSION cmake.sh
 mv centreon-broker-$VERSION.tar.gz /root/rpmbuild/SOURCES/
