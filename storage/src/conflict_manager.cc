b6581f8b857943ddbc329b7692e43b5d1b27fb25
Leonard Viktor Pooch <leonard.pooch@hotmail.fr>
2021-11-09 15:41:11 +0100

    fix(storage) : increasing conflict manager initialization timeout. (dev-20.10.x)(#690)
    REFS : MON-11553

--- storage/src/conflict_manager.cc
+++ storage/src/conflict_manager.cc
@@ -125,8 +125,8 @@ bool conflict_manager::init_storage(bool store_in_db,
 
   std::unique_lock<std::mutex> lk(_init_m);
 
-  for (count = 0; count < 10; count++) {
-    /* Let's wait for 10s for the conflict_manager to be initialized */
+  for (count = 0; count < 60; count++) {
+    /* Let's wait for 60s for the conflict_manager to be initialized */
     if (_init_cv.wait_for(lk, std::chrono::seconds(1), [&] {
           return _singleton != nullptr || _state == finished;
         })) {
@@ -152,7 +152,7 @@ bool conflict_manager::init_storage(bool store_in_db,
         count);
   }
   log_v2::sql()->error(
-      "conflict_manager: not initialized after 10s. Probably "
+      "conflict_manager: not initialized after 60s. Probably "
       "an issue in the sql output configuration.");
   return false;
 }
