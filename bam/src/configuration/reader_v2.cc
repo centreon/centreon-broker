9946d6824eec3aeb7d53502d3011626d9abf6ac4
David Boucher <dboucher@centreon.com>
2022-01-03 10:47:10 +0100

    fix(bam/cache): Queries improved.

--- bam/src/configuration/reader_v2.cc
+++ bam/src/configuration/reader_v2.cc
@@ -371,19 +371,14 @@ void reader_v2::_load(bam::hst_svc_mapping& mapping) {
     // XXX : expand hostgroups and servicegroups
     std::promise<database::mysql_result> promise;
     _mysql.run_query_and_get_result(
-        "SELECT h.host_id, s.service_id, h.host_name, "
-        "s.service_description,service_activate FROM mod_bam_kpi k LEFT JOIN "
-        "service s ON k.service_id=s.service_id LEFT JOIN "
-        "host_service_relation AS hsr ON s.service_id=hsr.service_service_id "
-        "LEFT JOIN host AS h ON hsr.host_host_id=h.host_id AND "
-        "h.host_id=k.host_id WHERE k.kpi_type='0'",
+        "SELECT DISTINCT h.host_id, s.service_id, h.host_name, s.service_description,service_activate FR
+        "service s LEFT JOIN host_service_relation AS hsr ON "
+        "s.service_id=hsr.service_service_id "
+        "LEFT JOIN host AS h ON hsr.host_host_id=h.host_id LEFT JOIN "
+        "mod_bam_kpi k ON "
+        "h.host_id=k.host_id AND k.service_id=s.service_id WHERE "
+        "k.kpi_type='0'",
         &promise, 0);
-    //    _mysql.run_query_and_get_result(
-    //        "SELECT h.host_id, s.service_id, h.host_name,
-    //        s.service_description," "   service_activate" "  FROM service AS
-    //        s" "  LEFT JOIN host_service_relation AS hsr" "    ON
-    //        s.service_id=hsr.service_service_id" "  LEFT JOIN host AS h" " ON
-    //        hsr.host_host_id=h.host_id", &promise);
     database::mysql_result res(promise.get_future().get());
     while (_mysql.fetch_row(res))
       mapping.set_service(res.value_as_str(2), res.value_as_str(3),
